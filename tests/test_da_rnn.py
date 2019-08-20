9696f2c5fccc87469d013aa88dc9ba18e52357f2
Isaac Godfried <igodfried@isaac26.com>
2019-08-19 20:28:53 -0400

    add new unit tests

--- tests/test_da_rnn.py
+++ tests/test_da_rnn.py
@@ -12,16 +12,14 @@ class TestPreprocessingDA(unittest.TestCase):
         config, da_network = da_rnn(self.preprocessed_data, 1, 64)
         train(da_network, self.preprocessed_data, config, n_epochs=20, tensorboard=True)
         self.assertEqual(1,1)
+        
     def test_make_data(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_create_model(self):
+        config, dnn_network = da_rnn(self.preprocessed_data, 1, 64)
+        self.assertNotEqual(config.batch_size, 20)
 
 if __name__ == '__main__':
     unittest.main()
