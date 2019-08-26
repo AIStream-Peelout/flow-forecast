bce7ef4365e69857f876e38b0551c0394d12a375
Isaac Godfried <igodfried@isaac26.com>
2019-08-25 22:53:28 -0400

    Description

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     version='0.01dev',
     packages=['flood_forecast', 'flood_forecast.transformer_xl', 'flood_forecast.preprocessing', 'flood_forecast.da_rnn', "flood_forecast.basic"],
     license='Public',
-    long_description='A public package',
+    long_description='A public package for forecasting river flows and flash floods',
     install_requires=['scikit-learn', 'torch', 'tensorflow', 'pandas']
 )
