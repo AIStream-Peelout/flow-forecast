fadb063675c270e95766d6a9e5f1526de933713e
Isaac Godfried <igodfried@isaac26.com>
2019-08-30 00:10:25 -0400

    argsparse updates

--- flood_forecast/trainer.py
+++ flood_forecast/trainer.py
@@ -17,9 +17,9 @@ def main():
     parser.add_argument("-d", "--dataset", help="Path to river flow csv file")
     parser.add_argument("-m", "--model", help="Model you want to use for training")
     parser.add_argument("-t", "--test", default=336, help="The number of hours to forecast for the test")
-    parser.add_argument("-t", "--task", help="The task you want to train the model for")
-    parser.add_argument("-c", "--columns", default="cfs", help="The target column either height, cfs or both")
-    parser.add_argument("")
+    parser.add_argument("-b", "--task", help="The task you want to train the model for")
+    parser.add_argument("-c", "--column", default="cfs", help="The target column either height, cfs or both")
+    parser.add_argument("-s", "--max_epochs", default="10")
     args = parser.parse_args()
     if args.column == "both":
         args.column = ['cfs', 'height']
@@ -28,3 +28,5 @@ def main():
     train_function(args.model, args.dataset, args.test, args.columns)
 if __name__ == "__main__":
     main()
+
+# Example command python trainer.py --dataset data/flow_data/concord_final.csv --model da_rnn --task flow --columns cfs
