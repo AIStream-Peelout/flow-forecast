2cf8e6da2d403bd576c374c41749b1c47c70dcc6
Isaac Godfried <igodfried@isaac26.com>
2019-08-29 23:43:37 -0400

    updates to trainer

--- flood_forecast/trainer.py
+++ flood_forecast/trainer.py
@@ -1,13 +1,15 @@
-import argparse 
+import argparse
+from typing import Sequence, List, Tuple 
 
 def train_function(model:str, training_file_dir:str, test_hours:int, target_col:List[str], n_targs=1):
     if model == "da_rnn":
         from flood_forecast.da_rnn.train_da import da_rnn, train
         from flood_forecast.preprocessing.preprocess_da_rnn import make_data
         preprocessed_data = make_data(training_file_dir, target_col, test_hours)
-        model = da_rnn(preprocessed_data, len(target_col))
-        elif model == "":
-            pass 
+        config, model = da_rnn(preprocessed_data, len(target_col))
+        train(model, preprocessed_data, config)
+    elif model == "":
+        pass 
 
 
 def main():
@@ -16,12 +18,13 @@ def main():
     parser.add_argument("-m", "--model", help="Model you want to use for training")
     parser.add_argument("-t", "--test", default=336, help="The number of hours to forecast for the test")
     parser.add_argument("-t", "--task", help="The task you want to train the model for")
-    parser.add_argument("-c", "--column", default="cfs", help="The target column either height, cfs or both")
+    parser.add_argument("-c", "--columns", default="cfs", help="The target column either height, cfs or both")
     parser.add_argument("")
+    args = parser.parse_args()
     if args.column == "both":
         args.column = ['cfs', 'height']
     else: 
         args.column = [args.column]
-    
+    train_function(args.model, args.dataset, args.test, args.columns)
 if __name__ == "__main__":
     main()
