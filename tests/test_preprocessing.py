e1a30a1927a1ad86c469db1c1af0d007a47cbf7c
Isaac Godfried <igodfried@isaac26.com>
2019-09-02 23:01:32 -0400

    rollback

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -3,15 +3,15 @@ sys.path.append("..")
 from flood_forecast.preprocessing.preprocess_da_rnn import TrainData, format_data, make_data 
 import unittest
 import pandas as pd
-import os 
+
 class TestPreprocessingDA(unittest.TestCase):
     def test_format_data(self):
-        df = pd.read_csv(os.path.join(os.path.dirname(__file__), "test_data", "test_format_data.csv"))
+        df = pd.read_csv("test_data/test_format_data.csv")
         self.assertEqual(type(format_data(df, ["height"])), TrainData)
         self.assertEqual(len(format_data(df, ["height"]).feats[0]), 2)
 
     def test_make_function(self):
-        result = make_data(os.path.join(os.path.dirname(__file__), "test_data", "test_format_data.csv"), "height", 3)
+        result = make_data("test_data/test_format_data.csv", "height", 3)
         self.assertEqual(len(result.feats), 1)
         self.assertEqual(len(result.targs), 1)
 
