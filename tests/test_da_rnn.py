7cf30290451e38a0d7fe26e42e21d67d0a7f3244
Isaac Godfried <igodfried@isaac26.com>
2019-09-02 21:55:09 -0400

    fix config

--- tests/test_da_rnn.py
+++ tests/test_da_rnn.py
@@ -7,7 +7,7 @@ import os
 import pandas as pd
 class TestPreprocessingDA(unittest.TestCase):
     def setUp(self):
-        self.preprocessed_data = self.preprocessed_data = make_data("test_data/keag_small.csv", ["cfs"], 72)
+        self.preprocessed_data = self.preprocessed_data = make_data(os.path.join(os.path.dirname(__file__), "test_data", "keag_small.csv"), ["cfs"], 72)
 
     def test_train_model(self):
         config, da_network = da_rnn(self.preprocessed_data, 1, 64)
