d9f13b9448502d0c35ee94c39865a91dcb4b0a1c
isaacmg <igodfried@isaac26.com>
2019-10-23 20:11:05 -0400

    Create closest_station.py

---
+++ flood_forecast/preprocessing/closest_station.py
@@ -0,0 +1,18 @@
+import math
+import pandas as pd
+def haversine(coord1, coord2):
+    R = 6372800  # Earth radius in meters
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+    
+    phi1, phi2 = math.radians(lat1), math.radians(lat2) 
+    dphi       = math.radians(lat2 - lat1)
+    dlambda    = math.radians(lon2 - lon1)
+    
+    a = math.sin(dphi/2)**2 + \
+        math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
+    
+    return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    
+def get_gage_coordinates(gage_csv):
+   pd = pd.read_csv(gage_csv_path)
