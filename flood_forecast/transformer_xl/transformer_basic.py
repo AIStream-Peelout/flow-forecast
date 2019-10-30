40b0119becd15f2766955d815500d0195d3f532e
isaacmg <igodfried@isaac26.com>
2019-10-30 16:49:21 -0400

    Added basic transformer outline

---
+++ flood_forecast/transformer_xl/transformer_basic.py
@@ -0,0 +1,7 @@
+from torch import nn
+from flood_forecast.time_model import TimeSeriesModel
+class TransformerModel(TimeSeriesModel):
+   def __init__(self, init_params, d_model, n_heads=8, layers=6):
+    pass 
+   
+
