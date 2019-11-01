790aa2267cadff15d5e9f0d38bdff4ab9fd9b775
isaacmg <igodfried@isaac26.com>
2019-11-01 13:15:16 -0400

    add basic function to find station

--- flood_forecast/preprocessing/closest_station.py
+++ flood_forecast/preprocessing/closest_station.py
@@ -1,18 +1,21 @@
-import math
+from math import radians, cos, sin, asin, sqrt
 import pandas as pd
-def haversine(coord1, coord2):
-    R = 6372800  # Earth radius in meters
-    lat1, lon1 = coord1
-    lat2, lon2 = coord2
-    
-    phi1, phi2 = math.radians(lat1), math.radians(lat2) 
-    dphi       = math.radians(lat2 - lat1)
-    dlambda    = math.radians(lon2 - lon1)
-    
-    a = math.sin(dphi/2)**2 + \
-        math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
-    
-    return 2*R*math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    
-def get_gage_coordinates(gage_csv):
-   pd = pd.read_csv(gage_csv_path)
+import os
+import json
+
+def get_closest_gage(gage_df:pd.DataFrame, station_df:pd.DataFrame, path_dir:str):
+  for row in gage_df.iterrows():
+      gage_info = {}
+      gage_info["river_id"] = row[1]['id']
+      gage_lat = row[1]['latitude']
+      gage_long = row[1]['logitude']
+      gage_info["stations"] = []
+      min_distance = 4000000
+
+      for stat_row in station_df.iterrows():
+            dist = haversine(stat_row[1]["lon"], stat_row[1]["lat"], gage_long, gage_lat)
+            st_id = stat_row[1]['stid']
+            gage_info["stations"].append({"station_id":st_id, "dist":dist})
+      gage_info["stations"] = sorted(gage_info['stations'], key = lambda i: i["dist"], reverse=True) 
+      with open(os.path.join(path_dir, str(gage_info["river_id"]) + "stations.json"), 'w') as w:
+        json.dump(gage_info, w)
