be3f123880ee4977de99e7afdf46984f33b6ab1a
isaacmg <igodfried@isaac26.com>
2019-11-05 00:22:01 -0500

    Update function to reflect boo

--- flood_forecast/preprocessing/closest_station.py
+++ flood_forecast/preprocessing/closest_station.py
@@ -3,19 +3,22 @@ import pandas as pd
 import os
 import json
 
-def get_closest_gage(gage_df:pd.DataFrame, station_df:pd.DataFrame, path_dir:str):
-  for row in gage_df.iterrows():
-      gage_info = {}
-      gage_info["river_id"] = row[1]['id']
-      gage_lat = row[1]['latitude']
-      gage_long = row[1]['logitude']
-      gage_info["stations"] = []
-      min_distance = 4000000
-
-      for stat_row in station_df.iterrows():
-            dist = haversine(stat_row[1]["lon"], stat_row[1]["lat"], gage_long, gage_lat)
-            st_id = stat_row[1]['stid']
-            gage_info["stations"].append({"station_id":st_id, "dist":dist})
-      gage_info["stations"] = sorted(gage_info['stations'], key = lambda i: i["dist"], reverse=True) 
-      with open(os.path.join(path_dir, str(gage_info["river_id"]) + "stations.json"), 'w') as w:
-        json.dump(gage_info, w)
+def get_closest_gage(gage_df:pd.DataFrame, station_df:pd.DataFrame, path_dir:str, start_row:int, end_row:int):
+  for row in range(start_row, end_row):
+    gage_info = {}
+    gage_info["river_id"] = int(gage_df.iloc[row]['id'])
+    gage_lat = gage_df.iloc[row]['latitude']
+    gage_long = gage_df.iloc[row]['logitude']
+    gage_info["stations"] = []
+    total = len(station_df.index)
+    for i in range(0, total):
+      stat_row = station_df.iloc[i]
+      dist = haversine(stat_row["lon"], stat_row["lat"], gage_long, gage_lat)
+      st_id = stat_row['stid']
+      gage_info["stations"].append({"station_id":st_id, "dist":dist})
+    gage_info["stations"] = sorted(gage_info['stations'], key = lambda i: i["dist"], reverse=True) 
+    with open(os.path.join(path_dir, str(gage_info["river_id"]) + "stations.json"), 'w') as w:
+      json.dump(gage_info, w)
+      if count%100 == 0:
+        print("Currently at " + str(count))
+      count +=1 
