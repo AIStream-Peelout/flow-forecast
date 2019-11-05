953dc3b61010db7a0db45b037fc172f40bc61757
isaacmg <igodfried@isaac26.com>
2019-11-05 00:25:49 -0500

    add model stubs

--- flood_forecast/time_model.py
+++ flood_forecast/time_model.py
@@ -2,4 +2,10 @@ class TimeSeriesModel(object):
     def __init__(self, model_base, training_data, test_data):
         pass 
     def train():
+        pass
+    
+class PyTorchForecast(TimeSeriesModel):
+     def __init__(self, model_base, training_data, test_data):
         pass 
+    def train():
+        pass
