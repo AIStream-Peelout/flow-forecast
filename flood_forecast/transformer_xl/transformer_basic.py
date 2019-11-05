31d08bd3233516c58fe1dcb23f597c534d254780
isaacmg <igodfried@isaac26.com>
2019-11-05 16:59:31 -0500

    change import stub

--- flood_forecast/transformer_xl/transformer_basic.py
+++ flood_forecast/transformer_xl/transformer_basic.py
@@ -1,5 +1,5 @@
 from torch import nn
-from flood_forecast.time_model import TimeSeriesModel
+from flood_forecast.time_model import PyTorchForecast
 class TransformerModel(TimeSeriesTorch):
    def __init__(self, init_params, d_model, n_heads=8, layers=6):
       super().__init__(init_params)
