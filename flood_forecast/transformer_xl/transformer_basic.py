2d42541f0c7d2d4db4adf368626f2b86d3e773a1
isaacmg <igodfried@isaac26.com>
2019-11-07 11:32:46 -0500

    add trans

--- flood_forecast/transformer_xl/transformer_basic.py
+++ flood_forecast/transformer_xl/transformer_basic.py
@@ -1,7 +1,42 @@
 from torch import nn
 from flood_forecast.time_model import PyTorchForecast
-class TransformerModel(TimeSeriesTorch):
-   def __init__(self, init_params, d_model, n_heads=8, layers=6):
-      super().__init__(init_params)
+import torch 
+import math
+from torch.nn.modules import Transformer 
+class SimpleTransformer(PyTorchForecast):
+    def __init__(self, param_dict, n_time_series, d_model=128, n_heads=6):
+        super().__init__(param_dict)
+        self.dense_shape = torch.nn.Linear(n_time_series, d_model)
+        self.pe = SimplePositionalEncoding(d_model)
+        self.transformer = Transformer(d_model, nhead=8)
+        self.final_layer = torch.nn.Linear(d_model, 1)
+    def forward(self, x, t, tgt_mask):
+        x = self.dense_shape(x)
+        x = self.pe(x)
+        t = self.dense_shape(t)
+        t = self.pe(t)
+        x = x.permute(1,0,2)
+        t = t.permute(1,0,2)
+        x = self.transformer(x, t, src_mask=tgt_mask, tgt_mask=tgt_mask)
+        print(torch.isnan(x))
+        x = self.final_layer(x)
+        return x
+        
+class SimplePositionalEncoding(torch.nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(SimplePositionalEncoding, self).__init__()
+        self.dropout = torch.nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
    
 
