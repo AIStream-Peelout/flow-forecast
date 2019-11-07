845f6705438cc683ec9f70218d187a578c0c59b1
isaacmg <igodfried@isaac26.com>
2019-11-07 14:00:00 -0500

    combine

--- flood_forecast/time_model.py
+++ flood_forecast/time_model.py
@@ -1,11 +1,16 @@
 class TimeSeriesModel(object):
     def __init__(self, model_base, training_data, test_data):
-        pass 
+        self.training_data_path =   
     def train():
         pass
     
 class PyTorchForecast(TimeSeriesModel):
-     def __init__(self, model_base, training_data, test_data):
-        pass 
+    def __init__(self, model_base, training_data, test_data, params_dict, weight_path=None):
+        super().__init__(model_base, training_data, test_data)
+       if self.weight_path is not None:
+          # TODO implement weight loading
+          pass 
+       
+        
     def train():
         pass
