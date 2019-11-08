180ea824d2663c1ae1120ee3be382cfa6af7bff1
isaacmg <igodfried@isaac26.com>
2019-11-08 13:10:10 -0500

    add new functions

--- flood_forecast/preprocessing/closest_station.py
+++ flood_forecast/preprocessing/closest_station.py
@@ -2,8 +2,11 @@ from math import radians, cos, sin, asin, sqrt
 import pandas as pd
 import os
 import json
+from typing import Set 
+import requests
 
 def get_closest_gage(gage_df:pd.DataFrame, station_df:pd.DataFrame, path_dir:str, start_row:int, end_row:int):
+  # Function that calculates the closest weather stations to gage and stores in JSON
   for row in range(start_row, end_row):
     gage_info = {}
     gage_info["river_id"] = int(gage_df.iloc[row]['id'])
@@ -22,3 +25,39 @@ def get_closest_gage(gage_df:pd.DataFrame, station_df:pd.DataFrame, path_dir:str
       if count%100 == 0:
         print("Currently at " + str(count))
       count +=1 
+      
+def get_weather_data(file_path:str, econet_gages:Set, base_url:str):
+  """
+  Function that retrieves if station has weather 
+  data for a specific gage either from ASOS or ECONet 
+  """
+  gage_meta_info = {}
+  
+  with open(file_path) as f:
+    gage_data = json.load(f)
+  gage_meta_info["gage_id"] = gage_data["river_id"]
+  gage_meta_info["stations"] = []
+  closest_stations = gage_data["stations"][-20:]
+  for station in reversed(closest_stations):
+    url = base_url.format(station["station_id"])
+    response = requests.get(url)
+    if len(response.text)>100:
+      print(response.text)
+      gage_meta_info["stations"].append({"station_id":station["station_id"], 
+                                         "dist":station["dist"], "cat":"ASOS"})
+    elif station["station_id"] in econet_gages:
+      gage_meta_info["stations"].append({"station_id":station["station_id"], 
+                                         "dist":station["dist"], "cat":"ECO"})
+  return gage_meta_info
+
+def process_asos_data(file_path, base_url):
+  """
+  Function that saves the ASOS data to a CSV file
+  """
+  with open(file_path) as f:
+    gage_data = json.load(f)
+  for station in gage_data["stations"]:
+    if station["cat"] = "ASOS":
+      response = requests.get(base_url)
+      df = pd.read_csv(response.text)
+      df.to_csv(gage_data["gage_id"] + "_" + gage_data["station_id"]+".csv")
