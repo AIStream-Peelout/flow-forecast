15024fdfaf1be117eaec5ae412db7af9b1b433fe
isaacmg <igodfried@isaac26.com>
2019-11-08 00:18:18 -0500

    add basic multihued

---
+++ flood_forecast/transformer_xl/multi_head_base.py
@@ -0,0 +1,17 @@
+from torch.nn.modules.activation import MultiheadAttention
+class MultiAttnHeadSimple(torch.nn.Module):
+    def __init__(self, n_time_series, d_model=128):
+        super().__init__()
+        self.dense_shape = torch.nn.Linear(n_time_series, d_model)
+        self.pe = SimplePositionalEncoding(d_model)
+        self.multi_attn = MultiheadAttention(embed_dim=d_model, num_heads=8, dropout=0.1)
+        self.final_layer = torch.nn.Linear(d_model, 1)
+    def forward(self, x, mask=None):
+        x = self.dense_shape(x)
+        x = self.pe(x)
+        x = x.permute(1,0,2)
+        if mask is None:
+            x = self.multi_attn(x, x, x)[0]
+        else: 
+            x = self.multi_attn(x, x, x, attn_mask=mask)[0]
+        x = self.final_layer(x)
