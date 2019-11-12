5d1d419fcc9a5792d89e372e534eb8c4182067ce
isaacmg <igodfried@isaac26.com>
2019-11-12 10:41:39 -0500

    update loading

--- flood_forecast/time_model.py
+++ flood_forecast/time_model.py
@@ -3,7 +3,9 @@ from abc import ABC
 class TimeSeriesModel(ABC):
     def __init__(self, model_base, training_data, validation_data, test_data):
         self.model = load_model()
-        self.training_data_path =   
+        self.training_data_path = training_data 
+        self.validation_data_path = validation_path
+        self.test_data = test_data
         
     @abstractmethod
     def load_model(model_path:str) -> object:
@@ -19,8 +21,6 @@ class PyTorchForecast(TimeSeriesModel):
        if self.weight_path is not None:
           # TODO implement weight loading
           pass 
-       
-       
         
     def train():
         pass
