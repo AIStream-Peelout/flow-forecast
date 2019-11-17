f7998034c264267f05432a9851797847518f6f8b
Isaac Godfried <igodfried@isaac26.com>
2019-11-17 11:03:45 -0500

    run save weig

--- tests/test_da_rnn.py
+++ tests/test_da_rnn.py
@@ -31,6 +31,7 @@ class TestDARNN(unittest.TestCase):
         """ This test is dependent on test_train_model succeding"""
         config, da = da_rnn(self.preprocessed_data, 1, 64)
         torch.save(da.encoder.state_dict(), os.path.join("checkpoint", "encoder.pth"))
+        print("Run save weight")
         config, dnn_network = da_rnn(self.preprocessed_data, 1, 64, save_path="checkpoint")
         self.assertTrue(config)
         #self.assertTrue(dnn_network)
