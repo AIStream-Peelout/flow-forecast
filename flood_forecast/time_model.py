be058ec9d5bbc565ac0e3f95db26148e604d264e
isaacmg <igodfried@isaac26.com>
2019-11-19 18:48:59 -0500

    model init

--- flood_forecast/time_model.py
+++ flood_forecast/time_model.py
@@ -7,11 +7,11 @@ class TimeSeriesModel(ABC):
     This class assumes that data is already split into test train 
     and validation at this point.
     """
-    def __init__(self, model_base, training_data, validation_data, test_data, params):
+    def __init__(self, model_base:str, training_data:str, validation_data:str, test_data:str, params:Dict):
         self.model = load_model(model_base, params)
         self.training = make_data_load(training_data, params)
         self.validation = make_data_load(validation_path, params)
-        self.test_loader = make_data_load(test_data, params)
+        self.test_data = make_data_load(test_data, params)
         
     @abstractmethod
     def load_model(self, model_base, model_params) -> object:
@@ -41,14 +41,20 @@ class TimeSeriesModel(ABC):
         
     
 class PyTorchForecast(TimeSeriesModel):
-    def __init__(self, model_base, training_data, test_data, params_dict, weight_path=None):
-        super().__init__(model_base, training_data, test_data)
+    def __init__(self, model_base, training_data, validation_data, test_data, params_dict, weight_path=None):
+        super().__init__(model_base, training_data, validation_data, test_data, params_dict)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-       if self.weight_path is not None:
-            model.load_state_dict(torch.load(weight_path, map_location=self.device)
-                                  
+        if weight_path is not None:
+            model.load_state_dict(torch.load(weight_path, map_location=self.device))
+            
     def load_model(self, model_base, model_params:Dict):
-        pass
-                                  
-    def train():
-        pass
+        model_base_to_class = {"PyTorchBasic":PyTorchBasic, "PyTorchTransformer":SimplePositionalEncoding} # Define all PyTorch models here
+        # Todo fix params for model (i.e params)...
+        model = model_base_to_class[model_base]
+        return model
+    
+    def save_model():
+        pass 
+    
+    def make_data_load():
+        pass 
