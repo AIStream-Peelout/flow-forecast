94c20361e303e1828aa44d3ebf11cadb022f8e0d
Isaac Godfried <igodfried@isaac26.com>
2019-11-20 23:55:07 -0500

    add appropiate paths

--- tests/data_format_tests.py
+++ tests/data_format_tests.py
@@ -1,8 +1,12 @@
 from flood_forecast.preprocessing.closest_station import get_weather_data, get_closest_gage, format_dt, convert_temp, process_asos_csv
 from datetime import datetime
 import unittest
+import os 
 
 class DataQualityTests(unittest.TestCase):
+    def __init__(self):
+        self.test_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_data")
+
     def test_format_dt(self):
         self.assertEqual(format_dt("2017-04-07 08:55"), datetime(year=2017, month=4, day=7, hour=9))
         self.assertEqual(format_dt("2018-04-08 23:55"), datetime(year=2018, month=4, day=9, hour=0)) 
@@ -13,7 +17,7 @@ class DataQualityTests(unittest.TestCase):
         self.assertEqual(convert_temp("M"), 50)
 
     def test_process_asos_csv(self):
-        df, precip_missing, temp_missing = process_asos_csv("small_test.csv")
+        df, precip_missing, temp_missing = process_asos_csv(os.path.join( self.test_data_path,"small_test.csv"))
         self.assertEqual(df.illoc[0]['p01m'], 92)
         self.assertEqual(df.illoc[0]['tmpf'], 52.5)
         self.assertEqual(df.illoc[0]['hour_updated'].hour, 1)
@@ -21,10 +25,10 @@ class DataQualityTests(unittest.TestCase):
         self.assertEqual(temp_missing, 0)
     
     def test_process_asos_full(self):
-        df, precip_missing, temp_missing = process_asos_csv("asos_raw.csv")
+        df, precip_missing, temp_missing = process_asos_csv(os.path.join( self.test_data_path, "asos_raw.csv"))
         self.assertGreater(temp_missing,10)
         self.assertEqual(1,2)
-        
+
 if __name__ == '__main__':
     unittest.main()
     
