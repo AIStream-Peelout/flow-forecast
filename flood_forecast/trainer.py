08acf9fcca2224c0d986a6d849bc67602a3f4b83
isaacmg <igodfried@isaac26.com>
2019-11-26 00:12:57 -0500

    update trainer to work with training config

--- flood_forecast/trainer.py
+++ flood_forecast/trainer.py
@@ -27,27 +27,11 @@ def train_function(model:str, training_file_dir:str, test_hours:int, target_col:
 
 def main():
     parser = argparse.ArgumentParser(description="Argument parsing for training and eval")
-    parser.add_argument("-d", "--dataset", help="Path to river flow csv file")
-    parser.add_argument("-m", "--model", help="Model you want to use for training")
-    parser.add_argument("-t", "--test", default=336, help="The number of hours to forecast for the test")
-    parser.add_argument("-b", "--task", help="The task you want to train the model for")
-    parser.add_argument("-c", "--column", default="cfs", help="The target column either height, cfs or both")
-    parser.add_argument("-r", "--resume", default=None, help="Resume from a checkpoint")
-    parser.add_argument("-s", "--max_epochs", default="10")
-    parser.add_argument("--tensorboard", default="False")
-    parser.add_argument("--wandb", default=False, help="Use weights and biases for enhanced logging" )
-    parser.add_argument("--gpu", default=False, help="If using GPU pass true")
+    parser.add_argument("-t", "--train_config", help="Path to model config file")
     args = parser.parse_args()
-    if args.wandb:
-        import wandb
-        wandb = True
-    else: 
-        wandb = False
-    if args.column == "both":
-        args.column = ['cfs', 'height']
-    else: 
-        args.column = [args.column]
-    train_function(args.model, args.dataset, args.test, args.column, wandb)
+    with open(args.c) as f: 
+      training_config = json.loads(f)
+    trained_model = train_function(training_config["model"])
 if __name__ == "__main__":
     main()
 
