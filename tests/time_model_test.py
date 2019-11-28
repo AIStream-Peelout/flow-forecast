06065bc8f7a17534251aa3810ef0953ad0141fd5
Isaac Godfried <igodfried@isaac26.com>
2019-11-27 21:06:05 -0500

    remove circular ref

--- tests/time_model_test.py
+++ tests/time_model_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("..")
-from flood_forecast.model_dict_function import pytorch_model_dict
+from flood_forecast.model_dict_function import pytorch_model_dict as pytorch_model_dict1
 from flood_forecast.time_model import PyTorchForecast
 import unittest
 import os
@@ -10,7 +10,7 @@ class TimeSeriesModelTest(unittest.TestCase):
         self.test_path = ""
 
     def test_pytorch_model_dict(self):
-        self.assertEqual(type(pytorch_model_dict), dict)
+        self.assertEqual(type(pytorch_model_dict1), dict)
 
     def test_pytorch_wrapper_default(self):
         model_params = {"number_time_series":3}
@@ -18,7 +18,10 @@ class TimeSeriesModelTest(unittest.TestCase):
         self.assertEqual(model.model.dense_shape.in_features, 3)
         self.assertEqual(model.model.multi_attn.embed_dim, 128)
 
-
-
+    def test_pytorch_wrapper_custom(self):
+        model_params = {"number_time_series":6, "d_model":112}
+        model = PyTorchForecast("MultiAttnHeadSimple", "", "", "", model_params)
+        self.assertEqual(model.model.dense_shape.in_features, 6)
+        self.assertEqual(model.model.multi_attn.embed_dim, 112)
 if __name__ == '__main__':
     unittest.main()
