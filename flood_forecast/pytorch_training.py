00690440c41c05bc03d410f3262525c86aae92b9
Isaac Godfried <igodfried@isaac26.com>
2019-12-02 22:14:19 -0500

    Begin long model refactoring

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -3,14 +3,19 @@ import torch
 import torch.optim as optim
 from torch.autograd import Variable
 import numpy as np
-from typing import Type
+from typing import Type, Dict
 from torch.nn.modules.loss import _Loss
 from flood_forecast.time_model import PyTorchForecast
-
-def train_transformer_style(model: PyTorchForecast , max_epochs: int, criterion: Type[torch.nn._Loss], use_wandb = False):
+from flood_forecast.model_dict_function import pytorch_opt, pytorch_criterion
+def train_transformer_style(model: PyTorchForecast, training_params:Dict, use_wandb = False):
   """
   Function to train any PyTorchForecast model  
+  :model The initialized
+  :training_params_dict
   """
+  opt = pytorch_opt(training_params["optimizer"])(model.model.parameters())
+  criterion = pytorch_criterion[training_params["criterion"]]
+  max_epochs = training_params["epochs"]
   #criterion = torch.nn.MSELoss()
   #optimizer = torch.optim.Adam(a.parameters())
   if use_wandb:
@@ -20,19 +25,23 @@ def train_transformer_style(model: PyTorchForecast , max_epochs: int, criterion:
       i = 0
       running_loss = 0.0
       for src, trg in data_loader:
-          mask = generate_square_subsequent_mask(sequence_size)
-          optimizer.zero_grad()
+          forward_params = {}
+          if model.model.mask: 
+              mask = generate_square_subsequent_mask(sequence_size)
+              forward_params["mask"] = mask
+          opt.zero_grad()
           #print(src)
-          output = a(src.float(), mask)
+          output = model.model(src, **forward_params)
           #output = s(src.float(), trg.float(), mask)
           labels = trg[:, :, 0] 
-          loss = criterion(output.view(-1, sequence_size), labels.float())
+          # TODO loss should be handled in model itseflf no need o view in output 
+          loss = criterion(output, labels.float())
           if loss > 10:
               print(src)
           #print(loss)
           loss.backward()
           #torch.nn.utils.clip_grad_norm_(s.parameters(), 0.5)
-          optimizer.step()
+          opt.step()
           running_loss += loss.item()
           i+=1
           if torch.isnan(loss) or loss==float('inf'):
