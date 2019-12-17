72ff72d986014ecace947a4a98d446ef2d73ebdd
isaacmg <igodfried@isaac26.com>
2019-12-17 02:08:55 -0500

    fix unit test add decoder style

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -1,3 +1,6 @@
+import unittest
+from flood_forecast.transformer_xl.transformer_basic import SimpleTransformer, greedy_decode
+
 class TestDecoding(unittest.TestCase):
     def setUp(self):
         self.model = SimpleTransformer(3, 48)
@@ -6,6 +9,7 @@ class TestDecoding(unittest.TestCase):
            pin_memory=False, drop_last=False, timeout=0,
            worker_init_fn=None)
         self.sequence_size = 48
+
     def test_for_leakage(self):
         src_mask = generate_square_subsequent_mask(sequence_size)
         src, trg = next(iter(validation_data_loader))
@@ -16,5 +20,6 @@ class TestDecoding(unittest.TestCase):
         self.assertNotEqual(result[0,2,0], trg_mem[0,1,0])
         self.assertEqual(result[0,2,1], trg_mem[0, 1, 1])
         self.assertEqual(result[0,2,2], trg_mem[0, 1, 2])
+        
     def test_make_function(self):
         self.assertEqual(1,1)
