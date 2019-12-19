55102c89a9b097a0e7ffe7daea4fa2f46b8c823e
isaacmg <igodfried@isaac26.com>
2019-12-18 22:30:37 -0500

    add wandb param

--- tests/pytorc_train_tests.py
+++ tests/pytorc_train_tests.py
@@ -10,7 +10,8 @@ class PyTorchTrainTests(unittest.TestCase):
         self.test_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_init")
         self.model_params = {"model_params":{"number_time_series":3, "seq_len":20}, 
         "dataset_params":{"history": 20, "class":"default", "forecast_length":20, "relevant_cols":["cfs", "temp", "precip"], "target_col":["cfs"]},
-        "training_params": {"optimizer":"Adam", "lr":.1, "criterion": "MSE", "epochs":1, "batch_size":2}}
+        "training_params": {"optimizer":"Adam", "lr":.1, "criterion": "MSE", "epochs":1, "batch_size":2},
+                            "wandb":"False"}
         self.keag_file = os.path.join(self.test_path, "keag_small.csv")
         self.model = PyTorchForecast("MultiAttnHeadSimple", self.keag_file, self.keag_file, self.keag_file, self.model_params)
 
