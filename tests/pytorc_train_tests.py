4f73ffdbb4dc618f87debf798f57a338cdb316b4
Isaac Godfried <igodfried@isaac26.com>
2019-12-23 00:37:47 -0500

    update name, no Wandb for basic

--- tests/pytorc_train_tests.py
+++ tests/pytorc_train_tests.py
@@ -11,7 +11,7 @@ class PyTorchTrainTests(unittest.TestCase):
         self.model_params = {"model_params":{"number_time_series":3, "seq_len":20}, 
         "dataset_params":{"history": 20, "class":"default", "forecast_length":20, "relevant_cols":["cfs", "temp", "precip"], "target_col":["cfs"]},
         "training_params": {"optimizer":"Adam", "lr":.1, "criterion": "MSE", "epochs":1, "batch_size":2},
-                            "wandb":"False"}
+                            "wandb":False}
         self.keag_file = os.path.join(self.test_path, "keag_small.csv")
         self.model = PyTorchForecast("MultiAttnHeadSimple", self.keag_file, self.keag_file, self.keag_file, self.model_params)
 
