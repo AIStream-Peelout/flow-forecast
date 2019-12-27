0cdaf6f32889c5539b2e17f49a84b9763e92aee0
Isaac Godfried <igodfried@isaac26.com>
2019-12-27 12:31:25 -0500

    Fix tz stuff

--- tests/test_join.py
+++ tests/test_join.py
@@ -3,12 +3,18 @@ from flood_forecast.preprocessing.buil_dataset import combine_data
 import pandas as pd
 import os
 import json
+from datetime import datetime
+import pytz 
 class JoinTest(unittest.TestCase):
     def setUp(self):
         self.test_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"test_data")
     def test_join_function(self):
         df = pd.read_csv(os.path.join(self.test_data_path, "big_black_test_small.csv"), sep="\t")
         asos_df = pd.read_csv(os.path.join(self.test_data_path, "asos-12N_small.csv"))
+        old_timezone = pytz.timezone("America/New_York")
+        new_timezone = pytz.timezone("UTC")
+        # This assumes timezones are consistent throughout the USGS stream (this should be true)
+        df["datetime"] = df["datetime"].map(lambda x: old_timezone.localize(datetime.strptime(x, "%Y-%m-%d %H:%M")).astimezone(new_timezone))
         with open(os.path.join(self.test_data_path, "big_black_md.json")) as a:
             meta_data = json.load(a)
         result_df, nan_f, nan_p = combine_data(df, asos_df)
