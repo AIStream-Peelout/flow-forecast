fix indent and los calculation
Horrible bug that went unnoticed for a long time. Need better unit tests. WIll think

flood_forecast/pytorch_training.py
@@ -69,6 +69,10 @@ def compute_validation(validation_loader, model, epoch, sequence_size, criterion
   model.eval()
   mask = generate_square_subsequent_mask(sequence_size)
   loop_loss = 0.0
+  # Log some of the validation tensor for analysis 
+  last_src = None
+  last_targ = None 
+  last_output = None
   with torch.no_grad():
     i = 0
     for src, targ in validation_loader:
@@ -79,18 +83,19 @@ def compute_validation(validation_loader, model, epoch, sequence_size, criterion
 
       # To do implement greedy decoding
       # https://github.com/budzianowski/PyTorch-Beam-Search-Decoding/blob/master/decode_beam.py
-    else:
-      output = model(src.float(), mask)
-      labels = targ[:, :, 0]
-    loss_unscaled_full = None
-    validation_dataset = validation_loader.dataset
-    if validation_dataset.scale :
-      unscaled_out = validation_dataset.inverse_scale(output)
-      unscaled_labels = validation_dataset.inverse_scale(labels)
-      loss_unscaled = criterion(unscaled_out, unscaled_labels.float())
-      loss_unscaled_full = len(labels.float())*loss_unscaled.item()
-    loss = criterion(output, labels.float())
-    loop_loss += len(labels.float())*loss.item()
+      else:
+        output = model(src.float(), mask)
+        labels = targ[:, :, 0]
+      loss_unscaled_full = None
+      validation_dataset = validation_loader.dataset
+      if validation_dataset.scale :
+        unscaled_src = validation_dataset.scale.inverse_scale(src)
+        unscaled_out = validation_dataset.inverse_scale(output)
+        unscaled_labels = validation_dataset.inverse_scale(labels)
+        loss_unscaled = criterion(unscaled_out, unscaled_labels.float())
+        loss_unscaled_full = len(labels.float())*loss_unscaled.item()
+      loss = criterion(output, labels.float())
+      loop_loss += len(labels.float())*loss.item()
   if use_wandb:
     import wandb
     if loss_unscaled_full: