265d09e75e8cb002416121a9689ef37614595b44
Isaac Godfried <igodfried@isaac26.com>
2020-01-10 14:15:20 -0500

    fix unit test for dattime

--- tests/data_loader_tests.py
+++ tests/data_loader_tests.py
@@ -22,7 +22,7 @@ class DataLoaderTests(unittest.TestCase):
         src, df, forecast_start_index, = self.test_loader.get_from_start_date(datetime(2014, 6, 3, 0))
         self.assertEqual(type(src), torch.Tensor)
         self.assertEqual(forecast_start_index, 783)
-        self.assertEqual(df.iloc[0]['datetime'], '2014-06-02 4:00:00')
+        self.assertEqual(df.iloc[0]['datetime'].day, datetime(2014, 6, 2, 4).day)
 
 if __name__ == '__main__':
     unittest.main()
