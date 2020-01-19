e707a4047eb58e8ca787595e875f6c29d11b77b9
Isaac Godfried <igodfried@isaac26.com>
2020-01-19 18:28:50 -0500

    print head

--- flood_forecast/preprocessing/pytorch_loaders.py
+++ flood_forecast/preprocessing/pytorch_loaders.py
@@ -87,7 +87,7 @@ class CSVTestLoader(CSVDataLoader):
     def get_from_start_date(self, forecast_start):
         dt_row = self.original_df[self.original_df['datetime'] == forecast_start]
         print("DT row below")
-        print(dt_row)
+        print(self.original_df.head())
         revised_index = dt_row.index[0]
         return self.__getitem__(revised_index-self.forecast_history)
 
