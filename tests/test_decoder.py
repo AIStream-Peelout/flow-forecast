4f88a3caae95f5160ec30a70f81e896b8d81c709
isaacmg <igodfried@isaac26.com>
2020-01-24 11:53:57 -0500

    Decoder fixes

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import torch
 from flood_forecast.transformer_xl.transformer_basic import SimpleTransformer, greedy_decode, generate_square_subsequent_mask
 
 class TestDecoding(unittest.TestCase):
@@ -11,17 +12,29 @@ class TestDecoding(unittest.TestCase):
            pin_memory=False, drop_last=False, timeout=0,
            worker_init_fn=None)
         self.sequence_size = 48
+    
+    def test_full_forward_method(self):
+        test_data = torch.rand(1, 48, 3)
+        result = self.model(test_data)
+        self.assertEqual(result.shape, torch.Size([1, 48]))
+    
+    def test_encoder_seq(self):
+        test_data = torch.rand(1, 3, 48)
+        result = self.model.encode_sequence(test_data)
+        self.assertEqual(result.shape, torch.Size([1, 48, 128]))
 
     def test_for_leakage(self):
-        src_mask = generate_square_subsequent_mask(sequence_size)
+        """
+        Simple test to check that raw target data does NOT
+        leak during validation.
+        """
+        src_mask = generate_square_subsequent_mask(self.sequence_size)
         src, trg = next(iter(self.validation_data_loader))
         trg_mem = trg.clone().detach()
-        print(trg_mem[0,1,0])
         result = greedy_decode(self.model, src, src_mask, self.sequence_size, trg, src)
-        print(trg_mem[0,1,0])
-        self.assertNotEqual(result[0,2,0], trg_mem[0,1,0])
-        self.assertEqual(result[0,2,1], trg_mem[0, 1, 1])
-        self.assertEqual(result[0,2,2], trg_mem[0, 1, 2])
+        self.assertNotEqual(result[0, 2, 0], trg_mem[0, 1, 0])
+        self.assertEqual(result[0, 2, 1], trg_mem[0, 1, 1])
+        self.assertEqual(result[0, 2, 2], trg_mem[0, 1, 2])
         
     def test_make_function(self):
         self.assertEqual(1,1)
