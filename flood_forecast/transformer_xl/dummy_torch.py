20920c94bd14fd648a15a4deac51cd67dd44c990
Isaac Godfried <igodfried@isaac26.com>
2020-02-02 23:16:54 -0500

    Add a dummy model

---
+++ flood_forecast/transformer_xl/dummy_torch.py
@@ -0,0 +1,14 @@
+"""
+A dummy model specifically for unit and integration testing purposes
+"""
+import torch
+from torch import nn
+from typing import Optional, Dict, List
+class DummyTorchModel(nn.Module):
+    def __init__(self, forecast_length:int):
+        self.out_len = forecast_length
+
+    def forward(self, x:torch.Tensor):
+        batch_sz = x.size(0)
+        result = torch.ones(batch_sz, self.out_len)
+        return result
