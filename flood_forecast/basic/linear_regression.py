161864c48b8bcc01fb294b306f41ffd4bed33926
Isaac Godfried <igodfried@isaac26.com>
2020-02-29 21:51:06 -0500

    add baseline and

--- flood_forecast/basic/linear_regression.py
+++ flood_forecast/basic/linear_regression.py
@@ -5,15 +5,16 @@ class SimpleLinearModel(torch.nn.Module):
     difficulties with bugs in the various train/validation loops 
     in code.
     """
-    def __init__(self, forecast_history:int, n_time_series:int):
+    def __init__(self, seq_length:int, n_time_series:int, output_seq_len=1):
         super.__init__()
-        self.forecast_history = forecast_history
+        self.forecast_history = seq_length
         self.n_time_series = n_time_series
         self.initial_layer = torch.nn.Linear(n_time_series, 1)
-        self.output_layer = torch.nn.Linear(forecast_history, 1)
+        self.output_layer = torch.nn.Linear(seq_length, output_seq_len)
+        self.output_len = output_seq_len
 
-    def forward(self, x:torch.Tensor):
+    def forward(self, x:torch.Tensor): 
         x = self.initial_layer(x)
         x = x.permute(0,2,1)
-        x= self.output_layer(x)
-        return x 
+        x = self.output_layer(x)
+        return x.view(-1, self.output_len)
