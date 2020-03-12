a10dbbd4a0df0865f37f6ebe00b5727bc5038050
isaacmg <igodfried@isaac26.com>
2020-03-11 22:34:56 -0400

    add type hint and unit stuff

--- flood_forecast/basic/linear_regression.py
+++ flood_forecast/basic/linear_regression.py
@@ -1,12 +1,13 @@
 import torch
 from typing import Type
+
 class SimpleLinearModel(torch.nn.Module):
     """
     A very simple baseline model to resolve some of the
     difficulties with bugs in the various train/validation loops 
     in code.
     """
-    def __init__(self, seq_length:int, n_time_series: int, output_seq_len=1):
+    def __init__(self, seq_length: int, n_time_series: int, output_seq_len=1):
         super().__init__()
         self.forecast_history = seq_length
         self.n_time_series = n_time_series
@@ -14,7 +15,7 @@ class SimpleLinearModel(torch.nn.Module):
         self.output_layer = torch.nn.Linear(seq_length, output_seq_len)
         self.output_len = output_seq_len
 
-    def forward(self, x: torch.Tensor):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         x: A tensor of dimension (B, L, M) where 
         B is the batch size, L is the length of the 
@@ -24,7 +25,7 @@ class SimpleLinearModel(torch.nn.Module):
         x = self.output_layer(x)
         return x.view(-1, self.output_len)
 
-def simple_decode(model: Type[torch.nn.Module], src: torch.Tensor, max_seq_len: int, real_target:torch.Tensor, start_symbol=None, output_len=1, device='cpu', unsqueeze_dim=1):
+def simple_decode(model: Type[torch.nn.Module], src: torch.Tensor, max_seq_len: int, real_target:torch.Tensor, start_symbol=None, output_len=1, device='cpu', unsqueeze_dim=1) -> torch.Tensor:
     """
     :model a PyTorch model to be used for decoding
     :src the source tensor
@@ -33,6 +34,7 @@ def simple_decode(model: Type[torch.nn.Module], src: torch.Tensor, max_seq_len: 
     :start_symbol used to match the function signature of greedy_decode not ever used here though. 
     :output_len potentially used to forecast multiple steps at once. Not implemented yet though.
     :device used to to match function signature 
+    :returns a torch.Tensor of dimension (B, L, M)
     """
     real_target2 = real_target.clone()
     ys = src[:, -1, :].unsqueeze(unsqueeze_dim)
@@ -44,6 +46,5 @@ def simple_decode(model: Type[torch.nn.Module], src: torch.Tensor, max_seq_len: 
             ys = torch.cat((ys, real_target2[:, i, :].unsqueeze(1)), 1 )
     return ys[:, 1:, :]
 
-
-
-
+def linear_dummy():
+    pass
--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -37,9 +37,9 @@ class EvaluationTest(unittest.TestCase):
         self.assertGreater(model_result["MSE"], 1)
 
     def test_linear_decoder(self):
-        decoder_params = {"decoder_function": "simple_decode", "decoder_function_params":{"unsqueeze_dim":1}} 
+        decoder_params = {"decoder_function": "simple_decode", "decoder_function_params": {"unsqueeze_dim": 1}} 
         self.data_base_params["forecast_history"] = 100
-        inference_params = {"datetime_start": datetime.datetime(2016, 5, 31, 0), "hours_to_forecast": 336 , "dataset_params": self.data_base_params, 
+        inference_params = {"datetime_start": datetime.datetime(2016, 5, 31, 0), "hours_to_forecast": 336, "dataset_params": self.data_base_params, 
         "test_csv_path": os.path.join(self.test_path2, "keag_small.csv"), "decoder_params": decoder_params}
         infer_on_torch_model(self.linear_model, **inference_params)
 
