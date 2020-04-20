1dbdf308ff5d7132d3d6eae57d83b4fba14069c1
Isaac Godfried <igodfried@isaac26.com>
2020-04-19 23:34:42 -0400

    re put stuff

--- flood_forecast/da_rnn/model.py
+++ flood_forecast/da_rnn/model.py
@@ -6,13 +6,13 @@ from flood_forecast.da_rnn.modules import Encoder, Decoder
 
 class DARNN(nn.Module):
 
-    def __init__(self, input_size: int, hidden_size: int, T: int, decoder_hidden_size: int, out_feats=1):
+    def __init__(self, input_size: int, hidden_size_encoder: int, T: int, decoder_hidden_size: int, out_feats=1):
         """
         input size: number of underlying factors (81)
         T: number of time steps (10)
         hidden_size: dimension of the hidden state
         """
-        pass
+    
 
     def forward(self, x):
         """will implement"""
