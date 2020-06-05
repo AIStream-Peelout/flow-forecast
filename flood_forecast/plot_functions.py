8c0e337925b8b8f5c8bffac93181dff77a0feed4
michaelwang1994 <hecheng.wang@gmail.com>
2020-06-04 21:10:00 -0500

    plot_functions.py: * make sure that df_preds does not go beyond CI * make forecast start index taller * correct overlapping CI test_plot.py: * test new functionality of plot_functions.py

--- flood_forecast/plot_functions.py
+++ flood_forecast/plot_functions.py
@@ -3,12 +3,14 @@ import pandas as pd
 from typing import Dict
 
 
-def calculate_confidence_intervals(df: pd.DataFrame, ci_lower: float, ci_upper) -> pd.DataFrame:
+def calculate_confidence_intervals(df: pd.DataFrame, df_preds: pd.Series, ci_lower: float, ci_upper) -> pd.DataFrame:
     assert 0.0 <= ci_lower <= 0.5
     assert 0.5 <= ci_upper <= 1.0
     assert ci_lower != ci_upper
-    return df.quantile(q=[ci_lower, ci_upper], axis=1).T
-
+    df_quantiles = df.quantile(q=[ci_lower, ci_upper], axis=1).T
+    df_quantiles.loc[df_quantiles[ci_lower] > df_preds, ci_lower] = df_preds
+    df_quantiles.loc[df_quantiles[ci_upper] < df_preds, ci_upper] = df_preds
+    return df_quantiles
 
 def plot_df_test_with_confidence_interval(
         df_test: pd.DataFrame,
@@ -24,16 +26,20 @@ def plot_df_test_with_confidence_interval(
     target_col = params["dataset_params"]["target_col"][0]
     fig.add_trace(go.Scatter(x=df_test.index, y=df_test['preds'], name='preds'))
     fig.add_trace(go.Scatter(x=df_test.index, y=df_test[target_col], name=target_col))
-    fig.add_trace(go.Scatter(
-        x=[forecast_start_index, forecast_start_index],
-        y=[df_test['preds'].min(), df_test['preds'].max()],
-        name='pred_start'))
     ci_lower, ci_upper = ((100.0 - ci) / 2.0) / 100.0, ((100.0 - ci) / 2.0 + ci) / 100.0
-    df_quantiles = calculate_confidence_intervals(df_prediction_samples, ci_lower, ci_upper)
+    df_quantiles = calculate_confidence_intervals(df_prediction_samples, df_test['preds'], ci_lower, ci_upper)
+
     print("plotting with CI now")
     fig.add_trace(go.Scatter(
         x=df_quantiles.index.tolist() + df_quantiles.index.tolist()[::-1],
-        y=df_quantiles[ci_lower].tolist() + df_quantiles[ci_upper].tolist(),
+        y=df_quantiles[ci_lower].tolist() + df_quantiles[ci_upper].tolist()[::-1],
         fill='toself',
         name=f'{int(ci)}% confidence interval'))
+    fig.add_trace(go.Scatter(
+        x=[forecast_start_index, forecast_start_index],
+        y=[
+            min(df_quantiles[ci_lower].min(), df_test[target_col].min()),
+            max(df_quantiles[ci_lower].max(), df_test[target_col].max())
+        ],
+        name='pred_start'))
     return fig
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -6,25 +6,27 @@ from flood_forecast.plot_functions import calculate_confidence_intervals, plot_d
 
 class PlotFunctionsTest(unittest.TestCase):
     df_test = pd.DataFrame({
-        'preds': [0.0, 1.0, 2.0, 3.0],
-        'target_col': [4.0, 5.0, 6.0, 7.0]
+        'preds': [0.0, 1.0, 2.0, 3.0, 4.0, 5.0],
+        'target_col': [4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
     })
     df_preds = pd.DataFrame({
-        0: [-1.0, -2.0, -1.0, 0.0],
-        1: [1.0, 2.0, 4.0, 3.0]
+        0: [-1.0, -2.0, -1.0, 0.0, -1.0, 6.0],
+        1: [1.0, 2.0, 4.0, 3.0, 2.0, 9.0]
     })
-    df_preds_empty = pd.DataFrame(index=[0, 1, 2, 3])
+    df_preds_empty = pd.DataFrame(index=[0, 1, 2, 3, 4, 5])
 
     def test_calculate_confidence_intervals(self):
         ci_lower, ci_upper = 0.025, 0.975
-        df_quantiles = calculate_confidence_intervals(self.df_preds, ci_lower, ci_upper)
+        df_quantiles = calculate_confidence_intervals(self.df_preds, self.df_test['preds'], ci_lower, ci_upper)
         df_preds_mean = self.df_preds.mean(axis=1)
         self.assertTrue((df_quantiles[ci_lower] < df_preds_mean).all())
         self.assertTrue((df_quantiles[ci_upper] > df_preds_mean).all())
+        self.assertTrue((df_quantiles[ci_lower] <= self.df_test['preds']).all())
+        self.assertTrue((df_quantiles[ci_upper] >= self.df_test['preds']).all())
 
     def test_calculate_confidence_intervals_df_preds_empty(self):
         ci_lower, ci_upper = 0.025, 0.975
-        df_quantiles = calculate_confidence_intervals(self.df_preds_empty, ci_lower, ci_upper)
+        df_quantiles = calculate_confidence_intervals(self.df_preds_empty, self.df_test['preds'], ci_lower, ci_upper)
         self.assertTrue(df_quantiles[ci_lower].isna().all())
         self.assertTrue(df_quantiles[ci_upper].isna().all())
 
