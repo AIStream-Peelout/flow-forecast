6a422d09fd6449e027f9539f6e5b65e0dbe8bb87
isaacmg <igodfried@isaac26.com>
2020-06-12 00:30:08 -0400

    begin fixes

--- flood_forecast/transformer_xl/xc_attn.py
+++ flood_forecast/transformer_xl/xc_attn.py
@@ -1,19 +1,18 @@
 import torch
-class TransformerModel(torch.nn.Module):
+class TransformerXCBasic(torch.nn.Module):
     """ Transformer model """
-    def __init__(self,args,input_dim, n_head, seq_num, layer, n_embd, win_len, device):
+    def __init__(self, seq_len, n_time_series, out_seg_len, device, d_model=128, dropout=.5, n_head=8):
         super(TransformerModel, self).__init__()
         self.input_dim = input_dim
         self.n_head = n_head
         self.seq_num = seq_num
         self.n_embd = n_embd
         self.win_len = win_len
-        self.dataset = args.dataset
         self.id_embed = torch.nn.Embedding(seq_num,n_embd)
         self.po_embed = torch.nn.Embedding(win_len,n_embd)
         self.drop_em = torch.nn.Dropout(args.embd_pdrop)
         self.device = device
-        self.xc_encoder = TransformerEncoderLayer()
+        self.xc_encoder = TransformerEncoderLayer(d_model,n_head)
         torch.nn.init.normal_(self.id_embed.weight, std=0.02)
         torch.nn.init.normal_(self.po_embed.weight, std=0.02)
 
@@ -27,5 +26,6 @@ class TransformerModel(torch.nn.Module):
         embedding_sum[:] = po_embedding
         embedding_sum = embedding_sum + id_embedding.unsqueeze(1)
         x = torch.cat((x,embedding_sum),dim=2)
-        
+        print(x.shape)
         return x
+
