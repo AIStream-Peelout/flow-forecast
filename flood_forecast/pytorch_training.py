2a24eefaac09abaf3fdb07274915c9834e6f5fcd
Isaac Godfried <igodfried@isaac26.com>
2020-06-15 11:50:17 -0400

    fix metrics for future

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -1,85 +1,142 @@
-import argparse
-from typing import Sequence, List, Tuple, Dict
-import json
-import plotly.graph_objects as go
-import wandb
-from flood_forecast.pytorch_training import train_transformer_style
+import torch
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+from typing import Type, Dict
+from torch.nn.modules.loss import _Loss
+from torch.utils.data import DataLoader
 from flood_forecast.time_model import PyTorchForecast
-from flood_forecast.evaluator import evaluate_model
-from flood_forecast.pre_dict import scaler_dict
-from flood_forecast.plot_functions import plot_df_test_with_confidence_interval
+from flood_forecast.model_dict_function import pytorch_opt_dict, pytorch_criterion_dict
+from flood_forecast.model_dict_function import generate_square_subsequent_mask
+from flood_forecast.transformer_xl.transformer_basic import greedy_decode
+from flood_forecast.basic.linear_regression import simple_decode
+from flood_forecast.training_utils import EarlyStopper
 
-def train_function(model_type: str, params:Dict):
-    """
-    Function to train a Model(TimeSeriesModel) or da_rnn. Will return the trained model
-    model_type str: Type of the model (for now) must be da_rnn or 
-    :params dict: Dictionary containing all the parameters needed to run the model
-    """
-    dataset_params = params["dataset_params"]
-    if model_type == "da_rnn":
-        from flood_forecast.da_rnn.train_da import da_rnn, train
-        from flood_forecast.preprocessing.preprocess_da_rnn import make_data
-        preprocessed_data = make_data(params["dataset_params"]["training_path"], params["dataset_params"]["target_col"], params["dataset_params"]["forecast_length"])
-        config, model = da_rnn(preprocessed_data, len(dataset_params["target_col"]))
-        # All train functions return trained_model
-        trained_model = train(model, preprocessed_data, config)
-    elif model_type == "PyTorch":
-        trained_model = PyTorchForecast(
-            params["model_name"],
-            dataset_params["training_path"],
-            dataset_params["validation_path"],
-            dataset_params["test_path"],
-            params)
-        train_transformer_style(trained_model, params["training_params"], params["forward_params"])
-        params["inference_params"]["dataset_params"]["scaling"] = scaler_dict[dataset_params["scaler"]]
-        test_acc = evaluate_model(
-            trained_model,
-            model_type,
-            params["dataset_params"]["target_col"],
-            params["metrics"],
-            params["inference_params"],
-            {})
-        wandb.run.summary["test_accuracy"] = test_acc[0]
-        df_train_and_test = test_acc[1]
-        forecast_start_idx = test_acc[2]
-        df_prediction_samples = test_acc[3]
-        inverse_mae = 1 / (
-                df_train_and_test.loc[forecast_start_idx:, "preds"] -
-                df_train_and_test.loc[forecast_start_idx:, params["dataset_params"]["target_col"][0]]).abs()
-        pred_std = df_prediction_samples.std(axis=1)
-        average_prediction_sharpe = (inverse_mae / pred_std).mean()
-        wandb.log({'average_prediction_sharpe': average_prediction_sharpe})
+def train_transformer_style(model: PyTorchForecast, training_params: Dict, takes_target=False, forward_params:Dict = {})->None:
+  """
+  Function to train any PyTorchForecast model  
+  :model The initialized PyTorchForecastModel
+  :training_params_dict A dictionary of the parameters needed to train model
+  :takes_target boolean: Determines whether to pass target during training
+  :forward_params: A dictionary for additional forward parameters (for instance target)
+  """
+  use_wandb = model.wandb
+  es = None
+  if "early_stopping" in model.params:
+    es = EarlyStopper(model.params["early_stopping"]['patience'])
+  opt = pytorch_opt_dict[training_params["optimizer"]](model.model.parameters(), **training_params["optim_params"])
+  criterion = pytorch_criterion_dict[training_params["criterion"]]
+  max_epochs = training_params["epochs"]
+  data_loader = DataLoader(model.training, batch_size=training_params["batch_size"], shuffle=False, sampler=None,
+           batch_sampler=None, num_workers=0, collate_fn=None,
+           pin_memory=False, drop_last=False, timeout=0,
+           worker_init_fn=None)
+  validation_data_loader = DataLoader(model.validation, batch_size=training_params["batch_size"], shuffle=False, sampler=None,
+           batch_sampler=None, num_workers=0, collate_fn=None,
+           pin_memory=False, drop_last=False, timeout=0,
+           worker_init_fn=None)
+  test_data_loader = DataLoader(model.test_data, batch_size=1, shuffle=False, sampler=None,
+           batch_sampler=None, num_workers=0, collate_fn=None,
+           pin_memory=False, drop_last=False, timeout=0,
+           worker_init_fn=None)
+  if use_wandb:
+    import wandb
+    wandb.watch(model.model)
+  session_params = []
+  for epoch in range(max_epochs):
+      total_loss = torch_single_train(model, opt, criterion, data_loader, takes_target, forward_params)
+      print("The loss for epoch " + str(epoch))
+      print(total_loss)
+      use_decoder = False
+      if "use_decoder" in model.params:
+        use_decoder = True
+      valid = compute_validation(validation_data_loader, model.model, epoch, model.params["dataset_params"]["forecast_length"], criterion, model.device, decoder_structure=use_decoder, use_wandb=use_wandb)
+      if valid <0.01:
+        raise("Error validation loss is zero there is a problem with the validator.")
+      if use_wandb:
+        wandb.log({'epoch': epoch, 'loss': total_loss})
+      epoch_params = {"epoch":epoch, "train_loss":str(total_loss), "validation_loss":str(valid)} 
+      session_params.append(epoch_params)
+      if es:
+        if not es.check_loss(model.model, valid):
+          print("Stopping model now")
+          model.model.load_state_dict(torch.load("checkpoint.pth"))
+          break
+  test = compute_validation(test_data_loader, model.model, epoch, model.params["dataset_params"]["forecast_length"], criterion, model.device, decoder_structure=True, use_wandb=use_wandb, val_or_test="test_loss")
+  model.params["run"] = session_params
+  model.save_model("model_save", max_epochs)
+  
+def torch_single_train(model:PyTorchForecast, opt:optim.Optimizer, criterion:Type[torch.nn.modules.loss._Loss], data_loader:DataLoader, takes_target:bool, forward_params:Dict={})->float:
+  i = 0
+  running_loss = 0.0
+  for src, trg in data_loader:
+    opt.zero_grad()
+    # Convert to CPU/GPU/TPU 
+    src = src.to(model.device)
+    trg = trg.to(model.device)
+    # TODO figure how to avoid
+    if takes_target:
+      forward_params["t"] = trg 
+    output = model.model(src, **forward_params)
+    labels = trg[:, :, 0] 
+    loss = criterion(output, labels.float())
+    if loss > 100:
+      print("Warning: high loss detected")
+    loss.backward()
+    opt.step()
+    if torch.isnan(loss) or loss==float('inf'):
+        raise("Error infinite or NaN loss detected. Try normalizing data or performing interpolation")
+    running_loss += loss.item()
+    i+=1
+  print("The running loss is:")
+  print(running_loss)
+  print("The number of items in train is: ")
+  print(i)
+  total_loss = running_loss/float(i)
+  return total_loss
 
-        # Log plots
-        test_plot = plot_df_test_with_confidence_interval(
-            df_train_and_test,
-            df_prediction_samples,
-            forecast_start_idx,
-            params,
-            ci=95,
-            alpha=0.25)
-        wandb.log({"test_plot": test_plot})
-
-        test_plot_all = go.Figure()
-        for relevant_col in params["dataset_params"]["relevant_cols"]:
-            test_plot_all.add_trace(go.Scatter(x=df_train_and_test.index, y=df_train_and_test[relevant_col], name=relevant_col))
-        wandb.log({"test_plot_all": test_plot_all})
+def compute_validation(validation_loader:DataLoader, model,  epoch:int, sequence_size:int, criterion:Type[torch.nn.modules.loss._Loss], 
+                          device:torch.device, decoder_structure=False, use_wandb:bool=False, val_or_test="validation_loss")->float:
+  """
+  Function to compute the validation or test loss 
+  """
+  model.eval()
+  loop_loss = 0.0
+  with torch.no_grad():
+    i = 0
+    loss_unscaled_full = 0.0
+    for src, targ in validation_loader:
+      src = src.to(device)
+      targ = targ.to(device)
+      i+=1
+      if decoder_structure:
+        if type(model).__name__ == "SimpleTransformer":
+          targ_clone = targ.detach().clone()
+          output = greedy_decode(model, src, targ.shape[1], targ_clone, device=device)[:, :, 0]
+        else:
+          output = simple_decode(model, src, targ.shape[1], targ, 1)[:, :, 0]
+      else:
+        output = model(src.float())
+      labels = targ[:, :, 0]
+      validation_dataset = validation_loader.dataset
+      if validation_dataset.scale:
+        #unscaled_src = validation_dataset.scale.inverse_transform(src.cpu())
+        unscaled_out = validation_dataset.inverse_scale(output.cpu())
+        unscaled_labels = validation_dataset.inverse_scale(labels.cpu())
+        loss_unscaled = criterion(unscaled_out, unscaled_labels.float())
+        loss_unscaled_full += len(labels.float())*loss_unscaled.item()
+        if i%10 ==0 and use_wandb:
+          import wandb
+          wandb.log({"trg":unscaled_labels, "model_pred":unscaled_out})
+      loss = criterion(output, labels.float()) 
+      loop_loss += len(labels.float())*loss.item()
+  if use_wandb:
+    import wandb
+    if loss_unscaled_full:
+      tot_unscaled_loss = loss_unscaled_full/(len(validation_loader.dataset)-1)
+      wandb.log({'epoch': epoch, val_or_test: loop_loss/(len(validation_loader.dataset)-1), "unscaled_" + val_or_test: tot_unscaled_loss})
     else:
-        raise Exception("Please supply valid model type for forecasting")
-    return trained_model
-
-def main():
-    """
-    Main function which is called from the command line. Entrypoint for all ML models.
-    """
-    parser = argparse.ArgumentParser(description="Argument parsing for training and eval")
-    parser.add_argument("-p", "--params", help="Path to model config file")
-    args = parser.parse_args()
-    with open(args.params) as f: 
-        training_config = json.load(f)
-    train_function(training_config["model_type"], training_config)
-    # evaluate_model(trained_model)
-    print("Process is now complete.")
-    
-if __name__ == "__main__":
-    main()
+      wandb.log({'epoch': epoch, val_or_test: loop_loss/(len(validation_loader.dataset)-1)})
+  model.train()
+  return loop_loss/(len(validation_loader.dataset)-1)
+  
--- flood_forecast/trainer.py
+++ flood_forecast/trainer.py
@@ -40,21 +40,21 @@ def train_function(model_type: str, params:Dict):
             params["inference_params"],
             {})
         wandb.run.summary["test_accuracy"] = test_acc[0]
-        df_test = test_acc[1]
-        forecast_start_index = test_acc[2]
-        df_preds = test_acc[3]
+        df_train_and_test = test_acc[1]
+        forecast_start_idx = test_acc[2]
+        df_prediction_samples = test_acc[3]
         inverse_mae = 1 / (
-                df_test.loc[forecast_start_index:, "preds"] -
-                df_test.loc[forecast_start_index:, params["dataset_params"]["target_col"][0]]).abs()
-        pred_std = df_preds.std(axis=1)
+                df_train_and_test.loc[forecast_start_idx:, "preds"] -
+                df_train_and_test.loc[forecast_start_idx:, params["dataset_params"]["target_col"][0]]).abs()
+        pred_std = df_prediction_samples.std(axis=1)
         average_prediction_sharpe = (inverse_mae / pred_std).mean()
         wandb.log({'average_prediction_sharpe': average_prediction_sharpe})
 
         # Log plots
         test_plot = plot_df_test_with_confidence_interval(
-            df_test,
-            df_preds,
-            forecast_start_index,
+            df_train_and_test,
+            df_prediction_samples,
+            forecast_start_idx,
             params,
             ci=95,
             alpha=0.25)
@@ -62,7 +62,7 @@ def train_function(model_type: str, params:Dict):
 
         test_plot_all = go.Figure()
         for relevant_col in params["dataset_params"]["relevant_cols"]:
-            test_plot_all.add_trace(go.Scatter(x=df_test.index, y=df_test[relevant_col], name=relevant_col))
+            test_plot_all.add_trace(go.Scatter(x=df_train_and_test.index, y=df_train_and_test[relevant_col], name=relevant_col))
         wandb.log({"test_plot_all": test_plot_all})
     else:
         raise Exception("Please supply valid model type for forecasting")
@@ -83,5 +83,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-
-# Example command python flood_forecast/trainer.py -t sample_config.json
