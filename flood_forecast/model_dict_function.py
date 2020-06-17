d9aa91906479fdcd341f6a81781a981c28024822
isaacmg <igodfried@isaac26.com>
2020-06-17 15:36:19 -0400

    Add RMSE

--- flood_forecast/model_dict_function.py
+++ flood_forecast/model_dict_function.py
@@ -9,6 +9,10 @@ from torch.nn import MSELoss, SmoothL1Loss, PoissonNLLLoss
 from flood_forecast.custom.custom_opt import BertAdam
 from flood_forecast.basic.linear_regression import simple_decode
 from flood_forecast.transformer_xl.transformer_basic import greedy_decode
+from flood_forecast.custom.custom_opt import RMSELoss, MAPELoss
+# criterion_params
+# { "quantile:""
+#  }
 import torch
 
 """
@@ -18,7 +22,7 @@ pytorch_model_dict = {"MultiAttnHeadSimple": MultiAttnHeadSimple, "SimpleTransfo
 "TransformerXL": TransformerXL, "DummyTorchModel": DummyTorchModel,  "LSTM":LSTMForecast, "SimpleLinearModel": SimpleLinearModel, 
 "CustomTransformerDecoder":CustomTransformerDecoder}
 
-pytorch_criterion_dict = {"MSE": MSELoss(), "SmoothL1Loss":SmoothL1Loss(), "PoissonNLLLoss":PoissonNLLLoss()}
+pytorch_criterion_dict = {"MSE": MSELoss(), "SmoothL1Loss":SmoothL1Loss(), "PoissonNLLLoss": PoissonNLLLoss(), "RMSELoss": RMSELoss(), "MAPELoss": MAPELoss()}
 
 evaluation_functions_dict = {"NSE": "", "MSE": ""}
 
