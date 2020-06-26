61f0fd7844075d2b47574cf952f273f24cc4e3d4
kritim13 <kriti.mahajan.13@gmail.com>
2020-06-26 18:24:48 +0530

    'docs'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
-
+master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
