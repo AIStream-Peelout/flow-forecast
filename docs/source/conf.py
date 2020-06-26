02eed8462ec41bc5c1656e171f971f28972750b1
kritim13 <kriti.mahajan.13@gmail.com>
2020-06-26 23:56:31 +0530

    'docs'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,8 @@ import sys
 import re
 
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..\..'))
+#sys.path.insert(0, os.path.abspath('..\..'))
+sys.path.insert(0, os.path.abspath('..\..\src'))
 
 # -- Project information -----------------------------------------------------
 
