5bda5e101c353584b3a817bcce3a77eea6cdab20
kritim13 <kriti.mahajan.13@gmail.com>
2020-06-26 23:12:43 +0530

    'docs'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 import re
 
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..\..\src'))
+sys.path.insert(0, os.path.abspath('..\..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -70,7 +70,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []#'_static'
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
@@ -84,5 +84,5 @@ autoclass_content = 'both'
 if os.environ.get('READTHEDOCS', None):
     tags.add('readthedocs')
 
-if 'READTHEDOCS' not in os.environ:
-    import cython_generated_ext
+#if 'READTHEDOCS' not in os.environ:
+#    import cython_generated_ext
