233f722cf864d34d2640d6f506a8d9d5f67ea961
isaacmg <igodfried@isaac26.com>
2020-07-08 20:09:56 -0400

    flake8 and functions etc

--- flood_forecast/gcp_integration/basic_utils.py
+++ flood_forecast/gcp_integration/basic_utils.py
@@ -26,11 +26,22 @@ def upload_file(bucket_name: str, file_name: str, upload_name: str, client: stor
 
 def download_file(bucket_name, file_name, client, destination_file_name):
     bucket = client.get_bucket(bucket_name)
-    #Create a blob object from the filepath
+    # Create a blob object from the filepath
     blob = bucket.blob(file_name)
     # Download the file to a destination
     blob.download_to_filename(destination_file_name)
     print("File sucessfully downloaded")
+
+
+def file_path_local_or_not(client, file_path: str):
+    if "gs://" in file_path:
+        print("Detected GCS URL will download file")
+        file_path_split = file_path.split("gs://")[1].split("/")
+        bucket_name = file_path_split[0]
+        return download_file(bucket_name, "".join(file_path_split[1]), client, file_path_split[:-1])
+    else:
+        return file_path
+
 
 def load_dataverse_file():
     pass
