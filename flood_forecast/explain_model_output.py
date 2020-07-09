2b6dc39bde41b2a498f1ef3043569e7d20038815
isaacmg <igodfried@isaac26.com>
2020-07-09 13:05:06 -0400

    Revert "Merge branch 'revert-76-add_heatmap' into add_heatmap"
    This reverts commit 36cd83a8e8ad5766d0fc42b2e7593311bc18d240, reversing
    changes made to c2bfa6f04d67c5c2d5d02ee0ef370e317f533132.

--- flood_forecast/explain_model_output.py
+++ flood_forecast/explain_model_output.py
@@ -2,171 +2,167 @@ import random
 from datetime import datetime
 from typing import Optional
 import numpy as np
-from typing import Dict
 import shap
 import torch
-from flood_forecast.preprocessing.pytorch_loaders import CSVTestLoader
-from datetime import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-import random
+
 import wandb
+from flood_forecast.plot_functions import (
+    plot_shap_value_heatmaps,
+    plot_shap_values_from_history,
+    plot_summary_shap_values,
+    plot_summary_shap_values_over_time_series,
+)
+from flood_forecast.preprocessing.pytorch_loaders import CSVTestLoader
+
+BACKGROUND_BATCH_SIZE = 5
 
 
-BACKGROUND_SAMPLE_SIZE = 5
+def _prepare_background_tensor(
+    csv_test_loader: CSVTestLoader, backgound_batch_size: int = BACKGROUND_BATCH_SIZE
+) -> torch.Tensor:
+    """Generate background batches for deep explainer.
+    Random sample batches as background data
+    background tensor of size (batch_size, history_len, num_feature)
+
+    Args:
+        csv_test_loader (CSVTestLoader): test data loader
+        backgound_batch_size (int): number of batches used as background data
+        for deep explainer. Default to BACKGROUND_BATCH_SIZE.
+
+    Returns:
+        torch.Tensor: background tensor of size
+        (batch_size, history_len, num_feature)
+    """
+    background_data = csv_test_loader.original_df
+    background_batches = csv_test_loader.convert_history_batches(
+        csv_test_loader.df.columns, background_data
+    )
+    # remove last batch in the list because it may not be of
+    # size (history_len, num_feature) due to length of original df
+    background_tensor = torch.stack(
+        random.sample(background_batches[:-1], backgound_batch_size)
+    ).float()
+    return background_tensor
 
 
 def deep_explain_model_summary_plot(
-    model,
-    datetime_start: datetime = datetime(2014, 6, 1, 0),
-    test_csv_path: str = None,
-    forecast_total: int = 336,
-    dataset_params: Dict = {},
-):
+    model, csv_test_loader: CSVTestLoader, datetime_start: Optional[datetime] = None
+) -> None:
+    """Generate feature summary plot for trained deep learning models
+
+    Args:
+        model (object): trained model
+        csv_test_loader (CSVTestLoader): test data loader
+        datetime_start (datetime, optional): start date of the test prediction,
+            Defaults to None, i.e. using model inference parameters.
+    """
+    if model.params["model_name"] == "SimpleTransformer":
+        print("SimpleTransformer currently not supported.")
+        return
+    use_wandb = model.wandb
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    num_features = len(model.params["dataset_params"]["relevant_cols"])
-    # If the test dataframe is none use default one supplied in params
-    if test_csv_path is None:
-        csv_test_loader = model.test_data
-    else:
-        csv_test_loader = CSVTestLoader(
-            test_csv_path,
-            forecast_total,
-            **dataset_params,
-            interpolate=dataset_params["interpolate_param"],
-        )
-    background_data = csv_test_loader.original_df
-    background_batches = csv_test_loader.convert_real_batches(
-        csv_test_loader.df.columns, background_data
-    )
-    background_tensor = (
-        torch.stack(random.sample(background_batches, BACKGROUND_SAMPLE_SIZE))
-        .float()
-        .to(device)
-    )
+    if datetime_start is None:
+        datetime_start = model.params["inference_params"]["datetime_start"]
+
+    history, _, forecast_start_idx = csv_test_loader.get_from_start_date(datetime_start)
+    background_tensor = _prepare_background_tensor(csv_test_loader)
+    background_tensor = background_tensor.to(device)
     model.model.eval()
 
     # background shape (L, N, M)
     # L - batch size, N - history length, M - feature size
     deep_explainer = shap.DeepExplainer(model.model, background_tensor)
     shap_values = deep_explainer.shap_values(background_tensor)
+    shap_values = np.stack(shap_values)
+    # shap_values needs to be 4-dimensional
+    if len(shap_values.shape) != 4:
+        shap_values = np.expand_dims(shap_values, axis=0)
+    shap_values = torch.tensor(shap_values, names=['preds', 'batches', 'observations', 'features'])
 
     # summary plot shows overall feature ranking
     # by average absolute shap values
-    mean_shap_values = np.concatenate(shap_values).mean(axis=0)
-    plt.figure(figsize=(12, 8))
-    shap.summary_plot(
-        mean_shap_values,
-        feature_names=csv_test_loader.df.columns,
-        plot_type="bar",
-        show=False,
-        max_display=10,
-        plot_size=(10, num_features * 2),
-    )
-    plt.savefig("overall_feature_ranking_by_shap_values.png")
-    wandb.log(
-        {
-            "Overall feature ranking by shap values": wandb.Image(
-                "overall_feature_ranking_by_shap_values.png"
-            )
-        }
-    )
-    plt.close()
+    fig = plot_summary_shap_values(shap_values, csv_test_loader.df.columns)
+    if use_wandb:
+        wandb.log({"Overall feature ranking by shap values": fig})
 
     # summary plot for multi-step outputs
-    multi_shap_values = list(np.stack(shap_values).mean(axis=1))
-    shap.summary_plot(
-        multi_shap_values,
-        feature_names=csv_test_loader.df.columns,
-        class_names=[
-            f"time-step-{t}" for t in range(model.model.forecast_length)
-        ],
-        max_display=10,  # max number of features to display
-        show=False,
-        plot_size=(10, num_features * 2),
-        sort=False,
+    # multi_shap_values = shap_values.apply_along_axis(np.mean, 'batches')
+    fig = plot_summary_shap_values_over_time_series(
+        shap_values, csv_test_loader.df.columns
     )
-    plt.savefig("overall_feature_rank_per_time_step.png")
-    wandb.log(
-        {
-            "Overall feature ranking per prediction time-step": wandb.Image(
-                "overall_feature_rank_per_time_step.png"
-            )
-        }
-    )
-    plt.close()
+    if use_wandb:
+        wandb.log({"Overall feature ranking per prediction time-step": fig})
 
     # summary plot for one prediction at datetime_start
-    history, _, forecast_start_idx = csv_test_loader.get_from_start_date(
-        datetime_start
+
+    history = history.to(device).unsqueeze(0)
+    history_numpy = torch.tensor(history.cpu().numpy(), names=['batches', 'observations', 'features'])
+
+    shap_values = deep_explainer.shap_values(history)
+    shap_values = np.stack(shap_values)
+    if len(shap_values.shape) != 4:
+        shap_values = np.expand_dims(shap_values, axis=0)
+    shap_values = torch.tensor(shap_values, names=['preds', 'batches', 'observations', 'features'])
+
+    fig = plot_shap_values_from_history(
+        shap_values, history_numpy, csv_test_loader.df.columns
     )
+    if use_wandb:
+        wandb.log({"Feature ranking for prediction" f" at {datetime_start}": fig})
+
+
+def deep_explain_model_heatmap(
+    model, csv_test_loader: CSVTestLoader, datetime_start: Optional[datetime] = None
+) -> None:
+    """Generate feature heatmap for prediction at a start time
+
+    Args:
+        model ([type]): trained model
+        csv_test_loader ([CSVTestLoader]): test data loader
+        datetime_start (Optional[datetime], optional): start date of the test prediction,
+            Defaults to None, i.e. using model inference parameters.
+
+    Returns:
+        None
+    """
+    if model.params["model_name"] == "SimpleTransformer":
+        print("SimpleTransformer currently not supported.")
+        return
+    use_wandb = model.wandb
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if datetime_start is None:
+        datetime_start = model.params["inference_params"]["datetime_start"]
+
+    history, _, forecast_start_idx = csv_test_loader.get_from_start_date(datetime_start)
+    background_tensor = _prepare_background_tensor(csv_test_loader)
+    background_tensor = background_tensor.to(device)
+    model.model.eval()
+
+    # background shape (L, N, M)
+    # L - batch size, N - history length, M - feature size
+    # for each element in each N x M batch in L,
+    # attribute to each prediction in forecast len
+    deep_explainer = shap.DeepExplainer(model.model, background_tensor)
+    shap_values = deep_explainer.shap_values(
+        background_tensor
+    )  # forecast_len x N x L x M
+    shap_values = np.stack(shap_values)
+    if len(shap_values.shape) != 4:
+        shap_values = np.expand_dims(shap_values, axis=0)
+    shap_values = torch.tensor(shap_values, names=['preds', 'batches', 'observations', 'features'])
+    fig = plot_shap_value_heatmaps(shap_values, csv_test_loader.df.columns)
+    if use_wandb:
+        wandb.log({"Average prediction heatmaps": fig})
+
+    # heatmap one prediction sequence at datetime_start
+    # (seq_len*forecast_len) per fop feature
     to_explain = history.to(device).unsqueeze(0)
     shap_values = deep_explainer.shap_values(to_explain)
-    mean_shap_values = np.concatenate(shap_values).mean(axis=0)
-    shap.summary_plot(
-        mean_shap_values,
-        history.cpu().numpy(),
-        feature_names=csv_test_loader.df.columns,
-        plot_type="dot",
-        max_display=10,
-        plot_size=(9, num_features * 2),
-        show=False,
-    )
-    plt.savefig("feature_ranking_for_prediction_at_timestamp.png")
-    wandb.log(
-        {
-            "Feature ranking for prediction at "
-            f"{datetime_start.strftime('%Y-%m-%d')}": wandb.Image(
-                "feature_ranking_for_prediction_at_timestamp.png"
-            )
-        }
-    )
-    plt.close()
+    shap_values = np.stack(shap_values)
+    if len(shap_values.shape) != 4:
+        shap_values = np.expand_dims(shap_values, axis=0)
+    shap_values = torch.tensor(shap_values, names=['preds', 'batches', 'observations', 'features'])
 
-
-def deep_explain_model_sample():
-    pass
-    # TODO
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # num_features = len(model.params['dataset_params']['relevant_cols'])
-
-    # if type(datetime_start) == str:
-    #     datetime_start = datetime.strptime(datetime_start, "%Y-%m-%d")
-    # # If the test dataframe is none use default one supplied in params
-    # if test_csv_path is None:
-    #     csv_test_loader = model.test_data
-    # else:
-    #     csv_test_loader = CSVTestLoader(
-    #         test_csv_path,
-    #         hours_to_forecast,
-    #         **dataset_params,
-    #         interpolate=dataset_params["interpolate_param"]
-    #     )
-    # # history, forecast_total_df, forecast_start_idx = csv_test_loader.get_from_start_date(datetime_start)
-    # background_data = csv_test_loader.original_df
-    # background_batches = csv_test_loader.convert_real_batches(
-    #     csv_test_loader.df.columns, background_data
-    #     )
-    # # TODO - better stratagy to choose background data
-    # background_tensor = torch.stack(background_batches[:-1]).float().to(device)
-    # model.model.eval()
-    # force plot for a simgle sample (in matplotlib)
-    # shap.force_plot(
-    #     deep_explainer.expected_value[0],
-    #     shap_values[0].reshape(-1, 3)[6, :],
-    #     show=True,
-    #     feature_names=csv_test_loader.df.columns,
-    #     matplotlib=True,
-    # )
-    # # force plot for multiple time-steps
-    # # can only be generated as html objects
-    # # shap.force_plot(e.expected_value[0], shap_values[0].reshape(-1, 3), show=True,
-    # feature_names=csv_test_loader.df.columns)
-    # # dependece plot shows feature value vs shap value
-    # shap.dependence_plot(
-    #     2,
-    #     shap_values[0].reshape(-1, 3),
-    #     to_explain.cpu().numpy().reshape(-1, 3),
-    #     interaction_index=0,
-    #     feature_names=csv_test_loader.df.columns,
-    # )
+    fig = plot_shap_value_heatmaps(shap_values, csv_test_loader.df.columns)
+    if use_wandb:
+        wandb.log({"Heatmap for prediction " f"at {datetime_start}": fig})
