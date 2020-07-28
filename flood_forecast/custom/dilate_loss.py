cfb77c7f13ef072725e19ade1fe45a3c6ccf832a
isaacmg <igodfried@isaac26.com>
2020-07-28 17:59:26 -0400

    auto lint

--- flood_forecast/custom/dilate_loss.py
+++ flood_forecast/custom/dilate_loss.py
@@ -25,17 +25,15 @@ class DilateLoss(torch.nn.Module):
         softdtw_batch = SoftDTWBatch.apply
         D = torch.zeros((batch_size, N_output, N_output)).to(self.device)
         for k in range(batch_size):
-            Dk = pairwise_distances(targets[k,:, :].view(-1, 1),outputs[k, :, :].view(-1, 1))
-            D[k:k+1, :, :] = Dk
+            Dk = pairwise_distances(targets[k, :, :].view(-1, 1), outputs[k, :, :].view(-1, 1))
+            D[k:k + 1, :, :] = Dk
         loss_shape = softdtw_batch(D, self.gamma)
         path_dtw = PathDTWBatch.apply
         path = path_dtw(D, self.gamma)
-        Omega =  pairwise_distances(torch.range(1,N_output).view(N_output,1)).to(self.device)
-        loss_temporal =  torch.sum( path*Omega ) / (N_output*N_output) 
-        loss = self.alpha*loss_shape+ (1-self.alpha)*loss_temporal
+        Omega = pairwise_distances(torch.range(1, N_output).view(N_output, 1)).to(self.device)
+        loss_temporal = torch.sum(path * Omega) / (N_output * N_output)
+        loss = self.alpha * loss_shape + (1 - self.alpha) * loss_temporal
         return loss, loss_shape, loss_temporal
-
-
 
 
 def pairwise_distances(x, y=None):
@@ -53,13 +51,14 @@ def pairwise_distances(x, y=None):
     else:
         y_t = torch.transpose(x, 0, 1)
         y_norm = x_norm.view(1, -1)
-    
+
     dist = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
     return torch.clamp(dist, 0.0, float('inf'))
 
-@jit(nopython = True)
+
+@jit(nopython=True)
 def compute_softdtw(D, gamma):
-    
+
     N = D.shape[0]
     M = D.shape[1]
     R = np.zeros((N + 2, M + 2)) + 1e8
@@ -75,7 +74,8 @@ def compute_softdtw(D, gamma):
             R[i, j] = D[i - 1, j - 1] + softmin
     return R
 
-@jit(nopython = True)
+
+@jit(nopython=True)
 def compute_softdtw_backward(D_, R, gamma):
     N = D_.shape[0]
     M = D_.shape[1]
@@ -96,44 +96,44 @@ def compute_softdtw_backward(D_, R, gamma):
             c = np.exp(c0)
             E[i, j] = E[i + 1, j] * a + E[i, j + 1] * b + E[i + 1, j + 1] * c
     return E[1:N + 1, 1:M + 1]
- 
+
 
 class SoftDTWBatch(Function):
     @staticmethod
-    def forward(ctx, D, gamma = 1.0): # D.shape: [batch_size, N , N]
+    def forward(ctx, D, gamma=1.0):  # D.shape: [batch_size, N , N]
         dev = D.device
-        batch_size,N,N = D.shape
+        batch_size, N, N = D.shape
         gamma = torch.FloatTensor([gamma]).to(dev)
         D_ = D.detach().cpu().numpy()
         g_ = gamma.item()
 
         total_loss = 0
-        R = torch.zeros((batch_size, N+2 ,N+2)).to(dev)   
-        for k in range(0, batch_size): # loop over all D in the batch    
-            Rk = torch.FloatTensor(compute_softdtw(D_[k,:,:], g_)).to(dev)
-            R[k:k+1,:,:] = Rk
-            total_loss = total_loss + Rk[-2,-2]
+        R = torch.zeros((batch_size, N + 2, N + 2)).to(dev)
+        for k in range(0, batch_size):  # loop over all D in the batch
+            Rk = torch.FloatTensor(compute_softdtw(D_[k, :, :], g_)).to(dev)
+            R[k:k + 1, :, :] = Rk
+            total_loss = total_loss + Rk[-2, -2]
         ctx.save_for_backward(D, R, gamma)
         return total_loss / batch_size
-  
+
     @staticmethod
     def backward(ctx, grad_output):
         dev = grad_output.device
         D, R, gamma = ctx.saved_tensors
-        batch_size,N,N = D.shape
+        batch_size, N, N = D.shape
         D_ = D.detach().cpu().numpy()
         R_ = R.detach().cpu().numpy()
         g_ = gamma.item()
 
-        E = torch.zeros((batch_size, N ,N)).to(dev) 
-        for k in range(batch_size):         
-            Ek = torch.FloatTensor(compute_softdtw_backward(D_[k,:,:], R_[k,:,:], g_)).to(dev)
-            E[k:k+1,:,:] = Ek
+        E = torch.zeros((batch_size, N, N)).to(dev)
+        for k in range(batch_size):
+            Ek = torch.FloatTensor(compute_softdtw_backward(D_[k, :, :], R_[k, :, :], g_)).to(dev)
+            E[k:k + 1, :, :] = Ek
 
         return grad_output * E, None
 
 
-@jit(nopython = True)
+@jit(nopython=True)
 def my_max(x, gamma):
     # use the log-sum-exp trick
     max_x = np.max(x)
@@ -141,21 +141,24 @@ def my_max(x, gamma):
     Z = np.sum(exp_x)
     return gamma * np.log(Z) + max_x, exp_x / Z
 
-@jit(nopython = True)
-def my_min(x,gamma) :
+
+@jit(nopython=True)
+def my_min(x, gamma):
     min_x, argmax_x = my_max(-x, gamma)
     return - min_x, argmax_x
 
-@jit(nopython = True)
-def my_max_hessian_product(p, z, gamma):
-    return  ( p * z - p * np.sum(p * z) ) /gamma
 
-@jit(nopython = True)
+@jit(nopython=True)
+def my_max_hessian_product(p, z, gamma):
+    return (p * z - p * np.sum(p * z)) / gamma
+
+
+@jit(nopython=True)
 def my_min_hessian_product(p, z, gamma):
     return - my_max_hessian_product(p, z, gamma)
 
 
-@jit(nopython = True)
+@jit(nopython=True)
 def dtw_grad(theta, gamma):
     m = theta.shape[0]
     n = theta.shape[1]
@@ -170,8 +173,8 @@ def dtw_grad(theta, gamma):
         for j in range(1, n + 1):
             # theta is indexed starting from 0.
             v, Q[i, j] = my_min(np.array([V[i, j - 1],
-                                                V[i - 1, j - 1],
-                                                V[i - 1, j]]) , gamma)                        
+                                          V[i - 1, j - 1],
+                                          V[i - 1, j]]), gamma)
             V[i, j] = theta[i - 1, j - 1] + v
 
     E = np.zeros((m + 2, n + 2))
@@ -180,16 +183,16 @@ def dtw_grad(theta, gamma):
     E[m + 1, n + 1] = 1
     Q[m + 1, n + 1] = 1
 
-    for i in range(m,0,-1):
-        for j in range(n,0,-1):
+    for i in range(m, 0, -1):
+        for j in range(n, 0, -1):
             E[i, j] = Q[i, j + 1, 0] * E[i, j + 1] + \
-                      Q[i + 1, j + 1, 1] * E[i + 1, j + 1] + \
-                      Q[i + 1, j, 2] * E[i + 1, j]
-    
+                Q[i + 1, j + 1, 1] * E[i + 1, j + 1] + \
+                Q[i + 1, j, 2] * E[i + 1, j]
+
     return V[m, n], E[1:m + 1, 1:n + 1], Q, E
 
 
-@jit(nopython = True)
+@jit(nopython=True)
 def dtw_hessian_prod(theta, Z, Q, E, gamma):
     m = Z.shape[0]
     n = Z.shape[1]
@@ -202,46 +205,46 @@ def dtw_hessian_prod(theta, Z, Q, E, gamma):
         for j in range(1, n + 1):
             # theta is indexed starting from 0.
             V_dot[i, j] = Z[i - 1, j - 1] + \
-                          Q[i, j, 0] * V_dot[i, j - 1] + \
-                          Q[i, j, 1] * V_dot[i - 1, j - 1] + \
-                          Q[i, j, 2] * V_dot[i - 1, j]
+                Q[i, j, 0] * V_dot[i, j - 1] + \
+                Q[i, j, 1] * V_dot[i - 1, j - 1] + \
+                Q[i, j, 2] * V_dot[i - 1, j]
 
             v = np.array([V_dot[i, j - 1], V_dot[i - 1, j - 1], V_dot[i - 1, j]])
             Q_dot[i, j] = my_min_hessian_product(Q[i, j], v, gamma)
     E_dot = np.zeros((m + 2, n + 2))
 
-    for j in range(n,0,-1):
-        for i in range(m,0,-1):
+    for j in range(n, 0, -1):
+        for i in range(m, 0, -1):
             E_dot[i, j] = Q_dot[i, j + 1, 0] * E[i, j + 1] + \
-                          Q[i, j + 1, 0] * E_dot[i, j + 1] + \
-                          Q_dot[i + 1, j + 1, 1] * E[i + 1, j + 1] + \
-                          Q[i + 1, j + 1, 1] * E_dot[i + 1, j + 1] + \
-                          Q_dot[i + 1, j, 2] * E[i + 1, j] + \
-                          Q[i + 1, j, 2] * E_dot[i + 1, j]
+                Q[i, j + 1, 0] * E_dot[i, j + 1] + \
+                Q_dot[i + 1, j + 1, 1] * E[i + 1, j + 1] + \
+                Q[i + 1, j + 1, 1] * E_dot[i + 1, j + 1] + \
+                Q_dot[i + 1, j, 2] * E[i + 1, j] + \
+                Q[i + 1, j, 2] * E_dot[i + 1, j]
 
     return V_dot[m, n], E_dot[1:m + 1, 1:n + 1]
 
 
 class PathDTWBatch(Function):
     @staticmethod
-    def forward(ctx, D, gamma): # D.shape: [batch_size, N , N]
-        batch_size,N,N = D.shape
+    def forward(ctx, D, gamma):  # D.shape: [batch_size, N , N]
+        batch_size, N, N = D.shape
         device = D.device
         D_cpu = D.detach().cpu().numpy()
         gamma_gpu = torch.FloatTensor([gamma]).to(device)
-        
-        grad_gpu = torch.zeros((batch_size, N ,N)).to(device)
-        Q_gpu = torch.zeros((batch_size, N+2 ,N+2,3)).to(device)
-        E_gpu = torch.zeros((batch_size, N+2 ,N+2)).to(device)  
-        
-        for k in range(0,batch_size): # loop over all D in the batch    
-            _, grad_cpu_k, Q_cpu_k, E_cpu_k = dtw_grad(D_cpu[k,:,:], gamma)     
-            grad_gpu[k,:,:] = torch.FloatTensor(grad_cpu_k).to(device)
-            Q_gpu[k,:,:,:] = torch.FloatTensor(Q_cpu_k).to(device)
-            E_gpu[k,:,:] = torch.FloatTensor(E_cpu_k).to(device)
-        ctx.save_for_backward(grad_gpu,D, Q_gpu ,E_gpu, gamma_gpu) 
-        return torch.mean(grad_gpu, dim=0) 
-    
+
+        grad_gpu = torch.zeros((batch_size, N, N)).to(device)
+        Q_gpu = torch.zeros((batch_size, N + 2, N + 2, 3)).to(device)
+        E_gpu = torch.zeros((batch_size, N + 2, N + 2)).to(device)
+
+        for k in range(0, batch_size):  # loop over all D in the batch
+            _, grad_cpu_k, Q_cpu_k, E_cpu_k = dtw_grad(D_cpu[k, :, :], gamma)
+            grad_gpu[k, :, :] = torch.FloatTensor(grad_cpu_k).to(device)
+            Q_gpu[k, :, :, :] = torch.FloatTensor(Q_cpu_k).to(device)
+            E_gpu[k, :, :] = torch.FloatTensor(E_cpu_k).to(device)
+        ctx.save_for_backward(grad_gpu, D, Q_gpu, E_gpu, gamma_gpu)
+        return torch.mean(grad_gpu, dim=0)
+
     @staticmethod
     def backward(ctx, grad_output):
         device = grad_output.device
@@ -251,11 +254,11 @@ class PathDTWBatch(Function):
         E_cpu = E_gpu.detach().cpu().numpy()
         gamma = gamma.detach().cpu().numpy()[0]
         Z = grad_output.detach().cpu().numpy()
-        
-        batch_size,N,N = D_cpu.shape
-        Hessian = torch.zeros((batch_size, N ,N)).to(device)
-        for k in range(0,batch_size):
-            _, hess_k = dtw_hessian_prod(D_cpu[k,:,:], Z, Q_cpu[k,:,:,:], E_cpu[k,:,:], gamma)
-            Hessian[k:k+1,:,:] = torch.FloatTensor(hess_k).to(device)
 
-        return  Hessian, None
+        batch_size, N, N = D_cpu.shape
+        Hessian = torch.zeros((batch_size, N, N)).to(device)
+        for k in range(0, batch_size):
+            _, hess_k = dtw_hessian_prod(D_cpu[k, :, :], Z, Q_cpu[k, :, :, :], E_cpu[k, :, :], gamma)
+            Hessian[k:k + 1, :, :] = torch.FloatTensor(hess_k).to(device)
+
+        return Hessian, None
