953b542f194e7b6f934f1cf342ee1fc9b4cfefb6
isaacmg <igodfried@isaac26.com>
2020-07-28 18:09:21 -0400

    cs

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -29,10 +29,10 @@ def train_transformer_style(
         es = EarlyStopper(model.params["early_stopping"]['patience'])
     opt = pytorch_opt_dict[training_params["optimizer"]](
         model.model.parameters(), **training_params["optim_params"])
-    criterion_params = {}
+    criterion_init_params = {}
     if "criterion_params" in training_params:
-        criterion_params = training_params["criterion_params"]
-    criterion = pytorch_criterion_dict[training_params["criterion"]](**criterion_params)
+        criterion_init_params = training_params["criterion_params"]
+    criterion = pytorch_criterion_dict[training_params["criterion"]](**criterion_init_params)
     max_epochs = training_params["epochs"]
     data_loader = DataLoader(
         model.training,
