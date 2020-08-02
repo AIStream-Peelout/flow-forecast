c7fecfc40c29ca04f463e5b406324cbabaf984c6
kritim13 <kriti.mahajan.13@gmail.com>
2020-08-02 22:27:30 +0530

    'more_conf_issues'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../flood_forecast'))
+sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath('../flood_forecast'))
 
 # -- Project information -----------------------------------------------------
