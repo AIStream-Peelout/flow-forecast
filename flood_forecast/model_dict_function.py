b59ae9f5200f17b6ebec1cfc07a33edc72387772
isaacmg <igodfried@isaac26.com>
2020-08-03 10:22:45 -0400

    add L1 loss

--- flood_forecast/model_dict_function.py
+++ flood_forecast/model_dict_function.py
@@ -5,7 +5,7 @@ from flood_forecast.transformer_xl.dummy_torch import DummyTorchModel
 from flood_forecast.basic.linear_regression import SimpleLinearModel
 from flood_forecast.basic.lstm_vanilla import LSTMForecast
 from torch.optim import Adam, SGD
-from torch.nn import MSELoss, SmoothL1Loss, PoissonNLLLoss
+from torch.nn import MSELoss, SmoothL1Loss, PoissonNLLLoss, L1
 from flood_forecast.custom.custom_opt import BertAdam
 from flood_forecast.basic.linear_regression import simple_decode
 from flood_forecast.transformer_xl.transformer_basic import greedy_decode
@@ -33,7 +33,8 @@ pytorch_criterion_dict = {
     "PoissonNLLLoss": PoissonNLLLoss,
     "RMSE": RMSELoss,
     "MAPE": MAPELoss,
-    "DilateLoss": DilateLoss}
+    "DilateLoss": DilateLoss,
+    "L1": L1}
 
 
 evaluation_functions_dict = {"NSE": "", "MSE": ""}
