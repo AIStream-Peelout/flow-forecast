821afe6a3b750d5e4a3a5ed579e4fcbd0f0f44b2
isaacmg <igodfried@isaac26.com>
2020-08-09 18:55:40 -0400

    FIX model

--- flood_forecast/da_rnn/model.py
+++ flood_forecast/da_rnn/model.py
@@ -1,27 +1,182 @@
 import torch
 from torch import nn
-from flood_forecast.da_rnn.modules import Encoder, Decoder
-
+from torch.autograd import Variable
+from torch.nn import functional as tf
 
 class DARNN(nn.Module):
 
     def __init__(
             self,
-            input_size: int,
+            n_time_series: int,
             hidden_size_encoder: int,
-            T: int,
+            forecast_history: int,
             decoder_hidden_size: int,
-            out_feats=1):
+            out_feats=1,
+            gru_lstm=True):
         """
         input size: number of underlying factors (81)
         T: number of time steps (10)
         hidden_size: dimension of the hidden state
         """
-        self.encoder = Encoder(input_size, hidden_size_encoder, T)
-        self.decoder = Decoder(hidden_size_encoder, decoder_hidden_size, T, out_feats)
+        super().__init__()
+        self.encoder = Encoder(n_time_series-1, hidden_size_encoder, forecast_history, gru_lstm)
+        self.decoder = Decoder(hidden_size_encoder, decoder_hidden_size, forecast_history, out_feats, gru_lstm)
 
-    def forward(self, x: torch.Tensor, y_history: torch.Tensor):
+    def forward(self, x: torch.Tensor):
         """will implement"""
-        input_weighted, input_encoded = self.encoder(x)
-        y_pred = self.decoder(input_encoded, y_history)
+        i=0
+        print(i)
+        input_weighted, input_encoded = self.encoder(x[:, :, 1:])
+        y_pred = self.decoder(input_encoded, x[:, :, 0].unsqueeze(2))
         return y_pred
+
+
+def init_hidden(x, hidden_size: int):
+    """
+    Train the initial value of the hidden state:
+    https://r2rt.com/non-zero-initial-states-for-recurrent-neural-networks.html
+    """
+    return Variable(torch.zeros(1, x.size(0), hidden_size)).to(x.device)
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, input_size: int, hidden_size: int, T: int, gru_lstm=True):
+        """
+        input size: number of underlying factors (81)
+        T: number of time steps (10)
+        hidden_size: dimension of the hidden state
+        """
+        super(Encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.T = T
+        self.gru_lstm = gru_lstm
+        if gru_lstm:
+          self.lstm_layer = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=1)
+        else:
+          self.gru_layer = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=1)
+        self.attn_linear = nn.Linear(in_features=2 * hidden_size + T - 1, out_features=1)
+
+    def forward(self, input_data: torch.Tensor):
+        # input_data: (batch_size, T - 1, input_size)
+        device = input_data.device
+        input_weighted = Variable(
+            torch.zeros(
+                input_data.size(0),
+                self.T - 1,
+                self.input_size)).to(device)
+        input_encoded = Variable(
+            torch.zeros(
+                input_data.size(0),
+                self.T - 1,
+                self.hidden_size)).to(device)
+        # hidden, cell: initial states with dimension hidden_size
+        hidden = init_hidden(input_data, self.hidden_size)  # 1 * batch_size * hidden_size
+        cell = init_hidden(input_data, self.hidden_size)
+
+        for t in range(self.T - 1):
+            # Eqn. 8: concatenate the hidden states with each predictor
+            x = torch.cat(
+                (hidden.repeat(
+                    self.input_size, 1, 1).permute(
+                    1, 0, 2), cell.repeat(
+                    self.input_size, 1, 1).permute(
+                    1, 0, 2), input_data.permute(
+                        0, 2, 1)), dim=2)  # batch_size * input_size * (2*hidden_size + T - 1)
+            # Eqn. 8: Get attention weights
+            x = self.attn_linear(x.view(-1, self.hidden_size * 2 + self.T - 1)
+                                 )  # (batch_size * input_size) * 1
+            # Eqn. 9: Softmax the attention weights
+            attn_weights = tf.softmax(x.view(-1, self.input_size),
+                                      dim=1)  # (batch_size, input_size)
+            # Eqn. 10: LSTM
+            # (batch_size, input_size)
+            weighted_input = torch.mul(attn_weights, input_data[:, t, :])
+            # Fix the warning about non-contiguous memory
+            # see https://discuss.pytorch.org/t/dataparallel-issue-with-flatten-parameter/8282
+            if self.gru_lstm:
+              self.lstm_layer.flatten_parameters()
+              _, generic_states = self.lstm_layer(weighted_input.unsqueeze(0), (hidden, cell))
+              cell = generic_states[1]
+              hidden = generic_states[0]
+            else: 
+              self.gru_layer.flatten_parameters()
+              print(weighted_input.shape)
+              __, generic_states = self.gru_layer(weighted_input.unsqueeze(0), hidden)
+              hidden = generic_states[0].unsqueeze(0)
+
+            
+            # Save output
+            input_weighted[:, t, :] = weighted_input
+            input_encoded[:, t, :] = hidden
+
+        return input_weighted, input_encoded
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, encoder_hidden_size: int, decoder_hidden_size: int, T: int, out_feats=1, gru_lstm=True):
+        super(Decoder, self).__init__()
+
+        self.T = T
+        self.encoder_hidden_size = encoder_hidden_size
+        self.decoder_hidden_size = decoder_hidden_size
+
+        self.attn_layer = nn.Sequential(nn.Linear(2 * decoder_hidden_size + encoder_hidden_size,
+                                                  encoder_hidden_size),
+                                        nn.Tanh(),
+                                        nn.Linear(encoder_hidden_size, 1))
+        self.gru_lstm = gru_lstm
+        if gru_lstm:
+           self.lstm_layer = nn.LSTM(input_size=out_feats, hidden_size=decoder_hidden_size)
+        else: 
+          self.gru_layer = nn.GRU(input_size=out_feats, hidden_size=decoder_hidden_size) 
+        
+        
+        self.fc = nn.Linear(encoder_hidden_size + out_feats, out_feats)
+        self.fc_final = nn.Linear(decoder_hidden_size + encoder_hidden_size, out_feats)
+
+        self.fc.weight.data.normal_()
+
+    def forward(self, input_encoded, y_history):
+        #y_history = input_encoded[:, :, 0]
+        # input_encoded: (batch_size, T - 1, encoder_hidden_size)
+        # y_history: (batch_size, (T-1))
+        # Initialize hidden and cell, (1, batch_size, decoder_hidden_size)
+        hidden = init_hidden(input_encoded, self.decoder_hidden_size)
+        cell = init_hidden(input_encoded, self.decoder_hidden_size)
+        context = Variable(torch.zeros(input_encoded.size(0), self.encoder_hidden_size))
+
+        for t in range(self.T - 1):
+            # (batch_size, T, (2 * decoder_hidden_size + encoder_hidden_size))
+            x = torch.cat((hidden.repeat(self.T - 1, 1, 1).permute(1, 0, 2),
+                           cell.repeat(self.T - 1, 1, 1).permute(1, 0, 2),
+                           input_encoded), dim=2)
+            # Eqn. 12 & 13: softmax on the computed attention weights
+            x = tf.softmax(
+                self.attn_layer(
+                    x.view(-1, 2 * self.decoder_hidden_size + self.encoder_hidden_size)
+                ).view(-1, self.T - 1),
+                dim=1)  # (batch_size, T - 1)
+
+            # Eqn. 14: compute context vector
+            context = torch.bmm(x.unsqueeze(1), input_encoded)[
+                :, 0, :]  # (batch_size, encoder_hidden_size)
+
+            # Eqn. 15
+            # (batch_size, out_size)
+            y_tilde = self.fc(torch.cat((context, y_history[:, t]), dim=1))
+            # Eqn. 16: LSTM
+            if self.gru_lstm:
+              self.lstm_layer.flatten_parameters()
+              _, lstm_output = self.lstm_layer(y_tilde.unsqueeze(0), (hidden, cell))
+              hidden = lstm_output[0]  # 1 * batch_size * decoder_hidden_size
+              cell = lstm_output[1]  # 1 * batch_size * decoder_hidden_size
+            else:
+              self.gru_layer.flatten_parameters()
+              __, generic_states = self.gru_layer(y_tilde.unsqueeze(0), hidden)
+              hidden = generic_states[0].unsqueeze(0)
+
+        # Eqn. 22: final output
+        return self.fc_final(torch.cat((hidden[0], context), dim=1))
