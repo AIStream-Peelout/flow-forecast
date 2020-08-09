40c5c5b57e5441c234c34ba5ccaa92709bbdb053
isaacmg <igodfried@isaac26.com>
2020-08-09 19:01:08 -0400

    fixes to lint

--- flood_forecast/model_dict_function.py
+++ flood_forecast/model_dict_function.py
@@ -9,7 +9,7 @@ from torch.nn import MSELoss, SmoothL1Loss, PoissonNLLLoss, L1Loss
 from flood_forecast.custom.custom_opt import BertAdam
 from flood_forecast.basic.linear_regression import simple_decode
 from flood_forecast.transformer_xl.transformer_basic import greedy_decode
-from flood_forecast.da_rnn.model import DARNN 
+from flood_forecast.da_rnn.model import DARNN
 from flood_forecast.transformer_xl.xc_attn import TransformerXCBasic
 from flood_forecast.custom.custom_opt import RMSELoss, MAPELoss
 from flood_forecast.custom.dilate_loss import DilateLoss
@@ -26,9 +26,9 @@ pytorch_model_dict = {
     "LSTM": LSTMForecast,
     "SimpleLinearModel": SimpleLinearModel,
     "CustomTransformerDecoder": CustomTransformerDecoder,
-    "DARNN":DARNN, 
-    "XCTransformer":TransformerXCBasic
-    }
+    "DARNN": DARNN,
+    "XCTransformer": TransformerXCBasic
+}
 
 pytorch_criterion_dict = {
     "MSE": MSELoss,
--- flood_forecast/transformer_xl/xc_attn.py
+++ flood_forecast/transformer_xl/xc_attn.py
@@ -3,6 +3,7 @@ import torch
 
 class TransformerXCBasic(torch.nn.Module):
     """ Transformer model """
+
     def __init__(self, n_time_series, out_seq_len, device, d_model=128, dropout=.5, n_head=8):
         super(TransformerXCBasic, self).__init__()
         self.input_dim = n_time_series
@@ -10,24 +11,23 @@ class TransformerXCBasic(torch.nn.Module):
         self.seq_num = seq_num
         self.n_embd = n_embd
         self.win_len = win_len
-        self.id_embed = torch.nn.Embedding(seq_num,n_embd)
-        self.po_embed = torch.nn.Embedding(seq_len,n_embd)
+        self.id_embed = torch.nn.Embedding(seq_num, n_embd)
+        self.po_embed = torch.nn.Embedding(seq_len, n_embd)
         self.drop_em = torch.nn.Dropout(dropout)
         self.device = device
-        self.xc_encoder = TransformerEncoderLayer(d_model,n_head)
+        self.xc_encoder = TransformerEncoderLayer(d_model, n_head)
         torch.nn.init.normal_(self.id_embed.weight, std=0.02)
         torch.nn.init.normal_(self.po_embed.weight, std=0.02)
 
     def forward(self, series_id, x):
         id_embedding = self.id_embed(series_id)
-        length = x.size(1) # (Batch_size,length,input_dim)
-        position = torch.tensor(torch.arange(length),dtype=torch.long).to(self.device)
+        length = x.size(1)  # (Batch_size,length,input_dim)
+        position = torch.tensor(torch.arange(length), dtype=torch.long).to(self.device)
         po_embedding = self.po_embed(position)
         batch_size = x.size(0)
-        embedding_sum = torch.zeros(batch_size,length,self.n_embd).to(self.device)
+        embedding_sum = torch.zeros(batch_size, length, self.n_embd).to(self.device)
         embedding_sum[:] = po_embedding
         embedding_sum = embedding_sum + id_embedding.unsqueeze(1)
-        x = torch.cat((x,embedding_sum),dim=2)
+        x = torch.cat((x, embedding_sum), dim=2)
         print(x.shape)
         return x
-    
