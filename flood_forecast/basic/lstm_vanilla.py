75d5ab4d7e8ca9a95cb57730f762682dd0b4d797
kritim13 <kriti.mahajan.13@gmail.com>
2020-08-12 02:44:56 +0530

    'probabalistic-lstm'

--- flood_forecast/basic/lstm_vanilla.py
+++ flood_forecast/basic/lstm_vanilla.py
@@ -16,14 +16,21 @@ class LSTMForecast(torch.nn.Module):
             hidden_states: int = 20,
             num_layers=2,
             bias=True,
-            batch_size=100):
+            batch_size=100,
+            probabalistic=False):
+
         super().__init__()
         self.forecast_history = seq_length
         self.n_time_series = n_time_series
         self.hidden_dim = hidden_states
         self.num_layers = num_layers
         self.lstm = torch.nn.LSTM(n_time_series, hidden_states, num_layers, bias, batch_first=True)
-        self.final_layer = torch.nn.Linear(seq_length * hidden_states, output_seq_len)
+        self.probabalistic = probabalistic
+        if self.probabalistic:
+            output_seq_len = 2
+            self.final_layer = torch.nn.Linear(seq_length * hidden_states, output_seq_len)
+        else:
+            self.final_layer = torch.nn.Linear(seq_length * hidden_states, output_seq_len)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.init_hidden(batch_size)
 
@@ -42,8 +49,13 @@ class LSTMForecast(torch.nn.Module):
                     self.device))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+
         batch_size = x.size()[0]
         self.init_hidden(batch_size)
         out_x, self.hidden = self.lstm(x, self.hidden)
         x = self.final_layer(out_x.contiguous().view(batch_size, -1))
+        if self.probabalistic:
+            mean = x[..., 0][..., None]
+            std = x[..., 1][..., None]
+            x = torch.distributions.Normal(mean, std)
         return x
