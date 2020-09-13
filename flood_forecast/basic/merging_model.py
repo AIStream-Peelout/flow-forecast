3e13d291534d83f74303eafe4be30b1dcd63b99d
isaacmg <igodfried@isaac26.com>
2020-09-12 23:17:46 -0400

    Revert "Create merging_model.py"
    This reverts commit 290050c9438030d6186a546b31cdb205b34383d8.

--- flood_forecast/basic/merging_model.py
+++
@@ -1,9 +0,0 @@
-class MergingModel(torch.nn.Module):
-  def __init__(self, method, other_params):
-    super().__init__()
-    self.method_dict = {"Bilinear":torch.nn.Bilinear}
-    self.method_layer = self.method_dict[method](**other_params)
-
-  def forward(self, x):
-    x = self.method_layer(x)
-    return x
