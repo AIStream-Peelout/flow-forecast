313376676005f64c0aaee78734949e7b547bdd51
isaacmg <igodfried@isaac26.com>
2020-09-13 23:15:20 -0400

    fix linting

--- flood_forecast/meta_models/merging_model.py
+++ flood_forecast/meta_models/merging_model.py
@@ -2,11 +2,11 @@ import torch
 
 
 class MergingModel(torch.nn.Module):
-  def __init__(self, method, other_params):
-    super().__init__()
-    self.method_dict = {"Bilinear":torch.nn.Bilinear}
-    self.method_layer = self.method_dict[method](**other_params)
+    def __init__(self, method, other_params):
+        super().__init__()
+        self.method_dict = {"Bilinear": torch.nn.Bilinear}
+        self.method_layer = self.method_dict[method](**other_params)
 
-  def forward(self, x):
-    x = self.method_layer(x)
-    return x
+    def forward(self, x):
+        x = self.method_layer(x)
+        return x
