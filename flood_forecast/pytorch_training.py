84c433f7f317860d3bbc423f70603d3be35d6fbc
kritim13 <kriti.mahajan.13@gmail.com>
2020-09-16 13:15:57 +0530

    change to architecture to solve bug 2

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -171,7 +171,7 @@ def torch_single_train(model: PyTorchForecast,
         if takes_target:
             forward_params["t"] = trg
         output = model.model(src, **forward_params)
-        print('output',output)
+        print('output', output)
         labels = trg[:, :, 0]
         loss = criterion(output, labels.float())
         if loss > 100:
