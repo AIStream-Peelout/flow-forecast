c33b3e415885537097c1dd04d2b62461868f0d9d
kritim13 <kriti.mahajan.13@gmail.com>
2020-09-30 00:42:30 +0530

    trying only rmse with abs

--- flood_forecast/custom/custom_opt.py
+++ flood_forecast/custom/custom_opt.py
@@ -59,14 +59,14 @@ class RMSELoss(torch.nn.Module):
         self.variance_penalty_type = variance_penalty_type
 
     def forward(self, target: torch.Tensor, output: torch.Tensor):
-        # if (self.variance_penalty_type == 'absolute') | (self.variance_penalty_type == 'abs'):
-        #    return torch.sqrt(self.mse(target, output)) + self.variance_penalty \
-        # * torch.std(torch.abs(target - output))
+        if (self.variance_penalty_type == 'absolute') | (self.variance_penalty_type == 'abs'):
+            return torch.sqrt(self.mse(target, output)) + self.variance_penalty \
+         * torch.std(torch.abs(target - output))
         # elif (self.variance_penalty_type == 'squared'):
         #    return torch.sqrt(self.mse(target, output)) + self.variance_penalty \
         # * torch.std((target - output)**2)
-        # elif (self.variance_penalty_type is None):
-        return torch.sqrt(self.mse(target, output))
+        elif (self.variance_penalty_type is None):
+            return torch.sqrt(self.mse(target, output))
 
 
 class MAPELoss(torch.nn.Module):
