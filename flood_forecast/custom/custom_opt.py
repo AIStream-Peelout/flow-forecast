00c5d473d9c4575fc0e494e14b7297b9f5393c4a
kritim13 <kriti.mahajan.13@gmail.com>
2020-10-20 00:44:27 +0530

    change to rmse penalty simplified

--- flood_forecast/custom/custom_opt.py
+++ flood_forecast/custom/custom_opt.py
@@ -59,14 +59,16 @@ class RMSELoss(torch.nn.Module):
         self.variance_penalty_type = variance_penalty_type
 
     def forward(self, target: torch.Tensor, output: torch.Tensor):
-        if (self.variance_penalty_type == 'absolute') | (self.variance_penalty_type == 'abs'):
-            rmse_loss = torch.sqrt(self.mse(target, output)) + self.variance_penalty \
-                        * torch.std(torch.abs(target - output))
+
+        #if (self.variance_penalty_type == 'absolute') | (self.variance_penalty_type == 'abs'):
+        #    rmse_loss = torch.sqrt(self.mse(target, output)) + self.variance_penalty \
+        #                * torch.std(torch.abs(target - output))
         # elif (self.variance_penalty_type == 'squared'):
         #    return torch.sqrt(self.mse(target, output)) + self.variance_penalty \
         # * torch.std((target - output)**2)
-        elif self.variance_penalty_type is None:
-            rmse_loss = torch.sqrt(self.mse(target, output))
+        #elif self.variance_penalty_type is None:
+        rmse_loss = torch.sqrt(self.mse(target, output))  + self.variance_penalty\
+                    *torch.std(target - output)#torch.abs(target - output))
         return rmse_loss
 
 
