c176d9ff3bb94f33e75ce4d71a66523bbf755621
isaacmg <igodfried@isaac26.com>
2020-10-29 16:51:38 -0300

    d

--- flood_forecast/da_rnn/infer_main.py
+++
@@ -1,8 +0,0 @@
-from flood_forecast.time_model import PyTorchForecast
-from typing import Dict
-
-
-def load_the_model(model_params: Dict, test_csv_path: str):
-    model_params[]
-    model = PyTorchForecast()
-    pass
---
+++ flood_forecast/deployment/infer.py
@@ -0,0 +1,2 @@
+from flood_forecast.time_model import PyTorchForecast
+from typing import Dict, List, Optional, Sequence, DefaultDict
