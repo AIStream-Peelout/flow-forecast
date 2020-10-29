5b3d1d03b6612e91aea7b6911bf4a7205e73beba
isaacmg <igodfried@isaac26.com>
2020-10-29 16:48:39 -0300

    new additions

---
+++ flood_forecast/da_rnn/infer_main.py
@@ -0,0 +1,8 @@
+from flood_forecast.time_model import PyTorchForecast
+from typing import Dict
+
+
+def load_the_model(model_params: Dict, test_csv_path: str):
+    model_params[]
+    model = PyTorchForecast()
+    pass
