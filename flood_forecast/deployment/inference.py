c6d119460cf08991322dcf5900f19a1171519e13
isaacmg <igodfried@isaac26.com>
2020-10-30 16:36:31 -0300

    add inference mode and fix

--- flood_forecast/deployment/inference.py
+++ flood_forecast/deployment/inference.py
@@ -1,8 +1,24 @@
 from flood_forecast.time_model import PyTorchForecast
+from flood_forecast.evaluator import infer_on_torch_model
+
+
+class InferenceMode(object):
+    def __init__(self, hours_to_forecast, num_prediction_samples, model_params, csv_path, weight_path):
+        self.hours_to_forecast = hours_to_forecast
+        self.model = load_model(model_params, weight_path, csv_path)
+        self.inference_params = model_params["inference_params"]
+        self.inference_params["hours_to_forecast"] = hours_to_forecast
+        self.inference_params["num_prediction_samples"] = num_prediction_samples
+
+    def infer_now(self, some_date, csv_path=None):
+        self.inference_params["start_datetime"] = some_date
+        if csv_path:
+            self.inference_params["test_csv_path"] = csv_path
+        infer_on_torch_model(self.model, **self.inference_params)
 
 
 def load_model(model_params_dict, weight_path=None, file_path: str = "") -> PyTorchForecast:
     if weight_path:
         model_params_dict["weight_path"] = weight_path
-    m = PyTorchForecast(model_params_dict, file_path, file_path, file_path, model_params_dict)
+    m = PyTorchForecast(model_params_dict["model_name"], file_path, file_path, file_path, model_params_dict)
     return m
