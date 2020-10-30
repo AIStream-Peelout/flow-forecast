6f4fc51d1aaaf3fd6ffe2c7f9563ab8a8ff5b35a
isaacmg <igodfried@isaac26.com>
2020-10-30 15:56:30 -0300

    test weight path
    New file function to fest

--- tests/infer_test.py
+++ tests/infer_test.py
@@ -10,9 +10,10 @@ class InferenceTests(unittest.TestCase):
         with open(os.path.join(os.path.dirname(__file__), "test_file.json")) as f:
             self.test_config = json.load(f)
         self.file_path = "gs://task_ts_data/Massachusetts_Middlesex_County.csv"
+        self.weight_path = "gs://coronaviruspublicdata/experiments/25_May_202010_29PM_model.pth"
 
     def test_load_model(self):
-        load_model(self.test_config, self.file_path)
+        load_model(self.test_config, self.file_path, self.weight_path)
 
 
 if __name__ == '__main__':
