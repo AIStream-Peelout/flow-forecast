beebf4382fcef7ea8bccb7372cf4c1d81c910c3e
isaacmg <igodfried@isaac26.com>
2020-10-30 14:54:16 -0300

    basic model and fixes

--- flood_forecast/deployment/infer.py
+++ flood_forecast/deployment/infer.py
@@ -2,11 +2,13 @@ from flood_forecast.time_model import PyTorchForecast
 from typing import Dict, List, Optional, Sequence, DefaultDict
 
 
-def load_model(model_params: Dict, new_csv_path: str, weight_path: str = ""):
-    if weight_path not in model_params:
+def load_model(model_params: Dict, new_csv_path: str, weight_path: str = None) -> PyTorchForecast:
+    if weight_path not in model_params and weight_path:
         model_params["weight_path"] = weight_path
     model = PyTorchForecast(model_params["name"], new_csv_path, new_csv_path, new_csv_path, model_params)
+    model.model.eval()
     return model
 
-def new():
+
+def new_instantiation(mode: str, model_params: DefaultDict, seq: Sequence, a_list: List) -> Optional:
     pass
