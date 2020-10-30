44f25fe704de49a46df54f34a3837492f5a83efa
isaacmg <igodfried@isaac26.com>
2020-10-30 15:13:35 -0300

    adding

---
+++ tests/infer_test.py
@@ -0,0 +1,19 @@
+import unittest
+from flood_forecast.deployment.infer import load_model
+import os
+import json
+
+
+class InferenceTests(unittest.TestCase):
+
+    def setUp(self):
+        with open(os.path.join(os.path.abspath(__file__), "test_file.json")) as f:
+            self.test_config = json.load(f)
+        self.file_path = "gs://task_ts_data/Massachusetts_Middlesex_County.csv"
+
+    def test_load_model(self):
+        load_model(self.test_config, self.file_path)
+
+
+if __name__ == '__main__':
+    unittest.main()
