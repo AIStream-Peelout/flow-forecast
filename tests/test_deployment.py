61c80e78fddb875c7dc50d05158fc8b70f6449da
isaacmg <igodfried@isaac26.com>
2020-11-02 14:32:13 -0400

    add intializiation to test

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flood_forecast.deployment.inference import load_model
+from flood_forecast.deployment.inference import load_model, InferenceMode
 import unittest
 
 
@@ -10,6 +10,7 @@ class InferenceTests(unittest.TestCase):
             self.config_test = json.load(y)
         self.new_csv_path = "gs://task_ts_data/Massachusetts_Middlesex_County.csv"
         self.weight_path = "gs://coronaviruspublicdata/experiments/01_July_202009_44PM_model.pth"
+        self.infer_class = InferenceMode(30, 20, self.config_test, self.new_csv_path, self.weight_path)
 
     def test_load_model(self):
         load_model(self.config_test, self.new_csv_path, self.weight_path)
