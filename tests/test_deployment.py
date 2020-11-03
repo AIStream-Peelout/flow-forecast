67181ea964cd71d50cd5bb409712446ff410538b
isaacmg <igodfried@isaac26.com>
2020-11-03 14:44:31 -0400

    swap hours

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -11,7 +11,7 @@ class InferenceTests(unittest.TestCase):
             self.config_test = json.load(y)
         self.new_csv_path = "gs://task_ts_data/Massachusetts_Middlesex_County.csv"
         self.weight_path = "gs://coronaviruspublicdata/experiments/01_July_202009_44PM_model.pth"
-        self.infer_class = InferenceMode(30, 20, self.config_test, self.new_csv_path, self.weight_path)
+        self.infer_class = InferenceMode(20, 30, self.config_test, self.new_csv_path, self.weight_path)
 
     def test_load_model(self):
         load_model(self.config_test, self.new_csv_path, self.weight_path)
