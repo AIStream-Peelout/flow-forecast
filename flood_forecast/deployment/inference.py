65aeda0ed40e8a820fdff738109d93e496a9a409
isaacmg <igodfried@isaac26.com>
2020-11-08 16:59:39 -0400

    add type

--- flood_forecast/deployment/inference.py
+++ flood_forecast/deployment/inference.py
@@ -6,7 +6,7 @@ from flood_forecast.gcp_integration.basic_utils import upload_file
 
 
 class InferenceMode(object):
-    def __init__(self, hours_to_forecast, num_prediction_samples, model_params, csv_path, weight_path):
+    def __init__(self, hours_to_forecast: int, num_prediction_samples: int, model_params, csv_path, weight_path):
         self.hours_to_forecast = hours_to_forecast
         self.model = load_model(model_params, csv_path, weight_path)
         self.inference_params = model_params["inference_params"]
@@ -26,13 +26,13 @@ class InferenceMode(object):
             upload_file(save_buck, "temp.csv", save_name, self.model.gcs_client)
         return df, tensor, history, forecast_start, test, samples
 
-    def make_plots(self, date, csv_path, csv_bucket=None, save_name=None):
+    def make_plots(self, date, csv_path: str, csv_bucket: str = None, save_name=None):
         df, tensor, history, forecast_start, test, samples = self.infer_now(date, csv_path, csv_bucket, save_name)
         plot_df_test_with_confidence_interval(df, samples, forecast_start, self.model.params)
         return tensor, history, test
 
 
-def load_model(model_params_dict, file_path, weight_path) -> PyTorchForecast:
+def load_model(model_params_dict, file_path, weight_path: str) -> PyTorchForecast:
     if weight_path:
         model_params_dict["weight_path"] = weight_path
     model_params_dict["inference_params"]["test_csv_path"] = file_path
