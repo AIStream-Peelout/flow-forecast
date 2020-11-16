df194a730cef7c655cf8c37e28541cd5f2c03dda
isaacmg <igodfried@isaac26.com>
2020-11-16 18:07:21 -0400

    s

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -153,8 +153,11 @@ def get_meta_representation(column_id: str, uuid: str, meta_model):
 def compute_loss(labels, output, src, criterion, validation_dataset, probabilistic=None, output_std=None):
     if probabilistic and not validation_dataset:
         print(output)
+        print(type(output))
+        print("middle")
         print(output_std)
         print("output above")
+        print(type(output_std))
         output_dist = torch.distributions.Normal(output, output_std)
     if validation_dataset:
         if probabilistic:
