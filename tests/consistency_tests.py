211b1ef48ee2665e5bb5e85c7be19330bfd823bf
isaacmg <igodfried@isaac26.com>
2020-11-30 03:03:48 -0400

    fixes to everything

--- tests/consistency_tests.py
+++ tests/consistency_tests.py
@@ -18,14 +18,13 @@ class ConsistencyTests(unittest.TestCase):
 
     def test_model_consistency(self):
         train_function("PyTorch", self.model_config)
-        self.updated_config = wandb.config
-        self.assertIn("gcs_m_path_5_model", self.updated_config)
+        self.updated_config = wandb.config.copy()
+        self.assertIn("gcs_m_path_2_model", self.updated_config)
 
     def test_inference_same(self):
-        print(wandb.config["gcs_m_path_2_params"])
         file_path = os.path.join(self.base_path, "test_data", "keag_small.csv")
         m = inference.InferenceMode(336, 20, self.updated_config["gcs_m_path_2_params"], file_path,
-                                    self.updated_config["gcs_m_path_1_model"])
+                                    self.updated_config["gcs_m_path_2_model"])
         tensor, history, test, plt = m.make_plots(datetime.datetime(2016, 5, 31))
         wandb.log({"plt_2": plt})
         print("Shut you stupid linter i will use variables later")
