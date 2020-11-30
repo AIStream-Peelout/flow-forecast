d70590824356dbe34f59c0de32c7801f62eb760b
isaacmg <igodfried@isaac26.com>
2020-11-30 00:04:20 -0400

    fixes to path

--- tests/consistency_tests.py
+++ tests/consistency_tests.py
@@ -9,7 +9,8 @@ import wandb
 
 class ConsistencyTests(unittest.TestCase):
     def setUp(self):
-        with open("decoder_test.json") as y:
+        self.base_path = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(self.base_path, "decoder_test.json")) as y:
             config = json.load(y)
         self.model_config = config
         self.updated_config = wandb.config
@@ -20,8 +21,7 @@ class ConsistencyTests(unittest.TestCase):
         self.assertIn("gcs_m_path_5_model", self.updated_config)
 
     def test_inference_same(self):
-        os
-        file_path = ""
+        file_path = os.path.join(self.base_path, "test_data", "keag_small.csv")
         m = inference.InferenceMode(336, 20, self.updated_config["gcs_m_path_5_params"], file_path,
                                     self.updated_config["gcs_m_path_5_model"])
         tensor, history, test, plt = m.make_plots(datetime.datetime(2016, 5, 31))
