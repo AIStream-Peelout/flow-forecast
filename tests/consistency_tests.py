8a66f777315585de17adca6a12d9cb5d9fa8a0ea
isaacmg <igodfried@isaac26.com>
2020-11-30 02:37:31 -0400

    fixes to stuff

--- tests/consistency_tests.py
+++ tests/consistency_tests.py
@@ -22,9 +22,10 @@ class ConsistencyTests(unittest.TestCase):
 
     def test_inference_same(self):
         print(self.updated_config)
+        print(self.updated_config["gcs_m_path_2_params"])
         file_path = os.path.join(self.base_path, "test_data", "keag_small.csv")
-        m = inference.InferenceMode(336, 20, self.updated_config["gcs_m_path_5_params"], file_path,
-                                    self.updated_config["gcs_m_path_5_model"])
+        m = inference.InferenceMode(336, 20, self.updated_config["gcs_m_path_2_params"], file_path,
+                                    self.updated_config["gcs_m_path_1_model"])
         tensor, history, test, plt = m.make_plots(datetime.datetime(2016, 5, 31))
         wandb.log({"plt_2": plt})
         print("Shut you stupid linter i will use variables later")
