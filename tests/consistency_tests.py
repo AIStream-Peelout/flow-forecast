73c16281ceb1985604af944ad44fe6d349bfbe7c
isaacmg <igodfried@isaac26.com>
2020-11-30 02:11:09 -0400

    print the config

--- tests/consistency_tests.py
+++ tests/consistency_tests.py
@@ -21,6 +21,7 @@ class ConsistencyTests(unittest.TestCase):
         self.assertIn("gcs_m_path_5_model", self.updated_config)
 
     def test_inference_same(self):
+        print(self.updated_config)
         file_path = os.path.join(self.base_path, "test_data", "keag_small.csv")
         m = inference.InferenceMode(336, 20, self.updated_config["gcs_m_path_5_params"], file_path,
                                     self.updated_config["gcs_m_path_5_model"])
