d1114fe875b88ad925244b115d4d035fd073397a
isaacmg <igodfried@isaac26.com>
2020-12-05 18:21:52 -0400

    dummy commit

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -135,8 +135,6 @@ def train_transformer_style(
                 model.model.load_state_dict(torch.load("checkpoint.pth"))
                 break
     decoder_structure = True
-    if model.params["dataset_params"]["class"] != "default":
-        decoder_structure = False
     test = compute_validation(
         test_data_loader,
         model.model,
