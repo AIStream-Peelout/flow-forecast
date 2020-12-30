2f57f6a1665aa92cdb92e229d05e051d42d2494f
isaacmg <igodfried@isaac26.com>
2020-12-30 04:25:02 -0400

    more fixes
    a

--- tests/test_merging_models.py
+++ tests/test_merging_models.py
@@ -9,7 +9,7 @@ class TestLossFunctions(unittest.TestCase):
 
     def test_merger_runs(self):
         m = self.merging_model(torch.rand(2, 6, 10), torch.rand(4))
-        self.assertEqual(m.shape[1], 14)
-
+        self.assertEqual(m.shape[1], 6)
+        self.assertEqual(m.shape[2], 14)
 if __name__ == '__main__':
     unittest.main()
