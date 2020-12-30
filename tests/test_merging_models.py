d6bc08ed34c72a4ce8131b7b86bdf96d8c8c838f
isaacmg <igodfried@isaac26.com>
2020-12-30 03:02:40 -0400

    dumb fixes

--- tests/test_merging_models.py
+++ tests/test_merging_models.py
@@ -8,7 +8,8 @@ class TestLossFunctions(unittest.TestCase):
         self.merging_model = MergingModel("concat", {"combined_shape": 40, "out_shape": 128, "cat_dim": 1})
 
     def test_merger_runs(self):
-        self.merging_model(torch.rand(2, 6, 10), torch.rand(2, 4, 10))
+        m = self.merging_model(torch.rand(2, 6, 10), torch.rand(2, 4, 10))
+        self.assertEqual(m.shape[1], 10)
 
 if __name__ == '__main__':
     unittest.main()
