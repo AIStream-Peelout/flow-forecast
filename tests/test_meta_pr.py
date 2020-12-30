1b5b5ec4c398d0d4304b94a6fdaf8970009fa261
isaacmg <igodfried@isaac26.com>
2020-12-30 01:53:27 -0400

    Update test_meta_pr.py

--- tests/test_meta_pr.py
+++ tests/test_meta_pr.py
@@ -11,11 +11,11 @@ class MetaModels(unittest.TestCase):
         self.AE = AE(10, 128)
 
     def test_ae_init(self):
-        self.assertEqual(self.AE.encoder_hidden_layer.input_shape, 10)
+        self.assertEqual(self.AE.encoder_hidden_layer.input_features, 10)
         self.assertEqual(self.AE(torch.rand(2, 10)).shape, (2, 128))
 
     def test_ae_2(self):
-        self.assertEqual(self.AE.decoder_output_layer.output_shape, 128)
+        self.assertEqual(self.AE.decoder_output_layer.out_features, 128)
         res = numpy_to_tvar(numpy.random(1, 2))
         self.assertIsInstance(res, torch.Tensor)
 
