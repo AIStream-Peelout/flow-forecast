a361566ed3d57c497258e7713fa6953191fffae1
isaacmg <igodfried@isaac26.com>
2021-01-05 02:16:11 -0400

    fix test

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -110,7 +110,7 @@ class EvaluationTest(unittest.TestCase):
         model_result = evaluate_model(
             self.model, "PyTorch", ["cfs"], ["MSE", "L1"], inference_params, {}
         )
-        self.assertGreater(model_result[0]["cfs_L1"], 0)
+        self.assertGreater(model_result[0]["cfs_L1Loss"], 0)
         self.assertGreater(model_result[0]["cfs_MSE"], 1)
 
     def test_evaluator_generate_prediction_samples(self):
