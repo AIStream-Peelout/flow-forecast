7e2eff0754ad6de3147eb620bf8928d198ffa84d
isaacmg <igodfried@isaac26.com>
2021-01-11 17:26:56 -0400

    Supervised auto-encoder

--- tests/test_merging_models.py
+++ tests/test_merging_models.py
@@ -2,11 +2,15 @@ from flood_forecast.meta_models.merging_model import MergingModel
 from flood_forecast.utils import make_criterion_functions
 import unittest
 import torch
+import os
+import json
 
 
-class TestLossFunctions(unittest.TestCase):
+class TestMerging(unittest.TestCase):
     def setUp(self):
         self.merging_model = MergingModel("Concat", {"cat_dim": 2, "repeat": True})
+        with open(os.path.join(os.path.dirname(__file__), "meta_da.json")) as f:
+            self.meta_model_params = json.load(f)
 
     def test_merger_runs(self):
         m = self.merging_model(torch.rand(2, 6, 10), torch.rand(4))
@@ -24,6 +28,7 @@ class TestLossFunctions(unittest.TestCase):
     def test_crit_functions_dict(self):
         res = make_criterion_functions({"MASELoss": {"baseline_method": "mean"}, "MSE": {}})
         self.assertIsInstance(res, list)
+
 
 if __name__ == '__main__':
     unittest.main()
