ef28f651ddda7a4e491afcb5e979dc787166d084
isaacmg <igodfried@isaac26.com>
2021-01-13 00:43:05 -0400

    Revert "add new test code"
    This reverts commit 2d8e6ede73664bb290712e4692ebe8163524eac7.

--- tests/test_multitask_decoder.py
+++ tests/test_multitask_decoder.py
@@ -1,28 +1,14 @@
 import os
 import json
 import unittest
-from flood_forecast.basic.linear_regression import simple_decode
-from flood_forecast.trainer import train_function
-
+from flood_forecast.basic.linear_regression import 
 
 class MultitTaskTests(unittest.TestCase):
     def setUp(self):
         """
         Modules to test model inference.
         """
-        with open(os.path.join(os.path.dirname(__file__), "multi_test.json")) as a:
-            self.model_params = json.load(a)
-        self.keag_path = os.path.join(os.path.dirname(__file__), "test_data", "keag_small.csv")
-        self.forecast_model = train_function("PyTorch", self.model_param)
-
-    def test_decoder_single_step(self):
-        self.forecast_mode= 
-
-
-
-    def test_decoder_multi_step(self):
-        simple_decode(self.forecast_model, torch.ones())
-
+        pass
 
 
 if __name__ == "__main__":
