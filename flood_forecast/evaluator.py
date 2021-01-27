aad8ba655f352e60ab6cd0aa2c68bb73a7f586a3
isaacmg <igodfried@isaac26.com>
2021-01-27 16:07:40 -0400

    fix parameter here

--- flood_forecast/evaluator.py
+++ flood_forecast/evaluator.py
@@ -108,7 +108,7 @@ def evaluate_model(
             if "n_targets" in model.params:
                 df_train_and_test["preds"][history_length:] = end_tensor[:, 0].numpy().tolist()
                 for i, target in enumerate(target_col):
-                    df_train_and_test["pred_" + target] = end_tensor[:, i].numpy().tolist()
+                    df_train_and_test["pred_" + target][history_length:] = end_tensor[:, i].numpy().tolist()
             else:
                 df_train_and_test["preds"][history_length:] = end_tensor_list
                 df_train_and_test["pred_" + target_col[0]] = 0
