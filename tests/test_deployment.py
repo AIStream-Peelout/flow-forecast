bb5ec9fa54dd3d5c2a3e4cf357196aca8c82a307
isaacmg <igodfried@isaac26.com>
2021-01-28 04:49:45 -0400

    add more multi-task support

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -12,11 +12,15 @@ class InferenceTests(unittest.TestCase):
         """
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")) as y:
             self.config_test = json.load(y)
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "multi_config.json")) as x:
+            self.multi_config = json.load(x)
         self.new_csv_path = "gs://flow_datasets/Massachusetts_Middlesex_County.csv"
         self.weight_path = "gs://coronaviruspublicdata/experiments/01_July_202009_44PM_model.pth"
-        self.multi_path = ""
-        self.multi_weight_path = ""
+        self.multi_csv_path = "gs://coronaviruspublicdata/experiments/28_January_202102_14AM.json"
+        self.multi_weight_path = "gs://coronaviruspublicdata/experiments/28_January_202102_14AM_model.pth"
         self.infer_class = InferenceMode(20, 30, self.config_test, self.new_csv_path, self.weight_path, "covid-core")
+        self.infer_class2 = InferenceMode(20, 30, self.multi_config, self.multi_csv_path,
+                                          self.multi_weight_path, "covid-core")
 
     def test_load_model(self):
         model = load_model(self.config_test, self.new_csv_path, self.weight_path)
@@ -29,6 +33,10 @@ class InferenceTests(unittest.TestCase):
 
     def test_plot_model(self):
         self.infer_class.make_plots(datetime(2020, 5, 1), self.new_csv_path, "flow_datasets", "tes1/t.csv", "prod_plot")
+
+    def test_deploy_multi(self):
+        self.infer_class2.make_plots(datetime(2020, 12, 2), self.multi_csv_path, "flow_datasets",
+                                     "tes/t.csv", "prod_plot")
 
     def test_speed(self):
         pass
