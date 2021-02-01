082b919c257c0c144ab1ec1d847dbeb555cdef2f
isaacmg <igodfried@isaac26.com>
2021-02-01 18:18:34 -0400

    begin adding the informer

--- flood_forecast/transformer_xl/informer.py
+++ flood_forecast/transformer_xl/informer.py
@@ -1,8 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from models.attn import FullAttention, ProbAttention, AttentionLayer
+from flood_forecast.transformer_xl.attn import FullAttention, ProbAttention, AttentionLayer
 from models.embed import DataEmbedding
+
+""" This is the implementation of the Informer available from the original authors
+    https://github.com/zhouhaoyi/Informer2020. We have done some minimal refactoring"""
 
 
 class Informer(nn.Module):
@@ -18,23 +21,23 @@ class Informer(nn.Module):
         self.enc_embedding = DataEmbedding(enc_in, d_model, embed, data, dropout)
         self.dec_embedding = DataEmbedding(dec_in, d_model, embed, data, dropout)
         # Attention
-        Attn = ProbAttention if attn=='prob' else FullAttention
+        Attn = ProbAttention if attn == 'prob' else FullAttention
         # Encoder
         self.encoder = Encoder(
             [
                 EncoderLayer(
-                    AttentionLayer(Attn(False, factor, attention_dropout=dropout), 
-                                d_model, n_heads),
+                    AttentionLayer(Attn(False, factor, attention_dropout=dropout),
+                                   d_model, n_heads),
                     d_model,
                     d_ff,
                     dropout=dropout,
                     activation=activation
-                ) for l in range(e_layers)
+                ) for b in range(e_layers)
             ],
             [
                 ConvLayer(
                     d_model
-                ) for l in range(e_layers-1)
+                ) for b in range(e_layers - 1)
             ],
             norm_layer=torch.nn.LayerNorm(d_model)
         )
@@ -42,16 +45,16 @@ class Informer(nn.Module):
         self.decoder = Decoder(
             [
                 DecoderLayer(
-                    AttentionLayer(FullAttention(True, factor, attention_dropout=dropout), 
-                                d_model, n_heads),
-                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout), 
-                                d_model, n_heads),
+                    AttentionLayer(FullAttention(True, factor, attention_dropout=dropout),
+                                   d_model, n_heads),
+                    AttentionLayer(FullAttention(False, factor, attention_dropout=dropout),
+                                   d_model, n_heads),
                     d_model,
                     d_ff,
                     dropout=dropout,
                     activation=activation,
                 )
-                for l in range(d_layers)
+                for c in range(d_layers)
             ],
             norm_layer=torch.nn.LayerNorm(d_model)
         )
@@ -140,4 +143,52 @@ class Encoder(nn.Module):
         if self.norm is not None:
             x = self.norm(x)
 
+        return x
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, self_attention, cross_attention, d_model, d_ff=None,
+                 dropout=0.1, activation="relu"):
+        super(DecoderLayer, self).__init__()
+        d_ff = d_ff or 4 * d_model
+        self.self_attention = self_attention
+        self.cross_attention = cross_attention
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.activation = F.relu if activation == "relu" else F.gelu
+
+    def forward(self, x, cross, x_mask=None, cross_mask=None):
+        x = x + self.dropout(self.self_attention(
+            x, x, x,
+            attn_mask=x_mask
+        ))
+        x = self.norm1(x)
+
+        x = x + self.dropout(self.cross_attention(
+            x, cross, cross,
+            attn_mask=cross_mask
+        ))
+
+        y = x = self.norm2(x)
+        y = self.dropout(self.activation(self.conv1(y.transpose(-1, 1))))
+        y = self.dropout(self.conv2(y).transpose(-1, 1))
+        return self.norm3(x + y)
+
+
+class Decoder(nn.Module):
+    def __init__(self, layers, norm_layer=None):
+        super(Decoder, self).__init__()
+        self.layers = nn.ModuleList(layers)
+        self.norm = norm_layer
+
+    def forward(self, x, cross, x_mask=None, cross_mask=None):
+        for layer in self.layers:
+            x = layer(x, cross, x_mask=x_mask, cross_mask=cross_mask)
+
+        if self.norm is not None:
+            x = self.norm(x)
         return x
