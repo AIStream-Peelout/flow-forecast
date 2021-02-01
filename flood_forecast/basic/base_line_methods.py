f1944959f92ca25e91a83d2fae3a2658020290b8
isaacmg <igodfried@isaac26.com>
2021-02-01 13:46:10 -0400

    fixes to num e

--- flood_forecast/basic/base_line_methods.py
+++ flood_forecast/basic/base_line_methods.py
@@ -5,11 +5,11 @@ class NaiveBase(torch.nn.Module):
     """
     A very simple baseline model that returns
     the fixed value based on the input sequence.
-    No learning used at all
+    No learning used at all a
     """
 
     def __init__(self, seq_length: int, n_time_series: int, output_seq_len=1, metric: str = "last"):
-        super.__init__()
+        super().__init__()
         self.forecast_history = seq_length
         self.n_time_series = n_time_series
         self.initial_layer = torch.nn.Linear(n_time_series, 1)
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,6 +1,9 @@
 from flood_forecast.custom.custom_opt import MASELoss, MAPELoss, RMSELoss, BertAdam, l1_regularizer, orth_regularizer
 from flood_forecast.da_rnn.model import DARNN
 from flood_forecast.custom.dilate_loss import pairwise_distances
+from flood_forecast.training_utils import EarlyStopper
+from flood_forecast.basic.base_line_methods import NaiveBase
+from flood_forecast.custom.custom_activation import _sparsemax_threshold_and_support, _entmax_threshold_and_support
 import unittest
 import torch
 
@@ -50,6 +53,32 @@ class TestLossFunctions(unittest.TestCase):
 
     def test_pairwise(self):
         pairwise_distances(torch.rand(2, 3))
+
+    def test_early_stopper(self):
+        e = EarlyStopper(3, .2)
+        n = NaiveBase(2, 2)
+        e.check_loss(n, .9)
+        e.check_loss(n, .8)
+        self.assertFalse(e.check_loss(n, .75))
+
+    def test_early_stopper2(self):
+        e = EarlyStopper(3, .2)
+        n = NaiveBase(2, 2)
+        e.check_loss(n, .9)
+        e.check_loss(n, .7)
+        self.assertTrue(e.check_loss(n, .6))
+
+    def test_early_stopper3(self):
+        e = EarlyStopper(3, .2, True)
+        n = NaiveBase(2, 2)
+        e.check_loss(n, .9)
+        e.check_loss(n, 1.1)
+        e.check_loss(n, 1.2)
+        self.assertFalse(e.check_loss(n, .8))
+
+    def test_sparse_max_run(self):
+        _entmax_threshold_and_support(torch.rand(2, 20, 3))
+        _sparsemax_threshold_and_support(torch.rand(2, 30, 3))
 
 if __name__ == '__main__':
     unittest.main()
