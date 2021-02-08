26b6e411bead2134f0d269661f7eff52fd3e3a36
isaacmg <igodfried@isaac26.com>
2021-02-08 19:53:46 -0400

    fix bug e

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -228,8 +228,10 @@ def compute_loss(labels, output, src, criterion, validation_dataset, probabilist
                 pass
             output_dist = torch.distributions.Normal(unscaled_out, output_std)
         elif len(output.shape) == 3:
-            output = validation_dataset.inverse_scale(output.cpu().numpy().transpose(0, 2, 1))
-            labels = validation_dataset.inverse_scale(labels.cpu().numpy().transpose(1, 2, 1))
+            output = output.cpu().numpy().transpose(0, 2, 1)
+            labels = labels.cpu().numpy().transpose(0, 2, 1)
+            output = validation_dataset.inverse_scale(torch.from_numpy(output))
+            labels = validation_dataset.inverse_scale(torch.from_numpy(labels))
             src = validation_dataset.inverse_scale(src.cpu().numpy().transpose(1, 2, 1))
         else:
             output = validation_dataset.inverse_scale(output.cpu().transpose(1, 0))
