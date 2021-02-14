2c2ff4b5dd5a4930c385a8cb78545aac94ffb519
isaacmg <igodfried@isaac26.com>
2021-02-14 02:21:16 -0400

    Revert "fix to stuff"
    This reverts commit cc2028a5c9fc3a12dab885628dea38d77e9f92df.

--- flood_forecast/basic/linear_regression.py
+++ flood_forecast/basic/linear_regression.py
@@ -80,12 +80,9 @@ def simple_decode(model: Type[torch.nn.Module],
             else:
                 out = model(src)
             if scaler:
-                out = out.detach().cpu()
                 if multi_targets == 1:
-                    out = out.reshape(-1, 1)
-                if out.shape > 2:
-                    out = out[0, :, :]
-                out = scaler.targ_scaler.transform(out)
+                    out = out.detach().cpu().reshape(-1, 1)
+                out = scaler.targ_scaler.transform(out.detach().cpu())
             if output_len == 1:
                 real_target2[:, i, 0:multi_targets] = out[:, 0]
                 src = torch.cat((src[:, 1:, :], real_target2[:, i, :].unsqueeze(1)), 1)
