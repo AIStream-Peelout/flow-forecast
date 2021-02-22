a14c495da80aa65511db66c01fc9ffd15f2a0742
isaacmg <igodfried@isaac26.com>
2021-02-22 12:25:04 -0400

    add tests of attention to actually run

--- flood_forecast/transformer_xl/masks.py
+++ flood_forecast/transformer_xl/masks.py
@@ -2,7 +2,7 @@ import torch
 
 
 def generate_square_subsequent_mask(sz: int) -> torch.Tensor:
-    r"""Generate a square mask for the sequence. The masked positions are filled with float('-inf').
+    """ Generate a square mask for the sequence. The masked positions are filled with float('-inf').
         Unmasked positions are filled with float(0.0).
     """
     mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
