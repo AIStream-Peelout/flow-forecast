cd51a0bb16a6bed9f26fc177a3bb91e7b229a74a
isaacmg <igodfried@isaac26.com>
2021-02-23 00:12:08 -0400

    add more shit

--- tests/test_informer.py
+++ tests/test_informer.py
@@ -2,6 +2,7 @@ import unittest
 from flood_forecast.transformer_xl.informer import Informer
 import torch
 
+
 class TestInformer(unittest.TestCase):
     def setUp(self):
         self.informer = Informer(3, 3, 3, 20, 20, 20)
