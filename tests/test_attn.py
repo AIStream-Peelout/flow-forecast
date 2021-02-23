6274c5060381d28eef9152c9ef76c9c1f41aac1d
isaacmg <igodfried@isaac26.com>
2021-02-23 00:48:57 -0400

    A shape

--- tests/test_attn.py
+++ tests/test_attn.py
@@ -15,7 +15,7 @@ class TestAttention(unittest.TestCase):
         a = torch.rand(2, 20, 8, 30)
 
         r = self.prob_attention(torch.rand(2, 20, 8, 30), a, torch.rand(2, 20, 8, 30), self.triangle)
-        self.assertGreater(r, 2)
+        self.assertGreater(len(r.shape), 2)
         self.assertIsInstance(r, torch.Tensor)
 
     def test_full_attn(self):
