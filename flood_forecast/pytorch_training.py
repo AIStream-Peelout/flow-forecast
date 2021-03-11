bc0031007b79991744a1855534e6505e36f6987c
isaacmg <igodfried@isaac26.com>
2021-03-11 14:52:58 -0400

    Update pytorch_training.py

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -431,6 +431,8 @@ def compute_validation(validation_loader: DataLoader,
                     output = model(src[0], src[1], filled_targ, targ[0])
                     targ = targ[1][:, -pred_len:, :]
                     src = src[0]
+                    print(targ)
+                    print(output)
 
                 else:
                     output = simple_decode(model=model,
