21cec0dffdb85ae3e9b0acac881c8dee2b2b598e
isaacmg <igodfried@isaac26.com>
2021-03-12 18:14:41 -0400

    more fi

--- flood_forecast/temporal_decoding.py
+++ flood_forecast/temporal_decoding.py
@@ -3,7 +3,8 @@ import torch
 from typing import List
 
 
-def decoding_function(src: torch.Tensor, trg: torch.Tensor, know_columns: List, unknown_columns):
+def decoding_function(src: torch.Tensor, trg: torch.Tensor, know_columns: List, unknown_columns: List,
+                      temp_trg: torch.Tensor):
     """This is function aims to generalize decoding for our temporal
     data loader models. Sepcifically it uses.s
     """
