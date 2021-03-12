35b354bd937e862905e81ff169b2b909603a915a
isaacmg <igodfried@isaac26.com>
2021-03-12 18:16:28 -0400

    e

--- flood_forecast/temporal_decoding.py
+++ flood_forecast/temporal_decoding.py
@@ -4,8 +4,11 @@ from typing import List
 
 
 def decoding_function(src: torch.Tensor, trg: torch.Tensor, know_columns: List, unknown_columns: List,
-                      temp_trg: torch.Tensor):
+                      max_seq_len: int, temp_trg: torch.Tensor, temp_src: torch.Tensor):
     """This is function aims to generalize decoding for our temporal data loader models. Sepcifically it uses.
+    Args:
 
     """
+    if temp_src is None:
+        pass
     pass
