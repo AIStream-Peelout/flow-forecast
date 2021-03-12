012d9fe0cd67a6305be4f0ad55b093c00d64e226
isaacmg <igodfried@isaac26.com>
2021-03-12 18:12:27 -0400

    Update temporal_decoding.py

--- flood_forecast/temporal_decoding.py
+++ flood_forecast/temporal_decoding.py
@@ -1,6 +1,6 @@
 
-def decoding_function():
+def decoding_function(src, trg, know_columns, unknown_columns):
     """This is function aims to generalize decoding for our temporal
-    data loader models. Sepcifically
+    data loader models. Sepcifically it uses.
     """
     pass
