b17914807d7886b62b4af5bcc27c9a0738775f58
isaacmg <igodfried@isaac26.com>
2021-04-12 16:33:09 -0300

    add

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ dev_requirements = [
 
 setup(
     name='flood_forecast',
-    version='0.95dev',
+    version='0.955dev',
     packages=[
         'flood_forecast',
         'flood_forecast.transformer_xl',
