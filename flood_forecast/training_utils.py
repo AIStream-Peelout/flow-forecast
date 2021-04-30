567de898b442e59ae17483232f8a994ed049c4cb
isaacmg <igodfried@isaac26.com>
2021-04-30 01:06:56 -0300

    Revert "fixes"
    This reverts commit 865838eb0a90737f3931fc02cef70e3699e41688.

--- flood_forecast/training_utils.py
+++ flood_forecast/training_utils.py
@@ -67,5 +67,5 @@ class EarlyStopper(object):
             self.counter = 0
         return True
 
-    def save_model_checkpoint(self, model: torch.nn.Module):
+    def save_model_checkpoint(self, model):
         torch.save(model.state_dict(), "checkpoint.pth")
