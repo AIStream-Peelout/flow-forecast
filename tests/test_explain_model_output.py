bb1c3a117809af1fa45ac453cd95ceb7aec4def7
isaacmg <igodfried@isaac26.com>
2021-05-13 03:11:02 -0300

    fix issue e

--- tests/test_explain_model_output.py
+++ tests/test_explain_model_output.py
@@ -145,6 +145,7 @@ class ModelInterpretabilityTest(unittest.TestCase):
              "forecast_length": 5,
              "no_scale": True,
              "relevant_cols": ["cfs", "precip", "temp"],
+             "sort_column": "datetime",
              "feature_params": {
                  "datetime_params": {
                      "hour": "numerical"
