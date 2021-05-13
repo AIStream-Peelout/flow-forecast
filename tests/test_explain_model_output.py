da1992f97162bfa9f9dc081d6a44c8601bd0a26f
isaacmg <igodfried@isaac26.com>
2021-05-13 02:19:23 -0300

    Update test_explain_model_output.py

--- tests/test_explain_model_output.py
+++ tests/test_explain_model_output.py
@@ -146,6 +146,12 @@ class ModelInterpretabilityTest(unittest.TestCase):
              "no_scale": True,
              "relevant_cols": ["cfs", "precip", "temp"],
              "target_col": ["cfs"],
+             "feature_param": {
+                 "datetime_params":
+                 {
+                     "hour": "numerical"
+                 }
+             },
              "interpolate_param": False}
         params_dict["df_path"] = self.keag_file
         params_dict["forecast_total"] = 35
