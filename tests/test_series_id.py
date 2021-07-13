06f1ba00ea184dbc36ad66bf65382ea8ebf79674
isaacmg <igodfried@isaac26.com>
2021-07-12 21:18:14 -0300

    tensor shit

--- tests/test_series_id.py
+++ tests/test_series_id.py
@@ -1,13 +1,23 @@
 from flood_forecast.preprocessing.pytorch_loaders import CSVSeriesIDLoader
 import unittest
-
+import os
 
 class TestInterpolationCSVLoader(unittest.TestCase):
     def setUp(self):
-        self.data_loader = CSVSeriesIDLoader("shit", {}, "")
+        self.dataset_params = {
+            "file_path": os.path.join(self.test_data_path, "keag_small.csv"),
+            "forecast_history": 20,
+            "forecast_length": 20,
+            "relevant_cols": ["cfs", "temp", "precip"],
+            "target_col": ["cfs"],
+            "interpolate_param": False,
+        }
+        self.data_loader = CSVSeriesIDLoader("shit", self.dataset_params, "")
 
     def test_seriesid(self):
-        print("D")
+        x, y = self.data_loader[0]
+        self.assertTrue(x)
+        self.assertGreater(x[0, 0], 1)
 
 if __name__ == '__main__':
     unittest.main()
