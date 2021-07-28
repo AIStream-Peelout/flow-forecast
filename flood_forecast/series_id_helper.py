ea8cbe1c6973cc8ee1affe6a7016628d3f91a250
isaacmg <igodfried@isaac26.com>
2021-07-28 13:50:47 -0300

    fixing stuff

--- flood_forecast/series_id_helper.py
+++ flood_forecast/series_id_helper.py
@@ -1,14 +1,14 @@
 def handle_csv_id_output(src, trg, model, criterion, random_sample=False):
     """A helper function to better handle the output of models with a series_id
 
-    :param src: A list of src sequences
+    :param src: A dictionary of src sequences (partitioned by series_id)
     :type src: torch.Tensor
-    :param trg: A list of target sequences
+    :param trg: A dictionary of target sequences(partitioned by series_id)
     :type trg: torch.Tensor
     :param model: A model that takes both a src and a series_id
     :type model: [type]
     """
-    total_loss = 0
+    total_loss = 0.0
     for (k, v), (k2, v2) in zip(src.items(), trg.items()):
         output = model(k, v)
         loss = criterion(output, v2)
