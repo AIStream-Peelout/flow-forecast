60e1cdcd1fc4428189808faa63fead7b3ab2b2b5
isaacmg <igodfried@isaac26.com>
2021-07-28 14:03:32 -0300

    bug fix

--- flood_forecast/preprocessing/pytorch_loaders.py
+++ flood_forecast/preprocessing/pytorch_loaders.py
@@ -189,10 +189,10 @@ class CSVSeriesIDLoader(CSVDataLoader):
             for va in self.listed_vals:
                 t = torch.Tensor(va.iloc[idx: self.forecast_history + idx].values)
                 targ_start_idx = idx + self.forecast_history
-                idx = va[self.series_id_col].iloc[0]
+                idx2 = va[self.series_id_col].iloc[0]
                 targ = torch.Tensor(va.iloc[targ_start_idx: targ_start_idx + self.forecast_length].to_numpy())
-                src_list[int(idx)] = t
-                targ_list[int(idx)] = targ
+                src_list[int(idx2)] = t
+                targ_list[int(idx2)] = targ
             return src_list, targ_list
         else:
             raise NotImplementedError
