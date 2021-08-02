0e43fda89c716149e89941a0601072a95869c97a
isaacmg <igodfried@isaac26.com>
2021-08-01 22:31:00 -0300

    proper parameter passing 4

--- tests/test_series_id.py
+++ tests/test_series_id.py
@@ -34,7 +34,7 @@ class TestInterpolationCSVLoader(unittest.TestCase):
         """Tests the handle_series_id method
         """
         mse1 = MSELoss()
-        d = DecoderTransformer(10, 8, 4, 128, 10, 0.2, 1, {}, seq_num=True)
+        d = DecoderTransformer(10, 8, 4, 128, 10, 0.2, 1, {}, seq_num=3)
         x, y = self.data_loader[0]
         l1 = handle_csv_id_output(x, y, d, mse1)
         self.assertGreater(l1, 0)
