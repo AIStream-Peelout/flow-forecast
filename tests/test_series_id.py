3f1e7df5c5a901d7f868c10a52d0b425336606fe
isaacmg <igodfried@isaac26.com>
2021-08-04 19:45:31 -0300

    2

--- tests/test_series_id.py
+++ tests/test_series_id.py
@@ -15,7 +15,7 @@ class TestInterpolationCSVLoader(unittest.TestCase):
         self.dataset_params = {
             "file_path": os.path.join(self.test_data_path, "test2.csv"),
             "forecast_history": 20,
-            "forecast_length": 20,
+            "forecast_length": 1,
             "relevant_cols": ["vel", "obs", "day_of_week"],
             "target_col": ["vel"],
             "interpolate_param": False,
@@ -29,16 +29,19 @@ class TestInterpolationCSVLoader(unittest.TestCase):
         self.assertIsInstance(x, dict)
         self.assertIsInstance(y, dict)
         self.assertGreater(x[2][0, 0], 1)
+        print(x[2].shape)
+        self.assertEqual(x[2].shape[1], 3)
 
     def test_handle_series_id(self):
         """Tests the handle_series_id method
         """
         mse1 = MSELoss()
         d1 = DataLoader(self.data_loader, batch_size=2)
-        d = DecoderTransformer(10, 8, 4, 128, 10, 0.2, 1, {}, seq_num=3)
+        d = DecoderTransformer(10, 8, 4, 128, 10, 0.2, 1, {}, seq_num1=3)
         x, y = d1.__iter__().__next__()
         l1 = handle_csv_id_output(x, y, d, mse1)
         self.assertGreater(l1, 0)
+
 
 
 if __name__ == '__main__':
