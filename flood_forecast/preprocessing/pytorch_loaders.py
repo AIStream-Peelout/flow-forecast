a4ac3bcac14ce107e3a5a88465124b89d2cfee89
isaacmg <igodfried@isaac26.com>
2021-08-23 13:53:21 -0300

    fix stuff

--- flood_forecast/preprocessing/pytorch_loaders.py
+++ flood_forecast/preprocessing/pytorch_loaders.py
@@ -79,7 +79,7 @@ class CSVDataLoader(Dataset):
             self.df = self.df[start_stamp:]
         elif end_stamp is not None:
             self.df = self.df[:end_stamp]
-        self.unscaled_df = self.df
+        self.unscaled_df = self.df.copy()
         if scaling is not None:
             print("scaling now")
             self.scale = scaling.fit(self.df[scaled_cols])
@@ -355,12 +355,14 @@ class AEDataloader(CSVDataLoader):
 
 
 class GeneralClassificationLoader(CSVDataLoader):
-    def __init__(self, params: Dict, n_classes=2):
+    def __init__(self, params: Dict, n_classes: int = 2):
         """A generic data loader class for TS classification problems.
 
         :param params: The standard dictionary for a dataloader (see CSVDataLoader)
         :type params: Dict
+        :param n_classes: The number of classes in the problem
         """ # noqa
+        self.n_classes = n_classes
         params["forecast_history"] = params["sequence_length"]
         params["no_scale"] = True
         # This could really be anything as forecast_length is not used
@@ -371,12 +373,16 @@ class GeneralClassificationLoader(CSVDataLoader):
 
     def __getitem__(self, idx: int):
         rows = self.df.iloc[idx: self.forecast_history + idx]
+        targ = self.unscaled_df.iloc[idx: self.forecast_history + idx]
         rows = torch.from_numpy(rows.to_numpy())
-        # Exclude the first row it is the target.
+        targ = torch.from_numpy(targ.to_numpy())
         src = rows[:, 1:]
         # Get label of the series sequence
-        targ = rows[-1, 0]
-        return src.float(), targ.float().unsqueeze(0).unsqueeze(0)
+        targ = targ[-1, 0]
+        targ_labs = torch.zeros(self.n_classes)
+        casted_shit = int(targ.data.tolist())
+        targ_labs[casted_shit] = 1
+        return src.float(), targ_labs.float().unsqueeze(0)
 
 
 class TemporalLoader(CSVDataLoader):
