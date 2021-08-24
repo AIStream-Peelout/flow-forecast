7dd26d08ad2b0f302bd60012271d4bc775a85667
isaacmg <igodfried@isaac26.com>
2021-08-24 14:25:28 -0300

    fix code up

--- tests/test_classification2_loader.py
+++ tests/test_classification2_loader.py
@@ -16,7 +16,7 @@ class TestGeneralClassificationCSVLoader(unittest.TestCase):
             "target_col": ["vel"],
             "interpolate_param": False,
         }
-        self.data_loader = GeneralClassificationLoader(self.dataset_params.copy(), 4)
+        self.data_loader = GeneralClassificationLoader(self.dataset_params.copy(), 7)
 
     def test_classification_return(self):
         """Tests the series_id method for one
