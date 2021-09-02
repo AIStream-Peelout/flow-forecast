a7888c83cf43275e5e122764484e510c6593500a
isaacmg <igodfried@isaac26.com>
2021-09-02 03:18:58 -0300

    w

--- flood_forecast/model_dict_function.py
+++ flood_forecast/model_dict_function.py
@@ -5,6 +5,7 @@ from flood_forecast.transformer_xl.transformer_xl import TransformerXL
 from flood_forecast.transformer_xl.dummy_torch import DummyTorchModel
 from flood_forecast.basic.linear_regression import SimpleLinearModel
 from flood_forecast.basic.lstm_vanilla import LSTMForecast
+from flood_forecast.custom.custom_opt import BertAdam
 from torch.optim import Adam, SGD
 from torch.nn import MSELoss, SmoothL1Loss, PoissonNLLLoss, L1Loss, CrossEntropyLoss, BCELoss
 from flood_forecast.basic.linear_regression import simple_decode
