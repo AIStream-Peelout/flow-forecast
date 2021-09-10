193d0246bf4fcd09490040f17d520893280bc93e
isaacmg <igodfried@isaac26.com>
2021-09-10 17:10:29 -0300

    Revert "s"
    This reverts commit 86da4445e71c344ea155df1f3eef75317ad798f5.

--- tests/test_compute_vallid.py
+++ tests/test_compute_vallid.py
@@ -1,10 +1,10 @@
 from flood_forecast.pytorch_training import compute_validation
 import unittest
 
-
 class TestComputeValidaton(unittest.TestCase):
     def setUp(self):
-        self.classification_loader = 1
+        self.classification_loader = GeneralCl
 
+    
     def test_compute_validation(self):
-        compute_validation()
+        pass
