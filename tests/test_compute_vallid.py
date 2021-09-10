e79830c55aa5bbd89d7afb0ad08c377ec1ddf492
isaacmg <igodfried@isaac26.com>
2021-09-10 18:30:06 -0300

    fix to max points 2

--- tests/test_compute_vallid.py
+++ tests/test_compute_vallid.py
@@ -5,7 +5,14 @@ import unittest
 
 class TestComputeValidation(unittest.TestCase):
     def setUp(self):
-        param = {}
+        param = {
+                    "file_path": "tests/test_data/keag_small.csv",
+                    "forecast_history": 5,
+                    "forecast_length": 1,
+                    "target_col": ["cfs"],
+                    "relevant_cols": ["cfs", "temp", "precip"],
+                    "sort_column": "date",
+                }
         self.classification_loader = GeneralClassificationLoader(param, 2)
 
     def test_compute_validation(self):
