f32d699e102a1c7a15b9cb33375d78efede25336
isaacmg <igodfried@isaac26.com>
2021-09-10 17:03:40 -0300

    e

---
+++ tests/test_compute_vallid.py
@@ -0,0 +1,10 @@
+from flood_forecast.pytorch_training import compute_validation
+import unittest
+
+class TestComputeValidaton(unittest.TestCase):
+    def setUp(self):
+        self.classification_loader = GeneralCl
+
+    
+    def test_compute_validation(self):
+        pass
