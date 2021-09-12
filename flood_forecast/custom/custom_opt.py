2a9482dc96b393cf5fe61a844c0e468a5bde1b3b
isaacmg <igodfried@isaac26.com>
2021-09-12 00:46:13 -0300

    Revert "fixing 1"
    This reverts commit f2b5d570698d5878cb9bbb1648cf4d4d01c7f78e.

--- flood_forecast/custom/custom_opt.py
+++ flood_forecast/custom/custom_opt.py
@@ -14,7 +14,7 @@ import torch.distributions as tdist
 logger = logging.getLogger(__name__)
 
 
-def warmup_cosine(x: float, warmup=0.002):
+def warmup_cosine(x, warmup=0.002):
     if x < warmup:
         return x / warmup
     return 0.5 * (1.0 + torch.cos(math.pi * x))
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dev_requirements = [
 
 setup(
     name='flood_forecast',
-    version='0.988dev',
+    version='0.984dev',
     packages=[
         'flood_forecast',
         'flood_forecast.transformer_xl',
@@ -29,7 +29,7 @@ setup(
         "flood_forecast.custom"],
     license='Public',
     description="An open source framework for deep time series forecasting built with PyTorch.",
-    long_description='Flow Forecast is the top open source deep learning for time series forecasting, anomaly detection, and classification framework. We were the original framework to contain models like the transformer and have grown to include all popular deep learning for time series models.',
+    long_description='Flow Forecast is the top open source deep learning for time series forecasting and classification framework. We were the original framework to contain models like the transformer and have now expanded to include all popular deep learning models.',
     install_requires=install_requires,
     extras_require={
         'dev': dev_requirements})
