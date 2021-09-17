0e58c037e632637d682bb3a0fb551c086c600da5
isaacmg <igodfried@isaac26.com>
2021-09-16 23:09:05 -0300

    Revert "e"
    This reverts commit 232bc778ba4762223f01773f70d067f5cce15aa8.

--- flood_forecast/transformer_xl/informer.py
+++ flood_forecast/transformer_xl/informer.py
@@ -6,7 +6,7 @@ from flood_forecast.transformer_xl.data_embedding import DataEmbedding
 
 
 class Informer(nn.Module):
-    def __init__(self, n_time_series: int, dec_in: int, c_out: int, seq_len: int, label_len, out_len,
+    def __init__(self, n_time_series: int, dec_in: int, c_out: int, seq_len, label_len, out_len,
                  factor: int = 5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512,
                  dropout=0.0, attn='prob', embed='fixed', temp_depth=4, activation='gelu',
                  device=torch.device('cuda:0')):
