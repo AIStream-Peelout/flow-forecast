774dc80056eb123c9c814bb7897c7caf5b9ab0fd
isaacmg <igodfried@isaac26.com>
2021-09-30 03:10:46 -0300

    remove unused function

--- flood_forecast/basic/linear_regression.py
+++ flood_forecast/basic/linear_regression.py
@@ -37,14 +37,7 @@ class SimpleLinearModel(torch.nn.Module):
             return x.view(-1, self.output_len)
 
 
-def handle_gaussian_loss(out: tuple):
-    # Oh shit this is gonna be tough
-    print("shit stuff")
-    print(out[1])
-    out1 = torch.mean(torch.stack([out[0], out[1]]), dim=0)
     return out1, out[0], out[1]
-
-
 def handle_no_scaling(scaler: torch.utils.data.Dataset, out: torch.Tensor, multi_targets: int):
     if multi_targets == 1:
         out = out.detach().cpu().reshape(-1, 1)
