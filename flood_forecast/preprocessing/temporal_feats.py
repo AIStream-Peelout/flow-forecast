72d699da8c05fb4fa713f1dfce7830378feadc83
isaacmg <igodfried@isaac26.com>
2021-10-04 19:05:46 -0300

    d

--- flood_forecast/preprocessing/temporal_feats.py
+++ flood_forecast/preprocessing/temporal_feats.py
@@ -22,8 +22,8 @@ def make_temporal_features(features_list: Dict, dt_column: str, df: pd.DataFrame
 
 
 def create_feature(key: str, value: str, df: pd.DataFrame, dt_column: str):
-    """Function to create temporal features
-        Uses dict to make val.
+    """Function to create temporal feature. Uses dict to make val.
+
     :param key: The datetime feature you would like to create
     :type key: str s
     :param value: The type of feature you would like to create (cyclical or numerical)
