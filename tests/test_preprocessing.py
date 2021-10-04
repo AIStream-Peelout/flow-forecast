2220369fb4daba9dad3ccdb0eb9dd96e51a11d4d
isaacmg <igodfried@isaac26.com>
2021-10-04 19:03:10 -0300

    add t

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,4 +1,5 @@
 from flood_forecast.preprocessing.interpolate_preprocess import back_forward_generic
+from flood_forecast.preprocessing.temporal_feats import make_temporal_features
 import unittest
 import pandas as pd
 import os
@@ -7,7 +8,9 @@ import os
 class TestInterpolationCode(unittest.TestCase):
     def setUp(self):
         file_path = os.path.join(os.path.dirname(__file__), "test_data", "farm_ex.csv")
+        file_path_2 = os.path.join(os.path.dirname(__file__), "test_data", "fake_test_small.csv")
         self.df = pd.read_csv(file_path)
+        self.df_2 = pd.read_csv(file_path_2)
 
     def test_back_forward(self):
         """Test the generation of forward and backward data
@@ -15,8 +18,10 @@ class TestInterpolationCode(unittest.TestCase):
         df = back_forward_generic(self.df, ["NumberOfAnimals"])
         self.assertEqual(df.iloc[3]["NumberOfAnimals"], 165)
 
-    def test_back_fill(self):
-        pass
+    def test_make_temp_feats(self):
+        feats = make_temporal_features({"hour": "cyclical"}, "datetime", self.df_2)
+        self.assertIn("sin_hour", feats.columns)
+        self.assertIn("cos_hour", feats.columns)
 
 if __name__ == '__main__':
     unittest.main()
