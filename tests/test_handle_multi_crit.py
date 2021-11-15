4c219148aa63549241b0b1d73c01f341f4a7c9d3
isaacmg <igodfried@isaac26.com>
2021-11-14 22:56:36 -0400

    Revert "add crit back"
    This reverts commit 0ce3ecd8f97546b12db8077447bb2c3c5391b56e.

--- tests/test_handle_multi_crit.py
+++ tests/test_handle_multi_crit.py
@@ -9,11 +9,7 @@ class TestHandleMultiCrit(unittest.TestCase):
         self.crit_list = [BCELoss(), MSELoss()]
 
     def test_multi_crit(self):
-        l1 = multi_crit(self.crit_list, torch.rand(20, 4, 2), torch.rand(20, 4, 2))
-        self.assertGreater(l1.item(), 2)
-
-    def test_focal_loss(self):
-        pass
+        multi_crit(self.crit_list, torch.rand(20, 4, 2), torch.rand(20, 4, 2))
 
 if __name__ == "__main__":
     unittest.main()
