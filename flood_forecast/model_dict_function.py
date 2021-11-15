0950ae76a8870a98459f9335d0cb35ae0272fc1f
isaacmg <igodfried@isaac26.com>
2021-11-15 15:47:30 -0400

    fix naming v
    vj and the otehrqp st

--- flood_forecast/model_dict_function.py
+++ flood_forecast/model_dict_function.py
@@ -10,6 +10,7 @@ from torch.optim import Adam, SGD
 from torch.nn import MSELoss, SmoothL1Loss, PoissonNLLLoss, L1Loss, CrossEntropyLoss, BCELoss, BCEWithLogitsLoss
 from flood_forecast.basic.linear_regression import simple_decode
 from flood_forecast.transformer_xl.transformer_basic import greedy_decode
+from flood_forecast.custom.focal_loss import FocalLoss
 from flood_forecast.da_rnn.model import DARNN
 from flood_forecast.custom.custom_opt import (RMSELoss, MAPELoss, PenalizedMSELoss, NegativeLogLikelihood, MASELoss,
                                               GaussianLoss)
@@ -47,8 +48,8 @@ pytorch_criterion_dict = {
     "PenalizedMSELoss": PenalizedMSELoss,
     "CrossEntropyLoss": CrossEntropyLoss,
     "NegativeLogLikelihood": NegativeLogLikelihood,
-    "BCELossLogists": BCEWithLogitsLoss,
-    "FocalLoss": "FocalLoss",
+    "BCELossLogits": BCEWithLogitsLoss,
+    "FocalLoss": FocalLoss,
     "BinaryCrossEntropy": BCELoss}
 
 decoding_functions = {"greedy_decode": greedy_decode, "simple_decode": simple_decode}
