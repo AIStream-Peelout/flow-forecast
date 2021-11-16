8f6e55f546e5fcb135a12c0a59aab505119f931f
isaacmg <igodfried@isaac26.com>
2021-11-16 01:12:15 -0400

    add new things 1 1

--- tests/test_handle_multi_crit.py
+++ tests/test_handle_multi_crit.py
@@ -1,15 +1,21 @@
 from torch.nn import MSELoss, BCELoss
 import unittest
 from flood_forecast.pytorch_training import multi_crit
+from flood_forecast.custom.focal_loss import FocalLoss
 import torch
 
 
 class TestHandleMultiCrit(unittest.TestCase):
     def setUp(self):
         self.crit_list = [BCELoss(), MSELoss()]
+        self.focal_loss = FocalLoss(0.25)
 
     def test_multi_crit(self):
-        multi_crit(self.crit_list, torch.rand(20, 4, 2), torch.rand(20, 4, 2))
+        l1 = multi_crit(self.crit_list, torch.rand(20, 4, 2), torch.rand(20, 4, 2))
+        self.assertGreater(l1.item(), 2)
+
+    def test_focal_loss(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
