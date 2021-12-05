7edcd8b420ab0f5e8630061f5219a642a681f87f
isaacmg <igodfried@isaac26.com>
2021-12-05 14:02:39 -0400

    fixing dual 1e 1

--- flood_forecast/pytorch_training.py
+++ flood_forecast/pytorch_training.py
@@ -21,9 +21,9 @@ def multi_crit(crit_multi, output, labels, valid=None):
     i = 0
     for crit in crit_multi:
         if len(output.shape) == 3:
-            loss += compute_loss(labels[:, :, i], output[:, :, i], torch.rand(1, 2), crit, valid)
+            loss += compute_loss(labels[:, :, i], output[:, :, i], torch.rand(1, 2), crit, valid).item()
         else:
-            loss += compute_loss(labels[:, i], output[:, i], torch.rand(1, 2), crit, valid)
+            loss += compute_loss(labels[:, i], output[:, i], torch.rand(1, 2), crit, valid).item()
     summed_loss = loss
     return summed_loss
 
@@ -164,7 +164,7 @@ def train_transformer_style(
             model,
             opt,
             criterion,
-            data_loader,  # s
+            data_loader,
             takes_target,
             meta_model,
             meta_representation,
@@ -426,13 +426,14 @@ def torch_single_train(model: PyTorchForecast,
             loss = multi_crit(criterion, output, labels, None)
         else:
             loss = compute_loss(labels, output, src, criterion, None, probablistic, output_std, m=multi_targets)
+            loss = loss.item()
         if loss > 100:
             print("Warning: high loss detected")
         loss.backward()
         opt.step()
         if torch.isnan(loss) or loss == float('inf'):
             raise ValueError("Error infinite or NaN loss detected. Try normalizing data or performing interpolation")
-        running_loss += loss.item()
+        running_loss += loss
         i += 1
     print("The running loss is: ")
     print(running_loss)
