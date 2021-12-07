f84181bc83243a376bbbe57ee13f3944dff9cc37
isaacmg <igodfried@isaac26.com>
2021-12-07 18:41:55 -0400

    fix multi crit

--- tests/test_handle_multi_crit.py
+++ tests/test_handle_multi_crit.py
@@ -1,13 +1,17 @@
 import unittest
 from flood_forecast.pytorch_training import multi_crit
 from torch.nn import BCELoss
-from flood_forecast.custom.focal_loss import FocalLoss
+from flood_forecast.custom.focal_loss import FocalLoss, BinaryFocalLossWithLogits
 import torch
 
 
 class TestMulticrit(unittest.TestCase):
     def setUp(self):
+<<<<<<< Updated upstream
         self.crit = [BCELoss(), FocalLoss(0.25, reduction="sum")]
+=======
+        self.crit = [BCELoss(), FocalLoss(0.25, reduction="sum"), BinaryFocalLossWithLogits(0.25, reduction="mean")]
+>>>>>>> Stashed changes
 
     def test_crit_function(self):
         r1 = multi_crit(self.crit, torch.rand(4, 20, 5), torch.ones(4, 20, 5, dtype=torch.int64))
