2dae8ac5bdde8d34bcfd0d804e080171b290eb94
isaacmg <igodfried@isaac26.com>
2021-12-13 00:32:02 -0400

    fixing target 1

--- flood_forecast/custom/focal_loss.py
+++ flood_forecast/custom/focal_loss.py
@@ -130,6 +130,8 @@ class FocalLoss(nn.Module):
         self.eps: Optional[float] = eps
 
     def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        if len(target.shape) == 3:
+            target = target[:, 0, :]
         if len(target.shape) == 2:
             target = target[:, 0]
         if len(input.shape) == 3:
