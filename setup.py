f1f84f96929a5cbea57c0eaf658556001c390a7a
isaacmg <igodfried@isaac26.com>
2022-05-09 22:32:34 -0300

    remove all sudo commands

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dev_requirements = [
 
 setup(
     name='flood_forecast',
-    version='0.99555dev',
+    version='0.9956dev',
     packages=[
         'flood_forecast',
         'flood_forecast.transformer_xl',
