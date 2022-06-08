6e1f8d83812b32a055c8f7d7b161d7da93451224
isaacmg <igodfried@isaac26.com>
2022-06-08 15:52:06 -0300

    v diff

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dev_requirements = [
 
 setup(
     name='flood_forecast',
-    version='0.9998dev',
+    version='0.99988dev',
     packages=[
         'flood_forecast',
         'flood_forecast.transformer_xl',
