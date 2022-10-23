37827900b31ae5d63b5d9cc41b26692ddd259f1a
isaacmg <igodfried@isaac26.com>
2022-10-23 15:35:32 -0300

    fix gru issue 1
    w2

--- flood_forecast/basic/gru_vanilla.py
+++ flood_forecast/basic/gru_vanilla.py
@@ -2,10 +2,16 @@ import torch
 
 
 class VanillaGRU(torch.nn.Module):
-    """
-    Simple GRU to preform deep time series forecasting.
-    """
-    def __init__(self, n_time_series, hidden_dim, layer_dim, output_dim, dropout_prob, use_hidden=False):
+    def __init__(self, n_time_series: int, hidden_dim: int, layer_dim: int, output_dim: int, dropout: float,
+                 forecast_length=1, use_hidden=False):
+        """
+        Simple GRU to preform deep time series forecasting.
+
+        :param n_time_series:
+        :type n_time_series:
+        :param hidden_dim:
+        :type hidden_dim:
+        """
         super(VanillaGRU, self).__init__()
 
         # Defining the number of layers and the nodes in each layer
@@ -13,18 +19,26 @@ class VanillaGRU(torch.nn.Module):
         self.hidden_dim = hidden_dim
         self.hidden = None
         self.use_hidden = use_hidden
+        self.forecast_length = forecast_length
 
         # GRU layers
         self.gru = torch.nn.GRU(
-            n_time_series, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+            n_time_series, hidden_dim, layer_dim, batch_first=True, dropout=dropout
         )
 
         # Fully connected layer
         self.fc = torch.nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward function for GRU
+
+        :param x: torch of shape
+        :type model: torch.Tensor
+        :return: Returns a
+        :rtype: torch.Tensor
+        """
         # Initializing hidden state for first input with zeros
-        if self.hidden and self.use_hidden:
+        if self.hidden is not None and self.use_hidden:
             h0 = self.hidden
         else:
             h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
@@ -34,7 +48,8 @@ class VanillaGRU(torch.nn.Module):
 
         # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
         # so that it can fit into the fully connected layer
-        out = out[:, -1, :]
+        out = out[:, -self.forecast_length, :]
 
         # Convert the final state to our desired output shape (batch_size, output_dim)
         out = self.fc(out)
+        return out
