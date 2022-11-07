6208beca74c07698b8bcde5421fbbce414e292bd
isaacmg <igodfried@isaac26.com>
2022-11-06 21:11:59 -0400

    Revert "re-add file and stuff"
    This reverts commit 9963a0beb4f549e41ce251963edebf95e4b6f1d0.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dev_requirements = [
 
 setup(
     name='flood_forecast',
-    version='0.9999998dev',
+    version='0.999998dev',
     packages=[
         'flood_forecast',
         'flood_forecast.transformer_xl',
