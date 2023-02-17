65ec02da1b2ad41dfa4cf0db8f9526162cb78d86
isaacmg <igodfried@isaac26.com>
2023-02-17 15:43:21 -0400

    add play id fix stuf 1 2

--- flood_forecast/preprocessing/pytorch_loaders.py
+++ flood_forecast/preprocessing/pytorch_loaders.py
@@ -571,8 +571,8 @@ class VariableSequenceLength(CSVDataLoader):
     def get_item_classification(self, idx: int):
         item = self.grouped_df.get_group(self.uniques[idx])
         item = item.to_numpy()
-        label = item[0, -1].to_numpy()
-        return torch.from_numpy(item[:, :]), torch.from_numpy(label)
+        label = item[0, -1]
+        return torch.from_numpy(item), torch.from_numpy(label)
 
     def get_item_auto_encoder(self, idx):
         item = self.grouped_df.get_group(self.uniques[idx])
