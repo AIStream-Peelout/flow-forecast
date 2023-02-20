f5190b0181770b52e429a57d3ddc742a5466bf25
isaacmg <igodfried@isaac26.com>
2023-02-20 01:52:39 -0400

    r fr

--- flood_forecast/preprocessing/pytorch_loaders.py
+++ flood_forecast/preprocessing/pytorch_loaders.py
@@ -548,7 +548,7 @@ class TemporalTestLoader(CSVTestLoader):
 
 class VariableSequenceLength(CSVDataLoader):
     def __init__(self, series_marker_column: str, csv_loader_params: Dict, pad_length=None, task="classification",
-                 n_classes=97):
+                 n_classes=9 + 90):
         """Enables easy loading of time-series with variable length data
 
         :param series_marker_column: The column that dealinates when an example begins and ends
