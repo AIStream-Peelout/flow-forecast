beaaab6c849a27184608e7d51065195943cf2ac7
isaacmg <igodfried@isaac26.com>
2023-03-24 19:01:39 -0300

    fixing shit1 4
    Fixing ths hti here. 1

--- flood_forecast/basic/d_n_linear.py
+++ flood_forecast/basic/d_n_linear.py
@@ -53,7 +53,7 @@ class DLinear(nn.Module):
         """
         super(DLinear, self).__init__()
         self.seq_len = forecast_history
-        self.pred_len = forecast_length
+        self.pre_len1 = forecast_length
         self.n_targs = n_targs
 
         # Decompsition Kernel Size
@@ -66,18 +66,18 @@ class DLinear(nn.Module):
             self.Linear_Seasonal = nn.ModuleList()
             self.Linear_Trend = nn.ModuleList()
             for i in range(self.channels):
-                self.Linear_Seasonal.append(nn.Linear(self.seq_len, self.pred_len))
-                self.Linear_Trend.append(nn.Linear(self.seq_len, self.pred_len))
+                self.Linear_Seasonal.append(nn.Linear(self.seq_len, self.pre_len1))
+                self.Linear_Trend.append(nn.Linear(self.seq_len, self.pre_len1))
 
                 # Use this two lines if you want to visualize the weights
-                # self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len])
-                # self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+                # self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pre_len1,self.seq_len])
+                # self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pre_len1,self.seq_len]))
         else:
-            self.Linear_Seasonal = nn.Linear(self.seq_len, self.pred_len)
-            self.Linear_Trend = nn.Linear(self.seq_len, self.pred_len)
+            self.Linear_Seasonal = nn.Linear(self.seq_len, self.pre_len1)
+            self.Linear_Trend = nn.Linear(self.seq_len, self.pre_len1)
             # Use this two lines if you want to visualize the weights
-            # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-            # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+            # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pre_len1,self.seq_len]))
+            # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pre_len1,self.seq_len]))
 
     def forward(self, x: torch.Tensor):
         """The
@@ -91,10 +91,10 @@ class DLinear(nn.Module):
         seasonal_init, trend_init = seasonal_init.permute(0, 2, 1), trend_init.permute(0, 2, 1)
         if self.individual:
             seasonal_output = torch.zeros([seasonal_init.size(0),
-                                          seasonal_init.size(1), self.pred_len],
+                                          seasonal_init.size(1), self.pre_len1],
                                           dtype=seasonal_init.dtype).to(seasonal_init.device)
             trend_output = torch.zeros([trend_init.size(0),
-                                        trend_init.size(1), self.pred_len], dtype=trend_init.dtype).to(trend_init.device)
+                                        trend_init.size(1), self.pre_len1], dtype=trend_init.dtype).to(trend_init.device)
             for i in range(self.channels):
                 seasonal_output[:, i, :] = self.Linear_Seasonal[i](seasonal_init[:, i, :])
                 trend_output[:, i, :] = self.Linear_Trend[i](trend_init[:, i, :])
