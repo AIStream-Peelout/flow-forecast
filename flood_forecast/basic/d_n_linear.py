2e9e6f440eafcf3696ac24230b6917181711f59b
isaacmg <igodfried@isaac26.com>
2023-03-24 21:42:59 -0300

    add

--- flood_forecast/basic/d_n_linear.py
+++ flood_forecast/basic/d_n_linear.py
@@ -2,6 +2,40 @@ import torch
 import torch.nn as nn
 
 
+class NLinear(nn.Module):
+    """
+    Normalization-Linear
+    """
+    def __init__(self, configs):
+        super(NLinear, self).__init__()
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        # Use this line if you want to visualize the weights
+        # self.Linear.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+        self.channels = configs.enc_in
+        self.individual = configs.individual
+        if self.individual:
+            self.Linear = nn.ModuleList()
+            for i in range(self.channels):
+                self.Linear.append(nn.Linear(self.seq_len, self.pred_len))
+        else:
+            self.Linear = nn.Linear(self.seq_len, self.pred_len)
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+        seq_last = x[:, -1:, :].detach()
+        x = x - seq_last
+        if self.individual:
+            output = torch.zeros([x.size(0), self.pred_len, x.size(2)], dtype=x.dtype).to(x.device)
+            for i in range(self.channels):
+                output[:, :, i] = self.Linear[i](x[:, :, i])
+            x = output
+        else:
+            x = self.Linear(x.permute(0, 2, 1)).permute(0, 2, 1)
+        x = x + seq_last
+        return x  # [Batch, Output length, Channel]
+
+
 class MovingAvg(nn.Module):
     """
     Moving average block to highlight the trend of time series
