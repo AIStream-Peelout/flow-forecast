c2d442e5d081f949111005d11712dc6858e498d5
isaacmg <igodfried@isaac26.com>
2023-05-19 21:37:15 -0300

    fixing code 2

--- flood_forecast/interpretability.py
+++ flood_forecast/interpretability.py
@@ -1,13 +1,15 @@
 from captum.attr import IntegratedGradients
+from typing import Tuple
 
+attr_dict = {}
 
-def run_attribution(model, test_loader, method):
+def run_attribution(model, test_loader, method) -> Tuple:
     """
     """
     ig = IntegratedGradients(model)
     x, y = test_loader[0]
-    attributions, approximation_error = ig.attribute((input1, input2),
-                                                 baselines=(baseline1, baseline2),
+    attributions, approximation_error = ig.attribute(x,
+                                                 baselines=(baseline1),
                                                  method='gausslegendre',
                                                  return_convergence_delta=True)
-    pass
+    return attributions, approximation_error
