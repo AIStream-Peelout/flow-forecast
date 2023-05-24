726e07737865f3c27a4e4242a25158519dcbd69b
isaacmg <igodfried@isaac26.com>
2023-05-24 14:26:53 -0300

    fixing captum 4 5

---
+++ tests/test_captum.py
@@ -0,0 +1,24 @@
+import unittest
+import torch
+from flood_forecast.interpretability import run_attribution, create_attribution_plots
+from flood_forecast.basic.gru_vanilla import GRUVanilla
+from flood_forecast.preprocessing.pytorch_loaders import CSVDataLoader
+
+
+class TestAttention(unittest.TestCase):
+    def setUp(self):
+        # n_time_series: int, hidden_dim: int, num_layers: int, n_target: int, dropout: float
+        self.test_model = GRUVanilla(3, 128, 2, 1, 0.2)
+        self.test_data_loader = CSVDataLoader(
+            "tests/data/test_data.csv",
+            100,
+            20,
+            "precip",
+            ["precip", "cfs", "temp"]
+        )
+
+    def test_run_attribution(self):
+        """_summary_"""
+        attributions, approx_error = run_attribution(self.test_model, self.test_data_loader, "IntegratedGradients", {})
+        self.assertEqual(approx_error.shape, torch.Size([1, 20, 3]))
+        self.assertEqual(attributions.shape, torch.Size([1, 20, 3]))
