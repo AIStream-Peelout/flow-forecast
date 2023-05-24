bd8061f6e9dbc58b2ae120efab3f85a8e7f427a0
isaacmg <igodfried@isaac26.com>
2023-05-24 17:34:39 -0300

    fixing captum tests

--- flood_forecast/interpretability.py
+++ flood_forecast/interpretability.py
@@ -1,5 +1,6 @@
 from captum.attr import IntegratedGradients, DeepLift, GradientShap, NoiseTunnel, FeatureAblation
 from typing import Tuple, Dict
+import numpy as np
 
 attr_dict = {"IntegratedGradients": IntegratedGradients, "DeepLift": DeepLift, "GradientSHAP": GradientShap,
              "NoiseTunnel": NoiseTunnel, "FeatureAblation": FeatureAblation}
@@ -14,7 +15,7 @@ def run_attribution(model, test_loader, method, additional_params: Dict) -> Tupl
     :type test_loader: _type_
     :param method: _description_
     :type method: _type_
-    :return: d
+    :return: Returns a Tuple of attributions and approximation error. This data is used to create plots.
     :rtype: Tuple
     """
 
@@ -24,7 +25,7 @@ def run_attribution(model, test_loader, method, additional_params: Dict) -> Tupl
     return attributions, approximation_error
 
 
-def make_attribution_plots(attributions, approximation_error, use_wandb: bool = True):
+def make_attribution_plots(x, attributions, approximation_error, feature_names, use_wandb: bool = True):
     """_summary_
 
     :param attributions: _description_
@@ -34,4 +35,6 @@ def make_attribution_plots(attributions, approximation_error, use_wandb: bool = 
     :param use_wandb: _description_, defaults to True
     :type use_wandb: bool, optional
     """
-    pass
+    x_axis_data = np.arange(x.shape[1])
+    print(x_axis_data)
+    print("Hello world")
