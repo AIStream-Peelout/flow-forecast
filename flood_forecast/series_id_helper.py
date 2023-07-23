57e3555639fa4d2edd85d6290d1bc848cd237f1f
isaacmg <igodfried@isaac26.com>
2023-07-23 05:21:13 +0100

    fixing computing of the loss
    Fixing the computing of loss and more for now 2
    s1

--- flood_forecast/series_id_helper.py
+++ flood_forecast/series_id_helper.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 
 def handle_csv_id_output(src: Dict[int, torch.Tensor], trg: Dict[int, torch.Tensor], model, criterion,
-                         random_sample: bool = False, n_targs: int = 1):
+                         random_sample: bool = False, n_targs: int = 1) -> float:
     """A helper function to better handle the output of models with a series_id and compute loss,
 
     :param src: A dictionary of src sequences (partitioned by series_id)
@@ -21,4 +21,6 @@ def handle_csv_id_output(src: Dict[int, torch.Tensor], trg: Dict[int, torch.Tens
         loss = criterion(output, v2[:, :, :n_targs])
         total_loss += loss.item()
     total_loss /= len(src.keys())
+    loss.backward()
+    loss.step()
     return total_loss
