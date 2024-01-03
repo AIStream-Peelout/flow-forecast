ea62c6cd6420dd1a2059c459c16be1cf2c1ece6c
isaacmg <igodfried@isaac26.com>
2024-01-03 01:09:26 -0500

    Revert "fixing the code"
    This reverts commit 68c1fe42ce082acf11c4a39a4ad5cb92dce2a23d.

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -163,7 +163,6 @@ class EvaluationTest(unittest.TestCase):
             inference_params_with_scaling,
             {},
         )
-        print("res bel")
         print(model_result_1)
         self.assertFalse(model_result_1[1]["preds"].equals(model_result_2[1]["preds"]))
 
@@ -193,7 +192,6 @@ class EvaluationTest(unittest.TestCase):
             inference_params_with_scaling,
             {},
         )
-        print("res bel")
         print(model_result_1)
         self.assertFalse(model_result_1[3][0].equals(model_result_2[3][0]))
 
