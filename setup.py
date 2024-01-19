0e650356a1de712aca1ef7e4325c4595f17c59cc
isaacmg <igodfried@isaac26.com>
2024-01-19 14:57:34 -0500

    fixing mixer  r

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ dev_requirements = [
 
 setup(
     name='flood_forecast',
-    version='1.00dev',
+    version='1.01dev',
     packages=[
         'flood_forecast',
         'flood_forecast.transformer_xl',
