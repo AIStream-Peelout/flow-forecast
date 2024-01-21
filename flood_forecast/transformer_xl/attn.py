3ae0071d4282edc359cf01803089c3dffb6a699c
isaacmg <igodfried@isaac26.com>
2024-01-20 22:47:11 -0500

    what

--- flood_forecast/transformer_xl/attn.py
+++ flood_forecast/transformer_xl/attn.py
@@ -6,10 +6,11 @@ from flood_forecast.transformer_xl.masks import TriangularCausalMask, ProbMask
 
 
 class FullAttention(nn.Module):
-    def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1):
+    def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1, output_attention=False):
         super(FullAttention, self).__init__()
         self.scale = scale
         self.mask_flag = mask_flag
+        self.output_attention = output_attention
         self.dropout = nn.Dropout(attention_dropout)
 
     def forward(self, queries, keys, values, attn_mask):
@@ -40,7 +41,10 @@ class FullAttention(nn.Module):
         A = self.dropout(torch.softmax(scale * scores, dim=-1))
         V = torch.einsum("bhls,bshd->blhd", A, values)
 
-        return V.contiguous()
+        if self.output_attention:
+            return (V.contiguous(), A)
+        else:
+            return (V.contiguous(), None)
 
 
 class ProbAttention(nn.Module):
