d2371294a8ca06371f03f940910473c2e1f1bd98
isaacmg <igodfried@isaac26.com>
2024-02-29 23:57:24 -0500

    Revert "finishing the print removal"
    This reverts commit 4e18a4b1e250e8d06333d89f2bab4b6178629371.

--- flood_forecast/transformer_xl/informer.py
+++ flood_forecast/transformer_xl/informer.py
@@ -124,7 +124,10 @@ class Informer(nn.Module):
         """
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
         enc_out, _ = self.encoder(enc_out, attn_mask=enc_self_mask)
+        print(type(enc_out))
+        print("thing above")
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
+        print(type(dec_out))
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
         dec_out = self.projection(dec_out)
         # dec_out = self.end_conv1(dec_out)
