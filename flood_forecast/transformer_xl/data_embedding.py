d0f80359b171d68ed1a2facf17bd26de93bf1f44
isaacmg <igodfried@isaac26.com>
2024-07-05 09:28:31 -0400

    more vivit models and embeddings

--- flood_forecast/transformer_xl/data_embedding.py
+++ flood_forecast/transformer_xl/data_embedding.py
@@ -257,3 +257,44 @@ class PositionalEncoding2D(nn.Module):
         emb[:, :, :, self.channels : 2 * self.channels] = emb_y
 
         return emb
+
+class NeRF_embedding(nn.Module):
+    def __init__(self, n_layers: int = 5):
+        super().__init__()
+        self.n_layers = n_layers
+        self.dim = self.n_layers * 4
+
+    def forward(self, spatial_coords: torch.Tensor):
+        """
+        Args:
+            spatial_coords (torch.Tensor): Spatial coordinates of shape [B, 2, H, W]
+        """
+        embeddings = []
+        for i in range(self.n_layers):
+            embeddings += [
+                torch.sin((2**i * torch.pi) * spatial_coords),
+                torch.cos((2**i * torch.pi) * spatial_coords),
+            ]
+        embeddings = torch.cat(embeddings, axis=1)
+        return embeddings
+
+
+class CyclicalEmbedding(nn.Module):
+    def __init__(self, frequencies: list = [12, 31, 24, 60]):
+        super().__init__()
+        self.frequencies = frequencies
+        self.dim = len(self.frequencies) * 2
+
+    def forward(self, time_coords: torch.Tensor):
+        """
+        Args:
+            time_coords (torch.Tensor): Time coordinates of shape [B, T, C, H, W]
+        """
+        embeddings = []
+        for i, frequency in enumerate(self.frequencies):
+            embeddings += [
+                torch.sin(2 * torch.pi * time_coords[:, :, i] / frequency),
+                torch.cos(2 * torch.pi * time_coords[:, :, i] / frequency),
+            ]
+        embeddings = torch.stack(embeddings, axis=2)
+        return embeddings
--- tests/mult_modal_tests/test_cross_vivit.py
+++ tests/mult_modal_tests/test_cross_vivit.py
@@ -1,11 +1,16 @@
 import unittest
 import torch
-from flood_forecast.multi_models.crossvivit import RoCrossViViT
+from flood_forecast.multi_models.crossvivit import RoCrossViViT, VisionTransformer
+from flood_forecast.transformer_xl.data_embedding import CyclicalEmbedding, NeRF_embedding
 
 
 class TestCrossVivVit(unittest.TestCase):
     def setUp(self):
-        self.crossvivit = RoCrossViViT(image_size=(128, 128), patch_size=(8, 8), time_coords_encoder=)
+        self.crossvivit = RoCrossViViT(image_size=(128, 128), patch_size=(8, 8), time_coords_encoder=NeRF_embedding(), **{"max_freq":12})
+
+    def test_vivit_model(self):
+        self.vivit_model = VisionTransformer(128, 5, 8, 128, 128, [512, 512, 512] )
+        pass
 
     def test_forward(self):
         x = self.crossvivit(torch.randn(1, 3, 128, 128))
